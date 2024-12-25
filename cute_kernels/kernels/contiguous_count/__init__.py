4f78a9f5b8f7558673e982905b6da6f407b020d5
Mayank Mishra <mayank31398@gmail.com>
2024-12-25 01:15:13 -0500

    better contiguous_count
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/contiguous_count/__init__.py
+++ cute_kernels/kernels/contiguous_count/__init__.py
@@ -15,12 +15,12 @@ def contiguous_count_cute(
     BLOCK_SIZE_B: int = 64,
 ) -> torch.Tensor:
     assert x.dim() == 1, "x should be 1-dimensional"
-    assert x.dtype in [torch.int32, torch.long]
+    assert x.dtype == torch.int32
 
     output = torch.zeros(size, dtype=torch.int32, device=x.device)
 
     if kernel_backend == KernelBackend.cuda:
-        contiguous_count_cuda(x=x.to(torch.int32), output=output, size=size, BLOCK_SIZE_B=BLOCK_SIZE_B)
+        contiguous_count_cuda(x=x, output=output, size=size, BLOCK_SIZE_B=BLOCK_SIZE_B)
     elif kernel_backend == KernelBackend.triton:
         contiguous_count_triton(x=x, output=output, size=size, BLOCK_SIZE_B=BLOCK_SIZE_B)
     else:
