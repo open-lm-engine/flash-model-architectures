14f73fdecfbc81dcf3cd31c2807f6636bea3c9fa
Mayank Mishra <mayank31398@gmail.com>
2024-12-24 20:01:56 -0500

    cuda
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/contiguous_count/cuda_implementation/kernels_forward.cu
+++ cute_kernels/kernels/contiguous_count/cuda_implementation/kernels_forward.cu
@@ -13,7 +13,7 @@ __global__ void _contiguous_count_cuda_kernel(const int32 *x,
                                               int32 *output,
                                               const uint64 num_elements,
                                               const uint32 C) {
-    const uint64 thread_id = get_global_thread_id();
+    const uint64 local_thread_id = get_local_thread_id();
     const int num_loops_C = (C + blockDim.x - 1) / blockDim.x;
 
     extern __shared__ uint32 output_shared[];
@@ -23,10 +23,9 @@ __global__ void _contiguous_count_cuda_kernel(const int32 *x,
     #pragma unroll
     // clang-format on
     for (int i = 0; i < num_loops_C; i++) {
-        const int index = thread_id + i * blockDim.x;
+        const int index = i * blockDim.x + local_thread_id;
         if (index < C) {
             output_shared[index] = 0;
-            output[index] = 0;
         }
     }
 
@@ -34,13 +33,19 @@ __global__ void _contiguous_count_cuda_kernel(const int32 *x,
 
     // count the number of occurances of each number in x
     const int num_elements_per_block = (num_elements + gridDim.x - 1) / gridDim.x;
-    const int num_loops_B = (num_elements_per_block + blockDim.x - 1) / blockDim.x;
 
     const int start = blockIdx.x * num_elements_per_block;
+    int end = start + num_elements_per_block;
+    if (end > num_elements) {
+        end = num_elements;
+    }
+
+    const int num_elements_in_current_block = end - start;
+    const int num_loops_B = (num_elements_in_current_block + blockDim.x - 1) / blockDim.x;
 
     for (int i = 0; i < num_loops_B; i++) {
-        const int index = start + i * num_loops_B + thread_id;
-        if (index < num_elements) {
+        const int index = start + i * num_loops_B + local_thread_id;
+        if (index < end) {
             atomicAdd(&output_shared[x[index]], 1);
         }
     }
@@ -52,7 +57,7 @@ __global__ void _contiguous_count_cuda_kernel(const int32 *x,
     #pragma unroll
     // clang-format on
     for (int i = 0; i < num_loops_C; i++) {
-        const int index = thread_id + i * blockDim.x;
+        const int index = i * blockDim.x + local_thread_id;
         if (index < C) {
             atomicAdd(&output[index], output_shared[index]);
         }
@@ -66,7 +71,7 @@ void contiguous_count_cuda(const torch::Tensor &x, const torch::Tensor &output, 
     const uint64 num_elements = x.numel();
 
     // we use vector instructions of width 4
-    int NUM_BLOCKS = num_elements + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    int NUM_BLOCKS = (num_elements + BLOCK_SIZE - 1) / BLOCK_SIZE;
     const int sm_count = get_sm_count();
     if (NUM_BLOCKS > sm_count) {
         NUM_BLOCKS = sm_count;
