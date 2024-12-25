46f7143ea6084fe9df4eb1dd2fc2316745264f74
Mayank Mishra <mayank31398@gmail.com>
2024-12-24 19:47:25 -0500

    cuda
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/contiguous_count/__init__.py
+++ cute_kernels/kernels/contiguous_count/__init__.py
@@ -17,12 +17,12 @@ def contiguous_count_cute(
     assert x.dim() == 1, "x should be 1-dimensional"
     assert x.dtype in [torch.int32, torch.long]
 
+    output = torch.zeros(size, dtype=torch.int32, device=x.device)
+
     if kernel_backend == KernelBackend.cuda:
-        output = torch.empty(size, device=x.device, dtype=torch.int32)
         contiguous_count_cuda(x=x.to(torch.int32), output=output, size=size, BLOCK_SIZE_B=BLOCK_SIZE_B)
-        output = output.type_as(x)
     elif kernel_backend == KernelBackend.triton:
-        output = contiguous_count_triton(x=x, size=size, BLOCK_SIZE_B=BLOCK_SIZE_B)
+        contiguous_count_triton(x=x, output=output, size=size, BLOCK_SIZE_B=BLOCK_SIZE_B)
     else:
         raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
 
