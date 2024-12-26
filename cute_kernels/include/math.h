a9eb31c8e272d398dc18416c91e71dfce3114716
Mayank Mishra <mayank31398@gmail.com>
2024-12-26 16:43:46 -0500

    chunk
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/include/math.h
+++ cute_kernels/include/math.h
@@ -1,5 +1,5 @@
 template <typename T>
-inline int ceil_divide(const T &x, const T &d) {
+inline T ceil_divide(const T &x, const T &d) {
     assert(x > 0 && d > 0);
     return (x / d) + (x % d != 0);
 }
