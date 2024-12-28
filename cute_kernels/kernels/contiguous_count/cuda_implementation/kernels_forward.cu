1db4d056cdf87b9f984d8d787ecc014b430fa5b6
Mayank Mishra <mayank31398@gmail.com>
2024-12-28 15:51:52 -0500

    functionalize
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/contiguous_count/cuda_implementation/kernels_forward.cu
+++ cute_kernels/kernels/contiguous_count/cuda_implementation/kernels_forward.cu
@@ -77,8 +77,8 @@ __global__ void _contiguous_count_cuda_kernel(const scalar_t *x,
         const uint32 cluster_block_rank = cluster.block_rank();
 
         if (cluster_block_rank != 0) {
-            uint32 *destination_output_shared = cluster.map_shared_rank(output_shared, 0);
-            _looped_atomic_add(output_shared, destination_output_shared, num_loops_C, C, local_thread_id);
+            _looped_atomic_add(
+                output_shared, cluster.map_shared_rank(output_shared, 0), num_loops_C, C, local_thread_id);
         }
 
         cluster.sync();
