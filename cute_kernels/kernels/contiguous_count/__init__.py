1b3061bea55f0faa46e94e02b341d42d185102e8
Mayank Mishra <mayank31398@gmail.com>
2024-12-28 18:27:03 -0500

    functionalize
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/contiguous_count/__init__.py
+++ cute_kernels/kernels/contiguous_count/__init__.py
@@ -10,11 +10,23 @@ from .torch_implementation import contiguous_count_torch
 from .triton_implementation import contiguous_count_triton
 
 
-def _contiguous_count_cute(
+@torch.no_grad()
+@cutotune(
+    configs=(
+        get_cartesian_product_cutotune_configs(
+            kernel_backend=[KernelBackend.cuda, KernelBackend.triton], BLOCK_SIZE=COMMON_CUDA_BLOCK_SIZES_POWERS_OF_2
+        )
+        if torch.cuda.is_available()
+        else []
+    ),
+    default_config=CutoTuneConfig({"kernel_backend": KernelBackend.triton, "BLOCK_SIZE": MAX_CUDA_BLOCK_SIZE}),
+    functional_triggers={"size_next_power_of_2": lambda **kwargs: get_next_power_of_2(kwargs["size"])},
+)
+def contiguous_count_cute(
     x: torch.Tensor,
     size: int,
-    kernel_backend: KernelBackend = KernelBackend.triton,
-    BLOCK_SIZE: int = 64,
+    kernel_backend: KernelBackend = CutoTuneParameter(),
+    BLOCK_SIZE: int = CutoTuneParameter(),
 ) -> torch.Tensor:
     assert x.dim() == 1, "x should be 1-dimensional"
     assert x.dtype in [torch.int32, torch.long]
@@ -36,24 +48,3 @@ def _contiguous_count_cute(
         raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
 
     return output
-
-
-@torch.no_grad()
-@cutotune(
-    configs=(
-        get_cartesian_product_cutotune_configs(
-            kernel_backend=[KernelBackend.cuda, KernelBackend.triton], BLOCK_SIZE=COMMON_CUDA_BLOCK_SIZES_POWERS_OF_2
-        )
-        if torch.cuda.is_available()
-        else []
-    ),
-    default_config=CutoTuneConfig({"kernel_backend": KernelBackend.triton, "BLOCK_SIZE": MAX_CUDA_BLOCK_SIZE}),
-    functional_triggers={"size_next_power_of_2": lambda **kwargs: get_next_power_of_2(kwargs["size"])},
-)
-def contiguous_count_cute(
-    x: torch.Tensor,
-    size: int,
-    kernel_backend: KernelBackend = CutoTuneParameter(),
-    BLOCK_SIZE: int = CutoTuneParameter(),
-) -> torch.Tensor:
-    return _contiguous_count_cute(x=x, size=size, kernel_backend=kernel_backend, BLOCK_SIZE=BLOCK_SIZE)
