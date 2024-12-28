29210eaf7d88332ab9275aba24b8a3ef511a4155
Mayank Mishra <mayank31398@gmail.com>
2024-12-28 18:11:34 -0500

    functionalize
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/contiguous_count/__init__.py
+++ cute_kernels/kernels/contiguous_count/__init__.py
@@ -42,13 +42,10 @@ def _contiguous_count_cute(
 @cutotune(
     configs=(
         get_cartesian_product_cutotune_configs(
-            kernel_backend=[KernelBackend.cuda], BLOCK_SIZE=COMMON_CUDA_BLOCK_SIZES_POWERS_OF_2
+            kernel_backend=[KernelBackend.cuda, KernelBackend.triton], BLOCK_SIZE=COMMON_CUDA_BLOCK_SIZES_POWERS_OF_2
         )
         if torch.cuda.is_available()
         else []
-    )
-    + get_cartesian_product_cutotune_configs(
-        kernel_backend=[KernelBackend.triton], BLOCK_SIZE=COMMON_CUDA_BLOCK_SIZES_POWERS_OF_2
     ),
     default_config=CutoTuneConfig({"kernel_backend": KernelBackend.triton, "BLOCK_SIZE": MAX_CUDA_BLOCK_SIZE}),
     functional_triggers={"size_next_power_of_2": lambda **kwargs: get_next_power_of_2(kwargs["size"])},
