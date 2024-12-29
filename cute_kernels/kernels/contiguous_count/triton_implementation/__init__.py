f9b04707f82fe7d199e9c8379e90d91cf78c1f9a
Mayank Mishra <mayank31398@gmail.com>
2024-12-28 22:55:29 -0500

    functionalize
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/contiguous_count/triton_implementation/__init__.py
+++ cute_kernels/kernels/contiguous_count/triton_implementation/__init__.py
@@ -1,7 +1,6 @@
 import torch
 
-from ....constants import COMMON_CUDA_BLOCK_SIZES_POWERS_OF_2, LIBRARY_NAME, MAX_CUDA_BLOCK_SIZE, MAX_TRITON_BLOCK_SIZE
-from ....cutotune import CutoTuneConfig, cutotune, get_cartesian_product_cutotune_configs
+from ....constants import LIBRARY_NAME
 from ....math import ceil_divide
 from ....utils import cute_op, get_sm_count
 from .kernels_forward import _contiguous_count_triton_kernel
@@ -10,14 +9,6 @@ from .kernels_forward import _contiguous_count_triton_kernel
 _KERNEL_NAME = "contiguous_count_triton"
 
 
-@cutotune(
-    configs=get_cartesian_product_cutotune_configs(
-        BLOCK_SIZE=COMMON_CUDA_BLOCK_SIZES_POWERS_OF_2,
-        condition=lambda **kwargs: 1024 <= kwargs["BLOCK_SIZE"] * kwargs["BLOCK_SIZE_C"] <= MAX_TRITON_BLOCK_SIZE,
-    ),
-    default_config=CutoTuneConfig({"BLOCK_SIZE": MAX_CUDA_BLOCK_SIZE}),
-    triggers={"BLOCK_SIZE_C"},
-)
 @cute_op(f"{LIBRARY_NAME}::{_KERNEL_NAME}", mutates_args={"output"})
 def contiguous_count_triton(
     x: torch.Tensor, output: torch.Tensor, size: int, BLOCK_SIZE: int, BLOCK_SIZE_C: int
