e5a87a9e5682c28358738b8122a7473f39207958
Mayank Mishra <mayank31398@gmail.com>
2025-01-02 18:25:17 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/contiguous_count/triton_implementation/__init__.py
+++ cute_kernels/kernels/contiguous_count/triton_implementation/__init__.py
@@ -1,11 +1,16 @@
 import torch
 
-from ....cutotune import cutotune
+from ....cutotune import cutotune, get_cartesian_product_cutotune_configs
 from ....math import get_next_power_of_2
 from .forward import contiguous_count_high_atomic_add_triton, contiguous_count_low_atomic_add_triton
 
 
-@cutotune()
+@cutotune(
+    configs=get_cartesian_product_cutotune_configs(
+        use_low_atomic_add=[False, True], condition=lambda **kwargs: kwargs["size"] <= 16384
+    )
+    + get_cartesian_product_cutotune_configs(use_low_atomic_add=[False])
+)
 def contiguous_count_triton(
     x: torch.Tensor, output: torch.Tensor, size: int, BLOCK_SIZE: int, use_low_atomic_add: bool
 ) -> None:
