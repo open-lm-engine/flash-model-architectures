fa39bf13fd5eb83569f46105fbfc69fb825777b8
Mayank Mishra <mayank31398@gmail.com>
2025-01-02 18:44:59 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/contiguous_count/__init__.py
+++ cute_kernels/kernels/contiguous_count/__init__.py
@@ -1,5 +1,7 @@
 import torch
 
+from ...constants import COMMON_CUDA_BLOCK_SIZES_POWERS_OF_2
+from ...cutotune import CutoTuneParameter, cutotune, get_cartesian_product_cutotune_configs
 from ...enums import KernelBackend
 from ...utils import get_sm_count
 from .cuda_implementation import contiguous_count_cuda
@@ -8,11 +10,21 @@ from .triton_implementation import contiguous_count_triton
 
 
 @torch.no_grad()
-def contiguous_count_cute(
+@cutotune(
+    get_cartesian_product_cutotune_configs(
+        kernel_backend=KernelBackend.triton,
+        BLOCK_SIZE=COMMON_CUDA_BLOCK_SIZES_POWERS_OF_2,
+        condition=lambda **kwargs: kwargs["size"] <= 16384,
+    )
+    + get_cartesian_product_cutotune_configs(
+        kernel_backend=KernelBackend.cuda, BLOCK_SIZE=COMMON_CUDA_BLOCK_SIZES_POWERS_OF_2
+    )
+)
+def _contiguous_count_cute(
     x: torch.Tensor,
     size: int,
-    kernel_backend: KernelBackend = KernelBackend.triton,
-    BLOCK_SIZE: int = 64,
+    kernel_backend: KernelBackend,
+    BLOCK_SIZE: int,
 ) -> torch.Tensor:
     assert x.dim() == 1, "x should be 1-dimensional"
     assert x.dtype in [torch.int32, torch.long]
@@ -34,3 +46,12 @@ def contiguous_count_cute(
         raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
 
     return output
+
+
+def contiguous_count_cute(
+    x: torch.Tensor,
+    size: int,
+    kernel_backend: KernelBackend = CutoTuneParameter(),
+    BLOCK_SIZE: int = CutoTuneParameter(),
+) -> torch.Tensor:
+    return _contiguous_count_cute(x=x, size=size, kernel_backend=kernel_backend, BLOCK_SIZE=BLOCK_SIZE)
--- cute_kernels/kernels/contiguous_count/triton_implementation/forward/__init__.py
+++
@@ -1,2 +0,0 @@
-from .high_atomic_add import contiguous_count_high_atomic_add_triton
-from .low_atomic_add import contiguous_count_low_atomic_add_triton
