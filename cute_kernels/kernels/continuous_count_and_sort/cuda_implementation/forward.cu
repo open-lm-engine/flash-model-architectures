766222a21de7ec03fd0d61c5868545048e9463bf
Mayank Mishra <mayank31398@gmail.com>
2025-01-05 14:13:57 +0530

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/continuous_count_and_sort/cuda_implementation/forward.cu
+++ cute_kernels/kernels/continuous_count_and_sort/cuda_implementation/forward.cu
@@ -27,10 +27,10 @@ inline __device__ void _looped_atomic_add(uint32 *output_shared,
 }
 
 template <typename scalar_t>
-__global__ void _continuous_count_and_sort_cuda_kernel(const scalar_t *x,
-                                                       uint32 *count,
-                                                       const uint64 num_elements,
-                                                       const uint32 C) {
+__global__ void _continuous_count_cuda_kernel(const scalar_t *x,
+                                              uint32 *output,
+                                              const uint64 num_elements,
+                                              const uint32 C) {
     const uint32 local_thread_id = get_local_thread_id();
     const uint32 num_loops_C = ceil_divide<uint32>(C, blockDim.x);
 
@@ -39,7 +39,7 @@ __global__ void _continuous_count_and_sort_cuda_kernel(const scalar_t *x,
     for (uint32 i = 0; i < num_loops_C; i++) {
         const uint32 index = i * blockDim.x + local_thread_id;
         if (index < C) {
-            count[index] = 0;
+            output[index] = 0;
             output_shared[index] = 0;
         }
     }
@@ -57,6 +57,9 @@ __global__ void _continuous_count_and_sort_cuda_kernel(const scalar_t *x,
 
     const int num_elements_in_current_block = end - start;
 
+    cg::cluster_group cluster = cg::this_cluster();
+    const bool is_first_cluster_block = cluster.block_rank() == 0;
+
     if (num_elements_in_current_block > 0) {
         const uint32 num_loops = ceil_divide<uint32>(num_elements_in_current_block, blockDim.x);
 
@@ -69,32 +72,72 @@ __global__ void _continuous_count_and_sort_cuda_kernel(const scalar_t *x,
 
         __syncthreads();
 
-        for (int i = 0; i < num_loops_C; i++) {
-            const int index = i * blockDim.x + local_thread_id;
-            if (index < C) {
-                atomicAdd(&count[index], output_shared[index]);
-            }
+        if (!is_first_cluster_block) {
+            _looped_atomic_add(
+                output_shared, cluster.map_shared_rank(output_shared, 0), num_loops_C, C, local_thread_id);
         }
+    }
+
+    cluster.sync();
+
+    // write the output to the global memory
+    if (is_first_cluster_block && num_elements_in_current_block > 0) {
+        _looped_atomic_add(output_shared, output, num_loops_C, C, local_thread_id);
     }
 }
 
-void continuous_count_and_sort_cuda(
-    const torch::Tensor &x, torch::Tensor &count, const uint32 &sm_count, const uint32 &C, const uint32 &BLOCK_SIZE) {
+void continuous_count_and_sort_cuda(const torch::Tensor &x,
+                                    torch::Tensor &output,
+                                    const uint32 &sm_count,
+                                    const uint32 &thread_block_cluster_size,
+                                    const uint32 &C,
+                                    const uint32 &BLOCK_SIZE) {
     assert(BLOCK_SIZE % WARP_SIZE == 0);
     assert(C <= MAX_ALLOWED_C);
 
-    const uint64 num_elements = x.numel();
-    assert(num_elements <= std::numeric_limits<uint32>::max());
+    const uint64 total_elements = x.numel();
+    const int max_num_blocks = get_max_thread_blocks(sm_count, thread_block_cluster_size);
 
-    const uint32 NUM_BLOCKS = ceil_divide<uint32>(num_elements, BLOCK_SIZE);
+    std::vector<ChunkedArray<uint32>> output_chunks = chunk_array<uint32>(output.data_ptr<uint32>(), total_elements);
 
-    AT_DISPATCH_CUSTOM_INT_TYPES(x.scalar_type(), "continuous_count_and_sort_cuda_kernel", ([&] {
-                                     cudaFuncSetAttribute(_continuous_count_and_sort_cuda_kernel<scalar_t>,
+    AT_DISPATCH_CUSTOM_INT_TYPES(x.scalar_type(), "continuous_count_cuda_kernel", ([&] {
+                                     cudaFuncSetAttribute(_continuous_count_cuda_kernel<scalar_t>,
                                                           cudaFuncAttributeMaxDynamicSharedMemorySize,
                                                           MAX_ALLOWED_C * sizeof(uint32));
 
-                                     _continuous_count_and_sort_cuda_kernel<scalar_t>
-                                         <<<NUM_BLOCKS, BLOCK_SIZE, C * sizeof(uint32)>>>(
-                                             x.data_ptr<scalar_t>(), count.data_ptr<uint32>(), num_elements, C);
+                                     std::vector<ChunkedArray<scalar_t>> x_chunks =
+                                         chunk_array<scalar_t>(x.data_ptr<scalar_t>(), total_elements);
+
+                                     for (int i = 0; i < x_chunks.size(); i++) {
+                                         ChunkedArray<scalar_t> x_chunk = x_chunks[i];
+                                         ChunkedArray<uint32> output_chunk = output_chunks[i];
+
+                                         const uint32 num_elements = x_chunk.num_elements;
+
+                                         auto [NUM_BLOCKS, cluster_size] = get_num_blocks(
+                                             num_elements, BLOCK_SIZE, max_num_blocks, thread_block_cluster_size);
+
+                                         // dynamically sized clusters need this stupid way of launching the kernel
+                                         cudaLaunchConfig_t launch_config = {0};
+                                         launch_config.blockDim = BLOCK_SIZE;
+                                         launch_config.gridDim = NUM_BLOCKS;
+                                         launch_config.dynamicSmemBytes = C * sizeof(uint32);
+
+                                         cudaLaunchAttribute attributes[1];
+                                         attributes[0].id = cudaLaunchAttributeClusterDimension;
+                                         attributes[0].val.clusterDim.x = cluster_size;
+                                         attributes[0].val.clusterDim.y = 1;
+                                         attributes[0].val.clusterDim.z = 1;
+
+                                         launch_config.attrs = attributes;
+                                         launch_config.numAttrs = 1;
+
+                                         cudaLaunchKernelEx(&launch_config,
+                                                            _continuous_count_cuda_kernel<scalar_t>,
+                                                            x_chunk.array,
+                                                            output_chunk.array,
+                                                            num_elements,
+                                                            C);
+                                     }
                                  }));
 }
--- cute_kernels/kernels/continuous_count_and_sort/cuda_implementation/forward.py
+++ cute_kernels/kernels/continuous_count_and_sort/cuda_implementation/forward.py
@@ -8,8 +8,8 @@ from ....utils import cute_op
 _KERNEL_NAME = "continuous_count_and_sort_cuda"
 
 
-@cute_op(f"{LIBRARY_NAME}::{_KERNEL_NAME}", mutates_args={"count"})
+@cute_op(f"{LIBRARY_NAME}::{_KERNEL_NAME}", mutates_args={"output"})
 @cpp_jit(_KERNEL_NAME)
 def continuous_count_and_sort_cuda(
-    x: torch.Tensor, count: torch.Tensor, sm_count: int, size: int, BLOCK_SIZE: int
+    x: torch.Tensor, output: torch.Tensor, sm_count: int, thread_block_cluster_size: int, size: int, BLOCK_SIZE: int
 ) -> None: ...
