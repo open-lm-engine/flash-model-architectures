32c97a377defa76c95d7a516ad295727309b398f
Mayank Mishra <mayank31398@gmail.com>
2025-01-05 10:01:13 +0530

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/continuous_count/cuda_implementation/ops.cpp
+++ cute_kernels/kernels/continuous_count/cuda_implementation/ops.cpp
@@ -8,5 +8,5 @@ void continuous_count_cuda(const torch::Tensor &x,
                            const uint &BLOCK_SIZE);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("continuous_count_cuda", &continuous_count_cuda, "contiguous count forward (CUDA)");
+    m.def("continuous_count_cuda", &continuous_count_cuda, "contiguous count (CUDA)");
 }
--- cute_kernels/kernels/continuous_count_and_sort/cuda_implementation/ops.cpp
+++ cute_kernels/kernels/continuous_count_and_sort/cuda_implementation/ops.cpp
@@ -4,5 +4,5 @@ void continuous_count_and_sort_cuda(
     const torch::Tensor &x, torch::Tensor &count, const uint &sm_count, const uint &C, const uint &BLOCK_SIZE);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("continuous_count_cuda", &continuous_count_cuda, "contiguous count forward (CUDA)");
+    m.def("continuous_count_and_sort_cuda", &continuous_count_and_sort_cuda, "contiguous count and sort (CUDA)");
 }
