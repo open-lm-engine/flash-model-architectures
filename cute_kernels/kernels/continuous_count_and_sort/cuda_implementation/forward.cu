5f4de024e87843aaa75f1d35bbd53ccda520db0d
Mayank Mishra <mayank31398@gmail.com>
2025-01-05 10:17:31 +0530

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/continuous_count_and_sort/cuda_implementation/forward.cu
+++ cute_kernels/kernels/continuous_count_and_sort/cuda_implementation/forward.cu
@@ -44,38 +44,38 @@ __global__ void _continuous_count_and_sort_cuda_kernel(const scalar_t *x,
         }
     }
 
-    __syncthreads();
+    // __syncthreads();
 
-    // count the number of occurances of each number in x
-    const uint32 num_elements_per_block = ceil_divide<uint64>(num_elements, gridDim.x);
+    // // count the number of occurances of each number in x
+    // const uint32 num_elements_per_block = ceil_divide<uint64>(num_elements, gridDim.x);
 
-    const uint32 start = blockIdx.x * num_elements_per_block;
-    uint64 end = start + num_elements_per_block;
-    if (end > num_elements) {
-        end = num_elements;
-    }
+    // const uint32 start = blockIdx.x * num_elements_per_block;
+    // uint64 end = start + num_elements_per_block;
+    // if (end > num_elements) {
+    //     end = num_elements;
+    // }
 
-    const int num_elements_in_current_block = end - start;
+    // const int num_elements_in_current_block = end - start;
 
-    if (num_elements_in_current_block > 0) {
-        const uint32 num_loops = ceil_divide<uint32>(num_elements_in_current_block, blockDim.x);
+    // if (num_elements_in_current_block > 0) {
+    //     const uint32 num_loops = ceil_divide<uint32>(num_elements_in_current_block, blockDim.x);
 
-        for (int i = 0; i < num_loops; i++) {
-            const int index = start + i * blockDim.x + local_thread_id;
-            if (index < end) {
-                atomicAdd(&output_shared[x[index]], 1);
-            }
-        }
+    //     for (int i = 0; i < num_loops; i++) {
+    //         const int index = start + i * blockDim.x + local_thread_id;
+    //         if (index < end) {
+    //             atomicAdd(&output_shared[x[index]], 1);
+    //         }
+    //     }
 
-        __syncthreads();
+    //     __syncthreads();
 
-        for (int i = 0; i < num_loops_C; i++) {
-            const int index = i * blockDim.x + local_thread_id;
-            if (index < C) {
-                atomicAdd(&count[index], output_shared[index]);
-            }
-        }
-    }
+    //     for (int i = 0; i < num_loops_C; i++) {
+    //         const int index = i * blockDim.x + local_thread_id;
+    //         if (index < C) {
+    //             atomicAdd(&count[index], output_shared[index]);
+    //         }
+    //     }
+    // }
 }
 
 void continuous_count_and_sort_cuda(
@@ -83,28 +83,17 @@ void continuous_count_and_sort_cuda(
     assert(BLOCK_SIZE % WARP_SIZE == 0);
     assert(C <= MAX_ALLOWED_C);
 
-    const uint64 total_elements = x.numel();
+    const uint64 num_elements = x.numel();
+    assert(num_elements <= std::numeric_limits<uint32>::max());
 
-    std::vector<ChunkedArray<uint32>> count_chunks = chunk_array<uint32>(count.data_ptr<uint32>(), total_elements);
+    const uint32 NUM_BLOCKS = ceil_divide<uint32>(num_elements, BLOCK_SIZE);
 
     AT_DISPATCH_CUSTOM_INT_TYPES(x.scalar_type(), "continuous_count_and_sort_cuda_kernel", ([&] {
                                      cudaFuncSetAttribute(_continuous_count_and_sort_cuda_kernel<scalar_t>,
                                                           cudaFuncAttributeMaxDynamicSharedMemorySize,
                                                           MAX_ALLOWED_C * sizeof(uint32));
 
-                                     std::vector<ChunkedArray<scalar_t>> x_chunks =
-                                         chunk_array<scalar_t>(x.data_ptr<scalar_t>(), total_elements);
-
-                                     for (int i = 0; i < x_chunks.size(); i++) {
-                                         ChunkedArray<scalar_t> x_chunk = x_chunks[i];
-                                         ChunkedArray<uint32> count_chunk = count_chunks[i];
-
-                                         const uint32 num_elements = x_chunk.num_elements;
-
-                                         const uint32 NUM_BLOCKS = ceil_divide<uint32>(num_elements, BLOCK_SIZE);
-
-                                         _continuous_count_and_sort_cuda_kernel<scalar_t><<<NUM_BLOCKS, BLOCK_SIZE>>>(
-                                             x_chunk.array, count_chunk.array, num_elements, C);
-                                     }
+                                     _continuous_count_and_sort_cuda_kernel<scalar_t><<<NUM_BLOCKS, BLOCK_SIZE>>>(
+                                         x.data_ptr<uint32>(), count.data_ptr<uint32>(), num_elements, C);
                                  }));
 }
