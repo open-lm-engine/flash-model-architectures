2a2fda84578c6205d3acae024c8a79d01b2dfb98
Mayank Mishra <mayank31398@gmail.com>
2025-01-10 19:19:18 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/include/dtypes/uint32.h
+++ cute_kernels/include/dtypes/uint32.h
@@ -13,5 +13,10 @@ struct DType<uint32> {
     using nv_dtype4 = uint32_4;
 
     inline __device__ static nv_dtype2 make2(const nv_dtype &x, const nv_dtype &y) { return make_uint2(x, y); }
-    inline __device__ static nv_dtype4 make4(const nv_dtype &x, const nv_dtype &y, const nv_dtype &z, const nv_dtype &t) { return make_uint4(x, y, z, t); }
+    inline __device__ static nv_dtype4 make4(const nv_dtype &x,
+                                             const nv_dtype &y,
+                                             const nv_dtype &z,
+                                             const nv_dtype &t) {
+        return make_uint4(x, y, z, t);
+    }
 };
