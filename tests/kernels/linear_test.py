ad84600518d36e66a545848bbe1d4eca56c91444
Mayank Mishra <mayank31398@gmail.com>
2025-01-13 22:20:03 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/kernels/linear_test.py
+++ tests/kernels/linear_test.py
@@ -33,21 +33,23 @@ class LinearTest(TestCommons):
     ) -> None:
         set_seed(_SEED)
 
-        input_kernel, input_expected = self.get_random_duplicated_tensors(size, device=device, dtype=dtype)
-        weight_kernel, weight_expected = self.get_random_duplicated_tensors(size[-1], device=device, dtype=dtype)
+        input_kernel, input_expected = self.get_random_duplicated_tensors((400, size[-1]), device=device, dtype=dtype)
+        weight_kernel, weight_expected = self.get_random_duplicated_tensors(size, device=device, dtype=dtype)
 
+        bias_kernel = None
+        bias_expected = None
         if has_bias:
-            bias_kernel = self.get_random_duplicated_tensors(size[-1], device=device, dtype=dtype)
-            bias_expected = self.get_random_duplicated_tensors(size[-1], device=device, dtype=dtype)
+            bias_kernel, bias_expected = self.get_random_duplicated_tensors(size[0], device=device, dtype=dtype)
 
-        z_kernel = function(x=x_kernel, weight=weight_kernel, eps=_EPSILON, memory_efficient=memory_efficient)
-        z_expected = linear_torch(input=input, weight=weight, bias=bias)
+        z_kernel = function(input=input_kernel, weight=weight_kernel, bias=bias_kernel)
+        z_expected = linear_torch(input=input_expected, weight=weight_expected, bias=bias_expected)
 
         z_kernel.sum().backward()
         z_expected.sum().backward()
 
         self.assert_equal_tensors(z_kernel, z_expected, False, atol_float16=8e-3, rtol_float16=0)
-        self.assert_equal_tensors(x_kernel.grad, x_expected.grad, False, atol_float16=9e-2, rtol_float16=0)
+        self.assert_equal_tensors(input_kernel.grad, input_expected.grad, False, atol_float16=9e-2, rtol_float16=0)
+        self.assert_equal_tensors(weight_kernel.grad, weight_expected.grad, False, atol_float16=9e-2, rtol_float16=0)
 
         if has_bias:
             self.assert_equal_tensors(
