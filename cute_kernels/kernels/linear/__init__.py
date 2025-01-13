ef0891c699dda86c6cf1bcce8d201109fe87247e
Mayank Mishra <mayank31398@gmail.com>
2025-01-13 22:24:52 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/linear/__init__.py
+++ cute_kernels/kernels/linear/__init__.py
@@ -6,11 +6,17 @@ from .torch_implementation import linear_torch
 class _Linear_Cute(torch.autograd.Function):
     @staticmethod
     def forward(ctx, input: torch.Tensor, weight: torch.Tensor, bias: torch.Tensor | None = None) -> torch.Tensor:
+        ctx.save_for_backward(input, weight, bias)
         output = torch.empty(*input.size()[:-1], weight.size(0), dtype=input.dtype, device=input.device)
 
         # TODO
 
         return output
+
+    @staticmethod
+    def backward(ctx, output_grad: torch.Tensor) -> torch.Tensor:
+        input, weight, bias = ctx.saved_tensors
+        return input, weight, bias
 
 
 def linear_cute(input: torch.Tensor, weight: torch.Tensor, bias: torch.Tensor | None = None) -> torch.Tensor:
