0b1889bb8c37a522b13b59ebdfa8628a966481e7
Mayank Mishra <mayank31398@gmail.com>
2025-01-18 03:51:43 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/linear/__init__.py
+++ cute_kernels/kernels/linear/__init__.py
@@ -1,7 +1,9 @@
 import torch
 
 from ...cutotune import CutoTuneParameter
+from ...enums import KernelBackend
 from ...utils import ensure_contiguous
+from ..gemm import gemm_cute
 from .torch_implementation import linear_torch
 from .triton_implementation import linear_backward_triton, linear_forward_triton
 
@@ -15,9 +17,11 @@ class _Linear_Cute(torch.autograd.Function):
         weight: torch.Tensor,
         bias: torch.Tensor | None,
         use_tf32: bool,
+        kernel_backend_forward: KernelBackend,
         BLOCK_SIZE_M_forward: int,
         BLOCK_SIZE_K_forward: int,
         BLOCK_SIZE_N_forward: int,
+        kernel_backend_backward: KernelBackend,
         BLOCK_SIZE_M_backward: int,
         BLOCK_SIZE_K_backward: int,
         BLOCK_SIZE_N_backward: int,
@@ -25,24 +29,28 @@ class _Linear_Cute(torch.autograd.Function):
         ctx.save_for_backward(input, weight)
         ctx.has_bias = bias is not None
         ctx.use_tf32 = use_tf32
+        ctx.kernel_backend_backward = kernel_backend_backward
         ctx.BLOCK_SIZE_M_backward = BLOCK_SIZE_M_backward
         ctx.BLOCK_SIZE_K_backward = BLOCK_SIZE_K_backward
         ctx.BLOCK_SIZE_N_backward = BLOCK_SIZE_N_backward
 
         output = torch.empty(*input.size()[:-1], weight.size(0), dtype=input.dtype, device=input.device)
 
-        linear_forward_triton(
-            input=input,
-            weight=weight,
-            bias=bias,
-            output=output,
-            use_tf32=use_tf32,
-            BLOCK_SIZE_M=BLOCK_SIZE_M_forward,
-            BLOCK_SIZE_K=BLOCK_SIZE_K_forward,
-            BLOCK_SIZE_N=BLOCK_SIZE_N_forward,
-            num_warps=CutoTuneParameter(),
-            num_stages=CutoTuneParameter(),
-        )
+        if kernel_backend_forward == KernelBackend.triton:
+            linear_forward_triton(
+                input=input,
+                weight=weight,
+                bias=bias,
+                output=output,
+                use_tf32=use_tf32,
+                BLOCK_SIZE_M=BLOCK_SIZE_M_forward,
+                BLOCK_SIZE_K=BLOCK_SIZE_K_forward,
+                BLOCK_SIZE_N=BLOCK_SIZE_N_forward,
+                num_warps=CutoTuneParameter(),
+                num_stages=CutoTuneParameter(),
+            )
+        else:
+            raise ValueError(f"unexpected kernel_backend_forward ({kernel_backend_forward})")
 
         return output
 
@@ -51,29 +59,46 @@ class _Linear_Cute(torch.autograd.Function):
     def backward(ctx, output_grad: torch.Tensor) -> torch.Tensor:
         input, weight = ctx.saved_tensors
 
-        input_grad = torch.empty_like(input)
-        weight_grad = torch.empty_like(weight)
-        bias_grad = None
-        if ctx.has_bias:
-            bias_grad = torch.empty(weight.size(0), device=weight.device, dtype=weight.dtype)
+        use_tf32 = ctx.use_tf32
+        kernel_backend_backward = ctx.kernel_backend_backward
+        BLOCK_SIZE_M_backward = ctx.BLOCK_SIZE_M_backward
+        BLOCK_SIZE_K_backward = ctx.BLOCK_SIZE_K_backward
+        BLOCK_SIZE_N_backward = ctx.BLOCK_SIZE_N_backward
 
-        # input_grad = output_grad @ weight
-        # weight_grad = output_grad.T @ input
-        # if bias_grad is not None:
-        #     bias_grad = output_grad.sum(dim=0)
+        if kernel_backend_backward == KernelBackend.triton:
+            input_grad = gemm_cute(
+                a=output_grad,
+                b=weight,
+                use_tf32=use_tf32,
+                BLOCK_SIZE_M=BLOCK_SIZE_M_backward,
+                BLOCK_SIZE_K=BLOCK_SIZE_K_backward,
+                BLOCK_SIZE_N=BLOCK_SIZE_N_backward,
+            )
 
-        linear_backward_triton(
-            input=input,
-            output_grad=output_grad,
-            weight_grad=weight_grad,
-            bias_grad=bias_grad,
-            use_tf32=ctx.use_tf32,
-            BLOCK_SIZE_M=ctx.BLOCK_SIZE_M_backward,
-            BLOCK_SIZE_K=ctx.BLOCK_SIZE_K_backward,
-            BLOCK_SIZE_N=ctx.BLOCK_SIZE_N_backward,
-            num_warps=CutoTuneParameter(),
-            num_stages=CutoTuneParameter(),
-        )
+            weight_grad = torch.empty_like(weight)
+            bias_grad = None
+            if ctx.has_bias:
+                bias_grad = torch.empty(weight.size(0), device=weight.device, dtype=weight.dtype)
+
+            # input_grad = output_grad @ weight
+            # weight_grad = output_grad.T @ input
+            # if bias_grad is not None:
+            #     bias_grad = output_grad.sum(dim=0)
+
+            linear_backward_triton(
+                input=input,
+                output_grad=output_grad,
+                weight_grad=weight_grad,
+                bias_grad=bias_grad,
+                use_tf32=ctx.use_tf32,
+                BLOCK_SIZE_M=ctx.BLOCK_SIZE_M_backward,
+                BLOCK_SIZE_K=ctx.BLOCK_SIZE_K_backward,
+                BLOCK_SIZE_N=ctx.BLOCK_SIZE_N_backward,
+                num_warps=CutoTuneParameter(),
+                num_stages=CutoTuneParameter(),
+            )
+        else:
+            raise ValueError(f"unexpected kernel_backend_backward ({kernel_backend_backward})")
 
         return input_grad, weight_grad, bias_grad, *[None] * 7
 
@@ -83,9 +108,11 @@ def linear_cute(
     weight: torch.Tensor,
     bias: torch.Tensor | None = None,
     use_tf32: bool = True,
+    kernel_backend_forward: KernelBackend = KernelBackend.triton,
     BLOCK_SIZE_M_forward: int = CutoTuneParameter(),
     BLOCK_SIZE_K_forward: int = CutoTuneParameter(),
     BLOCK_SIZE_N_forward: int = CutoTuneParameter(),
+    kernel_backend_backward: KernelBackend = KernelBackend.triton,
     BLOCK_SIZE_M_backward: int = CutoTuneParameter(),
     BLOCK_SIZE_K_backward: int = CutoTuneParameter(),
     BLOCK_SIZE_N_backward: int = CutoTuneParameter(),
