c034de851a8dbe6e5c88af473a60ca210fc33c26
Mayank Mishra <mayank31398@gmail.com>
2025-01-17 20:47:27 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/linear/triton_implementation/backward.py
+++ cute_kernels/kernels/linear/triton_implementation/backward.py
@@ -45,7 +45,7 @@ def _linear_backward_triton_kernel(
     accumulator_weight_grad = tl.zeros((BLOCK_SIZE_N, BLOCK_SIZE_K), dtype=tl.float32)
 
     if bias_grad_ptr is not None and pid_k == 0:
-        accumulator_bias_grad = tl.zeros(BLOCK_SIZE_N, dtype=tl.float32)
+        accumulator_bias_grad = tl.zeros((BLOCK_SIZE_N,), dtype=tl.float32)
 
     for m in range(tl.cdiv(M, BLOCK_SIZE_M)):
         indices_m = m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
