212b746ddc7d485999dadd5c069429b8c4e93b20
Mayank Mishra <mayank31398@gmail.com>
2025-01-21 15:16:27 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/gemm/cuda_implementation/naive.cu
+++ cute_kernels/kernels/gemm/cuda_implementation/naive.cu
@@ -54,7 +54,6 @@ void naive_gemm_cuda(const torch::Tensor &a,
                      const uint32 &BLOCK_SIZE_M,
                      const uint32 &BLOCK_SIZE_N) {
     TORCH_CHECK((BLOCK_SIZE_M * BLOCK_SIZE_N) % WARP_SIZE == 0);
-    TORCH_CHECK(BLOCK_SIZE_N % WARP_SIZE == 0);
 
     AT_DISPATCH_CUSTOM_FLOAT_TYPES(
         a.scalar_type(), "naive_gemm_cuda_kernel", ([&] {
