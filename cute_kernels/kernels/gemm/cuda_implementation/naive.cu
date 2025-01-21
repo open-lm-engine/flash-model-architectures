8c1d2d31ad378d227b5adc7b59edbffba757a5b5
Mayank Mishra <mayank31398@gmail.com>
2025-01-21 15:02:24 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/gemm/cuda_implementation/naive.cu
+++ cute_kernels/kernels/gemm/cuda_implementation/naive.cu
@@ -7,9 +7,15 @@
 #include "../../../include/threads.h"
 #include "index.h"
 
-template <typename scalar_t, bool is_a_transposed, bool is_b_transposed>
-__global__ void _naive_gemm_cuda_kernel(
-    const scalar_t *a, const scalar_t *b, scalar_t *c, const uint32 M, const uint32 K, const uint32 N) {
+template <typename scalar_t>
+__global__ void _naive_gemm_cuda_kernel(const scalar_t *a,
+                                        const scalar_t *b,
+                                        scalar_t *c,
+                                        const bool is_a_transposed,
+                                        const bool is_b_transposed,
+                                        const uint32 M,
+                                        const uint32 K,
+                                        const uint32 N) {
     const uint32 i = blockDim.y * blockIdx.y + threadIdx.y;
     const uint32 j = blockDim.x * blockIdx.x + threadIdx.x;
 
@@ -55,7 +61,13 @@ void naive_gemm_cuda(const torch::Tensor &a,
             dim3 NUM_BLOCKS = dim3(ceil_divide<uint32>(M, BLOCK_SIZE_M), ceil_divide<uint32>(N, BLOCK_SIZE_N), 1);
             dim3 BLOCK_SIZE = dim3(BLOCK_SIZE_M, BLOCK_SIZE_N, 1);
 
-            _naive_gemm_cuda_kernel<scalar_t, is_a_transposed, is_b_transposed><<<NUM_BLOCKS, BLOCK_SIZE>>>(
-                a.data_ptr<scalar_t>(), b.data_ptr<scalar_t>(), c.data_ptr<scalar_t>(), M, K, N);
+            _naive_gemm_cuda_kernel<scalar_t><<<NUM_BLOCKS, BLOCK_SIZE>>>(a.data_ptr<scalar_t>(),
+                                                                          b.data_ptr<scalar_t>(),
+                                                                          c.data_ptr<scalar_t>(),
+                                                                          is_a_transposed,
+                                                                          is_b_transposed,
+                                                                          M,
+                                                                          K,
+                                                                          N);
         }));
 }
