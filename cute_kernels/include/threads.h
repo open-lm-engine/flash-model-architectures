f0ced0bd51a3b4a894941e41186436ca438181cd
Mayank Mishra <mayank31398@gmail.com>
2025-01-21 18:17:47 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/include/threads.h
+++ cute_kernels/include/threads.h
@@ -9,16 +9,20 @@
 #include "dtypes/all.h"
 #include "math.h"
 
+inline __device__ uint32 get_threads_per_block() { return blockDim.x * blockDim.y * blockDim.z; }
+
 inline __device__ uint32 get_num_blocks() { return gridDim.x * gridDim.y * gridDim.z; }
+
+inline __device__ uint32 get_block_id() {
+    return gridDim.x * gridDim.y * blockIdx.z + gridDim.x * blockIdx.y + blockIdx.x;
+}
 
 inline __device__ uint32 get_local_thread_id() {
     return blockDim.x * blockDim.y * threadIdx.z + blockDim.x * threadIdx.y + threadIdx.x;
 }
 
 inline __device__ uint64 get_global_thread_id() {
-    return blockDim.x * blockDim.y * blockDim.z *
-               (gridDim.x * gridDim.y * blockIdx.z + gridDim.x * blockIdx.y + blockIdx.x) +
-           get_local_thread_id();
+    return get_threads_per_block() * get_block_id() + get_local_thread_id();
 }
 
 inline __host__ int get_max_thread_blocks(const int &sm_count, const int &thread_block_cluster_size) {
