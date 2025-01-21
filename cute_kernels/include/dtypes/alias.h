11e97397ee64b68c132d5d03d4d5317b46d2d175
Mayank Mishra <mayank31398@gmail.com>
2025-01-21 19:46:51 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

---
+++ cute_kernels/include/dtypes/alias.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <cuda.h>
+#include <cuda_fp16.h>
+#include <cuda_runtime.h>
+#include <torch/extension.h>
+
+#define AT_DISPATCH_CASE_CUSTOM_FLOAT_TYPES(...)            \
+    AT_DISPATCH_CASE(at::ScalarType::Half, __VA_ARGS__)     \
+    AT_DISPATCH_CASE(at::ScalarType::BFloat16, __VA_ARGS__) \
+    AT_DISPATCH_CASE(at::ScalarType::Float, __VA_ARGS__)
+
+#define AT_DISPATCH_CUSTOM_FLOAT_TYPES(TYPE, NAME, ...) \
+    AT_DISPATCH_SWITCH(TYPE, NAME, AT_DISPATCH_CASE_CUSTOM_FLOAT_TYPES(__VA_ARGS__))
+
+#define AT_DISPATCH_CASE_CUSTOM_INT_TYPES(...)         \
+    AT_DISPATCH_CASE(at::ScalarType::Int, __VA_ARGS__) \
+    AT_DISPATCH_CASE(at::ScalarType::Long, __VA_ARGS__)
+
+#define AT_DISPATCH_CUSTOM_INT_TYPES(TYPE, NAME, ...) \
+    AT_DISPATCH_SWITCH(TYPE, NAME, AT_DISPATCH_CASE_CUSTOM_INT_TYPES(__VA_ARGS__))
+
+// define dtype aliases
+using fp64 = double;
+using fp64_2 = double2;
+using fp64_4 = double4;
+
+using fp32 = float;
+using fp32_2 = float2;
+using fp32_4 = float4;
+
+using fp16 = half;
+using fp16_2 = half2;
+
+using bf16 = __nv_bfloat16;
+using bf16_2 = __nv_bfloat162;
+
+using int64 = long;
+using uint64 = ulong;
+using uint64_2 = ulong2;
+
+using int32 = int;
+using uint32 = uint;
+using uint32_2 = uint2;
+using uint32_4 = uint4;
+
+using int16 = short;
+using uint16 = ushort;
--- cute_kernels/kernels/gemm/cuda_implementation/ops.cpp
+++ cute_kernels/kernels/gemm/cuda_implementation/ops.cpp
@@ -1,14 +1,18 @@
 #include <torch/extension.h>
 
+#include "../../../include/dtypes/alias.h"
+
 void naive_gemm_cuda(const torch::Tensor &a,
                      const torch::Tensor &b,
                      torch::Tensor &c,
                      const bool &is_a_transposed,
                      const bool &is_b_transposed,
-                     const uint &M,
-                     const uint &K,
-                     const uint &N,
-                     const uint &BLOCK_SIZE_M,
-                     const uint &BLOCK_SIZE_N);
+                     const fp32 alpha,
+                     const fp32 beta,
+                     const uint32 &M,
+                     const uint32 &K,
+                     const uint32 &N,
+                     const uint32 &BLOCK_SIZE_M,
+                     const uint32 &BLOCK_SIZE_N);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) { m.def("naive_gemm_cuda", &naive_gemm_cuda, "naive GEMM (CUDA)"); }
