bca8e54044010cf23e534c8f97f24f15563e5dfe
Mayank Mishra <mayank31398@gmail.com>
2025-01-23 22:22:42 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/gemm/cuda_implementation/ops.cpp
+++ cute_kernels/kernels/gemm/cuda_implementation/ops.cpp
@@ -20,12 +20,12 @@ void shared_memory_gemm_cuda(const torch::Tensor &a,
                              torch::Tensor &output,
                              const bool &is_a_transposed,
                              const bool &is_b_transposed,
-                             const fp32 alpha,
-                             const fp32 beta,
-                             const uint32 &M,
-                             const uint32 &K,
-                             const uint32 &N,
-                             const uint32 &BLOCK_SIZE);
+                             const float alpha,
+                             const float beta,
+                             const uint &M,
+                             const uint &K,
+                             const uint &N,
+                             const uint &BLOCK_SIZE);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("naive_gemm_cuda", &naive_gemm_cuda, "naive GEMM (CUDA)");
--- cute_kernels/kernels/gemm/cuda_implementation/shared_memory_kernel.cu
+++ cute_kernels/kernels/gemm/cuda_implementation/shared_memory_kernel.cu
@@ -20,24 +20,27 @@ __global__ void _shared_memory_gemm_cuda_kernel(const scalar_t *a,
     const uint32 i = get_thread_id_along_axis(blockDim.x, blockIdx.y, threadIdx.y);
     const uint32 j = get_thread_id_along_axis(blockDim.x, blockIdx.x, threadIdx.x);
 
+    extern __shared__ scalar_t shared_memory1[];
+    scalar_t *a_shared = shared_memory1;
+    scalar_t *b_shared = &shared_memory1[blockDim.x * blockDim.x];
+
     if (i < M && j < N) {
         fp32 accumulator = 0;
         uint32 k;
 
         for (k = 0; k < K; k += blockDim.x) {
-            __shared__ scalar_t a_shared[blockDim.x][blockDim.x];
-            __shared__ scalar_t b_shared[blockDim.x][blockDim.x];
-
             if (k < K) {
-                a_shared[threadIdx.y][threadIdx.x] = a[get_matrix_index(i, k, M, K, false)];
-                b_shared[threadIdx.y][threadIdx.x] = b[get_matrix_index(k, j, K, N, false)];
+                const uint32 index = get_matrix_index(threadIdx.y, threadIdx.x, blockDim.x, blockDim.x, false);
+                a_shared[index] = a[get_matrix_index(i, k, M, K, false)];
+                b_shared[index] = b[get_matrix_index(k, j, K, N, false)];
             }
 
             __syncthreads();
 
             const uint32 max_q = min(K - k, blockDim.x);
             for (uint32 q = 0; q < max_q; q++) {
-                accumulator += a_shared[i][q] * b_shared[q][j];
+                accumulator += a_shared[get_matrix_index(i, q, blockDim.x, blockDim.x, false)] *
+                               b_shared[get_matrix_index(q, j, blockDim.x, blockDim.x, false)];
             }
 
             __syncthreads();
@@ -73,16 +76,18 @@ void shared_memory_gemm_cuda(const torch::Tensor &a,
 
     dim3 NUM_BLOCKS = dim3(ceil_divide<uint32>(N, BLOCK_SIZE), ceil_divide<uint32>(M, BLOCK_SIZE), 1);
 
-    AT_DISPATCH_CUSTOM_FLOAT_TYPES(a.scalar_type(), "shared_memory_gemm_cuda_kernel", ([&] {
-                                       _shared_memory_gemm_cuda_kernel<scalar_t><<<NUM_BLOCKS, BLOCK_SIZE>>>(
-                                           a.data_ptr<scalar_t>(),
-                                           b.data_ptr<scalar_t>(),
-                                           c.has_value() ? c.value().data_ptr<scalar_t>() : nullptr,
-                                           output.data_ptr<scalar_t>(),
-                                           alpha,
-                                           beta,
-                                           M,
-                                           K,
-                                           N);
-                                   }));
+    AT_DISPATCH_CUSTOM_FLOAT_TYPES(
+        a.scalar_type(), "shared_memory_gemm_cuda_kernel", ([&] {
+            _shared_memory_gemm_cuda_kernel<scalar_t>
+                <<<NUM_BLOCKS, BLOCK_SIZE, 2 * BLOCK_SIZE * BLOCK_SIZE * sizeof(scalar_t)>>>(
+                    a.data_ptr<scalar_t>(),
+                    b.data_ptr<scalar_t>(),
+                    c.has_value() ? c.value().data_ptr<scalar_t>() : nullptr,
+                    output.data_ptr<scalar_t>(),
+                    alpha,
+                    beta,
+                    M,
+                    K,
+                    N);
+        }));
 }
