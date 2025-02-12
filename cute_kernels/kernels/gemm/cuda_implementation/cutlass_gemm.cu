c28d8cee12ea52c44b87d95699d325a8b42fd32a
Mayank Mishra <mayank31398@gmail.com>
2025-02-11 23:52:14 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/gemm/cuda_implementation/cutlass_gemm.cu
+++ cute_kernels/kernels/gemm/cuda_implementation/cutlass_gemm.cu
@@ -17,15 +17,19 @@ inline void _cutlass_gemm_cuda(const scalar_t *a,
                                const int &N) {
     // PyTorch tensors are row major
     using RowMajor = cutlass::layout::RowMajor;
-
+    // using element_A = cutlass::half_t;
     using element_A = fp32;
-    using element_B = fp32;
-    using element_C = fp32;
 
-    using CutlassGemm = cutlass::gemm::device::Gemm<element_A, RowMajor, element_B, RowMajor, element_C, RowMajor>;
+    using CutlassGemm =
+        cutlass::gemm::device::Gemm<element_A, RowMajor, element_A, RowMajor, element_A, RowMajor, fp32>;
 
     CutlassGemm gemm_operator;
-    CutlassGemm::Arguments args({M, N, K}, {a, K}, {b, N}, {c, N}, {output, N}, {alpha, beta});
+    CutlassGemm::Arguments args({M, N, K},
+                                {reinterpret_cast<const element_A *>(a), K},
+                                {reinterpret_cast<const element_A *>(b), N},
+                                {reinterpret_cast<const element_A *>(c), N},
+                                {reinterpret_cast<element_A *>(output), N},
+                                {alpha, beta});
 
     // call the kernel
     cutlass::Status status = gemm_operator(args);
@@ -44,16 +48,17 @@ void cutlass_gemm_cuda(const torch::Tensor &a,
                        const uint32 &N) {
     TORCH_CHECK(!is_a_transposed);
     TORCH_CHECK(!is_b_transposed);
-    _cutlass_gemm_cuda<fp32>(a.data_ptr<fp32>(),
-                             b.data_ptr<fp32>(),
-                             c.has_value() ? c.value().data_ptr<fp32>() : nullptr,
-                             output.data_ptr<fp32>(),
-                             alpha,
-                             beta,
-                             M,
-                             K,
-                             N);
-    // AT_DISPATCH_CUSTOM_FLOAT_TYPES(
-    //     fp32, "gemm_fp32", ([&] { _cutlass_gemm_cuda<scalar_t>(a.data_ptr<fp32>(), b.data_ptr<fp32>(),
-    //     c.data_ptr<fp32>(), output.data_ptr<fp32>(), alpha, beta, M, K, N); }));
+
+    AT_DISPATCH_CUSTOM_FLOAT_TYPES(a.scalar_type(), "gemm_fp32", ([&] {
+                                       _cutlass_gemm_cuda<scalar_t>(
+                                           a.data_ptr<scalar_t>(),
+                                           b.data_ptr<scalar_t>(),
+                                           c.has_value() ? c.value().data_ptr<scalar_t>() : nullptr,
+                                           output.data_ptr<scalar_t>(),
+                                           alpha,
+                                           beta,
+                                           M,
+                                           K,
+                                           N);
+                                   }));
 }
