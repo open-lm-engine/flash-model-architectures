09c3b4c4ec55b2463542df0909d66e11a4119e47
Mayank Mishra <mayank31398@gmail.com>
2025-02-18 01:41:57 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/cross_entropy/__init__.py
+++ cute_kernels/kernels/cross_entropy/__init__.py
@@ -2,6 +2,7 @@ import torch
 
 from ...cutotune import CutoTuneParameter
 from ...utils import ensure_contiguous
+from ..softmax import _forward as _softmax_forward
 from .torch_implementation import cross_entropy_torch
 from .triton_implementation import cross_entropy_forward_triton
 
@@ -9,29 +10,71 @@ from .triton_implementation import cross_entropy_forward_triton
 class _CrossEntropy_Cute(torch.autograd.Function):
     @staticmethod
     @ensure_contiguous
-    def forward(ctx, x: torch.Tensor, labels: torch.Tensor, BLOCK_SIZE_B: int, BLOCK_SIZE_V: int) -> torch.Tensor:
+    def forward(
+        ctx,
+        x: torch.Tensor,
+        labels: torch.Tensor,
+        kernel_backend_forward: str,
+        BLOCK_SIZE_B_forward: int,
+        BLOCK_SIZE_V_forward: int,
+        kernel_backend_backward: str,
+        BLOCK_SIZE_B_backward: int,
+        BLOCK_SIZE_V_backward: int,
+    ) -> torch.Tensor:
         assert x.dim() == 2, "x should be 2 dimensional"
         assert labels.dim() == 1, "labels should be 1 dimensional"
         assert x.size(0) == labels.size(0), "x and labels have different number of elements along dim 0"
 
-        loss = torch.tensor(0, device=x.device, dtype=torch.float32)
+        ctx.save_for_backward(x, labels)
 
+        ctx.kernel_backend_backward = kernel_backend_backward
+        ctx.BLOCK_SIZE_B_backward = BLOCK_SIZE_B_backward
+        ctx.BLOCK_SIZE_V_backward = BLOCK_SIZE_V_backward
+
+        loss = torch.tensor(0, device=x.device, dtype=torch.float32)
         cross_entropy_forward_triton(
-            x=x, labels=labels, loss=loss, V=x.size(-1), BLOCK_SIZE_B=BLOCK_SIZE_B, BLOCK_SIZE_V=BLOCK_SIZE_V
+            x=x,
+            labels=labels,
+            loss=loss,
+            V=x.size(-1),
+            BLOCK_SIZE_B=BLOCK_SIZE_B_forward,
+            BLOCK_SIZE_V=BLOCK_SIZE_V_forward,
         )
 
         return loss
 
     @staticmethod
     @ensure_contiguous
-    def backward(ctx, grad_output: torch.Tensor) -> tuple[torch.Tensor | None]:
-        return grad_output, *[None] * 3
+    def backward(ctx, output_grad: torch.Tensor) -> tuple[torch.Tensor | None]:
+        x, _ = ctx.saved_tensors
+
+        x_grad = _softmax_forward(
+            x=x,
+            kernel_backend=ctx.kernel_backend_backward,
+            BLOCK_SIZE_B=ctx.BLOCK_SIZE_B_backward,
+            BLOCK_SIZE_H=ctx.BLOCK_SIZE_V_backward,
+        )
+
+        return x_grad
 
 
 def cross_entropy_cute(
     x: torch.Tensor,
     labels: torch.Tensor,
-    BLOCK_SIZE_B: int = CutoTuneParameter(),
-    BLOCK_SIZE_V: int = CutoTuneParameter(),
+    kernel_backend_forward: str = CutoTuneParameter(),
+    BLOCK_SIZE_B_forward: int = CutoTuneParameter(),
+    BLOCK_SIZE_V_forward: int = CutoTuneParameter(),
+    kernel_backend_backward: str = CutoTuneParameter(),
+    BLOCK_SIZE_B_backward: int = CutoTuneParameter(),
+    BLOCK_SIZE_V_backward: int = CutoTuneParameter(),
 ) -> torch.Tensor:
-    return _CrossEntropy_Cute.apply(x, labels, BLOCK_SIZE_B, BLOCK_SIZE_V)
+    return _CrossEntropy_Cute.apply(
+        x,
+        labels,
+        kernel_backend_forward,
+        BLOCK_SIZE_B_forward,
+        BLOCK_SIZE_V_forward,
+        kernel_backend_backward,
+        BLOCK_SIZE_B_backward,
+        BLOCK_SIZE_V_backward,
+    )
