56e3cd51eb894cbed206ecc2341f4b0ac05c9654
Mayank Mishra <mayank31398@gmail.com>
2025-02-18 13:37:04 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/continuous_count/__init__.py
+++ cute_kernels/kernels/continuous_count/__init__.py
@@ -11,12 +11,6 @@ from .triton_implementation import continuous_count_triton
 
 @cutotune(
     get_cartesian_product_cutotune_configs(
-        kernel_backend=["triton"],
-        thread_block_cluster_size=[None],
-        BLOCK_SIZE=COMMON_CUDA_BLOCK_SIZES_POWERS_OF_2,
-        condition=lambda **kwargs: kwargs["size"] <= 256,
-    )
-    + get_cartesian_product_cutotune_configs(
         kernel_backend=["cuda"],
         thread_block_cluster_size=THREAD_BLOCK_CLUSTER_SIZES,
         BLOCK_SIZE=COMMON_CUDA_BLOCK_SIZES_POWERS_OF_2,
@@ -31,31 +25,20 @@ def _continuous_count_cute(
     x: torch.Tensor,
     size: int,
     thread_block_cluster_size: int,
-    kernel_backend: str,
     BLOCK_SIZE: int,
 ) -> torch.Tensor:
     assert x.dim() == 1, "x should be 1-dimensional"
     assert x.dtype in [torch.int32, torch.long]
 
-    if kernel_backend == "cuda":
-        output = torch.empty(size, dtype=torch.uint32, device=x.device)
-        continuous_count_cuda(
-            x=x,
-            output=output,
-            sm_count=get_sm_count(x.device),
-            thread_block_cluster_size=thread_block_cluster_size,
-            size=size,
-            BLOCK_SIZE=BLOCK_SIZE,
-        )
-    elif kernel_backend == "triton":
-        assert thread_block_cluster_size is None
-        output = torch.zeros(size, dtype=torch.uint32, device=x.device)
-
-        continuous_count_triton(
-            x=x, output=output, size=size, BLOCK_SIZE=BLOCK_SIZE, BLOCK_SIZE_C=get_next_power_of_2(size)
-        )
-    else:
-        raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
+    output = torch.empty(size, dtype=torch.uint32, device=x.device)
+    continuous_count_cuda(
+        x=x,
+        output=output,
+        sm_count=get_sm_count(x.device),
+        thread_block_cluster_size=thread_block_cluster_size,
+        size=size,
+        BLOCK_SIZE=BLOCK_SIZE,
+    )
 
     return output
 
@@ -75,6 +58,5 @@ def continuous_count_cute(
         x=x,
         size=size,
         thread_block_cluster_size=thread_block_cluster_size,
-        kernel_backend=kernel_backend,
         BLOCK_SIZE=BLOCK_SIZE,
     )
