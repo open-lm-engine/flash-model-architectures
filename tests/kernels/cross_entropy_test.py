f149de7848c276e8c42dad9cb036703b9c361cb9
Mayank Mishra <mayank31398@gmail.com>
2025-02-18 00:17:26 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/kernels/cross_entropy_test.py
+++ tests/kernels/cross_entropy_test.py
@@ -16,8 +16,7 @@ _SEED = 42
 class CrossEntropyTest(TestCommons):
     @parameterized.expand(
         TestCommons.make_args_matrix(
-            # TestCommons.get_2d_tensor_sizes(),  # size
-            [(3, 5)],  # size
+            TestCommons.get_2d_tensor_sizes(),  # size
             [torch.device("cuda")],  # device
             [torch.float32, torch.bfloat16],  # dtype
             ["triton"],  # kernel_backend_forward
