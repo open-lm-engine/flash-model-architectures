934d396fc3ac38038216a5e4930e8f741cc4a617
Mayank Mishra <mayank31398@gmail.com>
2025-02-21 21:47:11 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- a.py
+++ a.py
@@ -1,6 +1,7 @@
 import torch
 import torch._inductor.config as config
-from torch._inductor.pattern_matcher import PatternMatcherPass, fwd_only, register_replacement
+import torch.nn.functional as F
+from torch._inductor.pattern_matcher import PatternMatcherPass, fwd_only, joint_fwd_bwd, register_replacement
 
 from cute_kernels import CuteInductor, swiglu_unchunked_cute, swiglu_unchunked_torch
 
@@ -23,7 +24,9 @@ class _CustomPass(PatternMatcherPass):
 
 
 with config.patch(post_grad_custom_post_pass=_CustomPass()):
-    my_args = [torch.empty([10, 10], device=torch.cuda.current_device())]
+    my_args = [
+        torch.empty([10, 10], device=torch.cuda.current_device(), requires_grad=True),
+    ]
 
     invoked = False
 
@@ -43,12 +46,19 @@ with config.patch(post_grad_custom_post_pass=_CustomPass()):
 
     compiled = torch.compile(swiglu_unchunked_torch, dynamic=True, backend=CuteInductor().compiler)
 
-    x = torch.rand([8, 8], device=torch.cuda.current_device())
-    x.detach().requires_grad_()
+    x = torch.randn([8, 8], device=torch.cuda.current_device())
+    x = x.detach()
+    # x.requires_grad_()
+
     x_clone = x.clone().detach().requires_grad_()
 
     z = compiled(x)
-    z_clone = swiglu_unchunked_torch(x)
+    z_clone = swiglu_unchunked_torch(x_clone)
 
-    print(z - z_clone)
     print(saved_graph)
+    print(z - z_clone)
+
+    z.sum().backward()
+    z_clone.sum().backward()
+
+    print(x.grad - x_clone.grad)
