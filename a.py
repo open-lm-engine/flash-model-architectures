b38706b831f7fb74305463a47828eddf532643c0
Mayank Mishra <mayank31398@gmail.com>
2025-02-22 01:24:06 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- a.py
+++ a.py
@@ -3,11 +3,15 @@ import torch._inductor.config as config
 import torch.nn.functional as F
 from torch._inductor.pattern_matcher import PatternMatcherPass, fwd_only, joint_fwd_bwd, register_replacement
 
-from cute_kernels import CuteInductor, swiglu_unchunked_cute, swiglu_unchunked_torch
+
+def swiglu_unchunked_torch(x: torch.Tensor) -> torch.Tensor:
+    x = x.chunk(2, dim=-1)
+    return x[0] * F.silu(x[1])
 
 
 def f(x):
-    return swiglu_unchunked_cute(x)
+    x = x.chunk(2, dim=-1)
+    return F.relu(x[0])
 
 
 class _CustomPass(PatternMatcherPass):
@@ -15,11 +19,17 @@ class _CustomPass(PatternMatcherPass):
         super().__init__()
 
     def __call__(self, g: torch.fx.graph.Graph):
+        print(g)
         self.apply(g)
         print(g)
 
 
-with config.patch(joint_custom_post_pass=_CustomPass()):
+with config.patch(
+    pattern_matcher=False,
+    # define pattern match as custom post grad opt pass
+    post_grad_custom_pre_pass=None,
+    post_grad_custom_post_pass=_CustomPass(),
+):
     my_args = [
         torch.empty([10, 10], device=torch.cuda.current_device(), requires_grad=True),
     ]
@@ -35,15 +45,15 @@ with config.patch(joint_custom_post_pass=_CustomPass()):
         swiglu_unchunked_torch,
         f,
         my_args,
-        joint_fwd_bwd,
-        [config.joint_custom_post_pass],
+        fwd_only,
+        [config.post_grad_custom_post_pass],
         extra_check=extra_check,
     )
 
-    compiled = torch.compile(swiglu_unchunked_torch, dynamic=True, backend=CuteInductor().compiler)
+    compiled = torch.compile(swiglu_unchunked_torch, dynamic=True)
 
     x = torch.randn([8, 8], device=torch.cuda.current_device())
-    x = x.detach().requires_grad_()
+    x = x.detach()  # .requires_grad_()
 
     x_clone = x.clone().detach().requires_grad_()
 
@@ -52,7 +62,7 @@ with config.patch(joint_custom_post_pass=_CustomPass()):
 
     print(z - z_clone)
 
-    z.sum().backward()
-    z_clone.sum().backward()
+    # z.sum().backward()
+    # z_clone.sum().backward()
 
-    print(x.grad - x_clone.grad)
+    # print(x.grad - x_clone.grad)
