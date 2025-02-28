e229439d04f11155309290b44d0c1470f98a3f25
Mayank Mishra <mayank31398@gmail.com>
2025-02-27 19:52:13 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/kernels/softmax_test.py
+++ tests/kernels/softmax_test.py
@@ -52,13 +52,5 @@ class SoftmaxTest(TestCommons):
         z_kernel.sum().backward()
         z_expected.sum().backward()
 
-        self.assert_equal_tensors(z_kernel, z_expected, False)
-        self.assert_equal_tensors(
-            x_kernel.grad,
-            x_expected.grad,
-            False,
-            atol_float32=4e-5,
-            rtol_float32=0,
-            atol_bfloat16=2e-5,
-            rtol_bfloat16=0,
-        )
+        self.assert_equal_tensors(z_kernel, z_expected, False, atol_float32=4e-5, rtol_float32=0)
+        self.assert_equal_tensors(x_kernel.grad, x_expected.grad, False, atol_bfloat16=2e-5, rtol_bfloat16=0)
