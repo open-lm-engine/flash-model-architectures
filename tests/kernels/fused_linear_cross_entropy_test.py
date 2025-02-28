62b604aedcd4fe045448a3fcccaee2bfb96637b3
Mayank Mishra <mayank31398@gmail.com>
2025-02-28 00:07:41 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/kernels/fused_linear_cross_entropy_test.py
+++ tests/kernels/fused_linear_cross_entropy_test.py
@@ -63,6 +63,6 @@ class FusedLinearCrossEntropyTest(TestCommons):
         loss_kernel.backward()
         loss_expected.backward()
 
-        self.assert_equal_tensors(loss_kernel, loss_expected, False, atol_float32=2e-4, rtol_float32=0)
+        self.assert_equal_tensors(loss_kernel, loss_expected, False, atol_float32=3e-4, rtol_float32=0)
         self.assert_equal_tensors(x_kernel.grad, x_expected.grad, False)
         self.assert_equal_tensors(weight_kernel.grad, weight_expected.grad, False)
