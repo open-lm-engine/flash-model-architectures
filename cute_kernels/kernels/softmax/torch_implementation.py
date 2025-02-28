36406d9ab7996d7f614c614a38a93a3f57aa224a
Mayank Mishra <mayank31398@gmail.com>
2025-02-27 19:41:52 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/softmax/torch_implementation.py
+++ cute_kernels/kernels/softmax/torch_implementation.py
@@ -3,6 +3,10 @@ import torch.nn.functional as F
 
 
 def softmax_torch(x: torch.Tensor, logits_multiplier: float = 1) -> torch.Tensor:
+    dtype = x.dtype
+
     x = x.float()
     x /= logits_multiplier
-    return F.softmax(x, dim=-1)
+    x = F.softmax(x, dim=-1)
+
+    return x.to(dtype)
