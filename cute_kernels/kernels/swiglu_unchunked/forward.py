0ac9f9412d5700089947a2de9c3bc19a62683415
Mayank Mishra <mayank31398@gmail.com>
2025-03-01 16:30:29 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/swiglu_unchunked/forward.py
+++ cute_kernels/kernels/swiglu_unchunked/forward.py
@@ -9,15 +9,17 @@ from .triton_implementation import _swiglu_unchunked_forward_triton_kernel
 
 @cutotune(**get_cutotune_parameters())
 def _forward(x: torch.Tensor, kernel_backend: str, BLOCK_SIZE_B: int, BLOCK_SIZE_H: int) -> torch.Tensor:
-    B, H = get_num_elements_and_hidden_size(x)
-    output = torch.empty(*x.size()[:-1], divide_if_divisible(H, 2), device=x.device, dtype=x.dtype)
+    num_elements, hidden_size = get_num_elements_and_hidden_size(x)
+    output = torch.empty(*x.size()[:-1], divide_if_divisible(hidden_size, 2), device=x.device, dtype=x.dtype)
 
     if kernel_backend == "triton":
-        _swiglu_unchunked_forward_triton_kernel[(ceil_divide(B, BLOCK_SIZE_B), ceil_divide(H, BLOCK_SIZE_H))](
+        _swiglu_unchunked_forward_triton_kernel[
+            (ceil_divide(num_elements, BLOCK_SIZE_B), ceil_divide(hidden_size, BLOCK_SIZE_H))
+        ](
             x_ptr=x,
             output_ptr=output,
-            B=B,
-            H=H,
+            B=num_elements,
+            H=hidden_size,
             BLOCK_SIZE_B=BLOCK_SIZE_B,
             BLOCK_SIZE_H=BLOCK_SIZE_H,
         )
