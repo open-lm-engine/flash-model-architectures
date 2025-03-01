3f30143127c90cd3c7b4ba18117b9b53d42701a7
Mayank Mishra <mayank31398@gmail.com>
2025-03-01 16:37:32 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/swiglu/backward.py
+++ cute_kernels/kernels/swiglu/backward.py
@@ -1,9 +1,10 @@
 import torch
 
 from ...cutotune import cutotune
+from ...math import ceil_divide
 from .cuda_implementation import swiglu_backward_cuda
 from .parameters import get_cutotune_parameters
-from .triton_implementation import swiglu_backward_triton
+from .triton_implementation import _swiglu_backward_triton_kernel
 
 
 @cutotune(**get_cutotune_parameters())
@@ -18,12 +19,22 @@ def _backward(
     up_grad = torch.empty_like(up)
 
     if kernel_backend == "cuda":
-        function = swiglu_backward_cuda
+        swiglu_backward_cuda(
+            gate=gate, up=up, output_grad=output_grad, gate_grad=gate_grad, up_grad=up_grad, BLOCK_SIZE=BLOCK_SIZE
+        )
     elif kernel_backend == "triton":
-        function = swiglu_backward_triton
+        num_elements = gate.numel()
+
+        _swiglu_backward_triton_kernel[(ceil_divide(num_elements, BLOCK_SIZE),)](
+            gate_ptr=gate,
+            up_ptr=up,
+            output_grad_ptr=output_grad,
+            gate_grad_ptr=gate_grad,
+            up_grad_ptr=up_grad,
+            num_elements=num_elements,
+            BLOCK_SIZE=BLOCK_SIZE,
+        )
     else:
         raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
-
-    function(gate=gate, up=up, output_grad=output_grad, gate_grad=gate_grad, up_grad=up_grad, BLOCK_SIZE=BLOCK_SIZE)
 
     return gate_grad, up_grad
--- cute_kernels/kernels/swiglu/forward.py
+++ cute_kernels/kernels/swiglu/forward.py
@@ -1,17 +1,29 @@
 import torch
 
 from ...cutotune import cutotune
+from ...math import ceil_divide
 from .cuda_implementation import swiglu_forward_cuda
 from .parameters import get_cutotune_parameters
-from .triton_implementation import swiglu_forward_triton
-
-
-_KERNEL_MAP = {"cuda": swiglu_forward_cuda, "triton": swiglu_forward_triton}
+from .triton_implementation import _swiglu_forward_triton_kernel
 
 
 @cutotune(**get_cutotune_parameters())
 def _forward(gate: torch.Tensor, up: torch.Tensor, kernel_backend: str, BLOCK_SIZE: int) -> torch.Tensor:
     output = torch.empty_like(gate)
-    _KERNEL_MAP[kernel_backend](gate=gate, up=up, output=output, BLOCK_SIZE=BLOCK_SIZE)
+
+    if kernel_backend == "cuda":
+        swiglu_forward_cuda(gate=gate, up=up, output=output, BLOCK_SIZE=BLOCK_SIZE)
+    elif kernel_backend == "triton":
+        num_elements = gate.numel()
+
+        _swiglu_forward_triton_kernel[(ceil_divide(num_elements, BLOCK_SIZE),)](
+            gate_ptr=gate,
+            up_ptr=up,
+            output_ptr=output,
+            num_elements=num_elements,
+            BLOCK_SIZE=BLOCK_SIZE,
+        )
+    else:
+        raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
 
     return output
