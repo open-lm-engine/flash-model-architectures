ec182dc0ad9bde1a5ecc8f3f6a937a6a75082a43
Mayank Mishra <mayank31398@gmail.com>
2025-03-10 15:15:38 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/counters.py
+++ cute_kernels/counters.py
@@ -1,11 +1,12 @@
 from collections import defaultdict
+from typing import Callable
 
 
 kernel_counter = defaultdict(int)
 
 
-def kernel_counter_increment(*args, **kwargs):
-    def _call(func):
+def kernel_counter_increment(func: Callable) -> Callable:
+    def _call(*args, **kwargs):
         kernel_counter[func.__name__] += 1
         return func(*args, **kwargs)
 
