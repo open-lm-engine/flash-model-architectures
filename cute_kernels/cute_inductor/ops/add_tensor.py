b19f8c4db20f8a223419963ec60f3f068b50dd2c
Mayank Mishra <mayank31398@gmail.com>
2025-03-11 19:13:32 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/cute_inductor/ops/add_tensor.py
+++ cute_kernels/cute_inductor/ops/add_tensor.py
@@ -1,4 +1,5 @@
 import torch
+from torch._inductor.pattern_matcher import Match
 
 from ...kernels import add_tensor_cute, add_tensor_torch
 from ..config import ReplacementConfig
@@ -11,8 +12,13 @@ def _get_example_inputs() -> tuple[torch.Tensor]:
     ]
 
 
+def _extra_check(match: Match) -> bool:
+    return True
+
+
 add_tensor_replacement_config = ReplacementConfig(
     pattern_function=add_tensor_torch,
     replacement_function=add_tensor_cute,
     example_inputs_function=_get_example_inputs,
+    extra_check=_extra_check,
 )
