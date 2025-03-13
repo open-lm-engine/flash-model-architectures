3c38c2b7cb2019776d304902c98ed79bb5189f97
Mayank Mishra <mayank31398@gmail.com>
2025-03-13 00:40:01 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/cute_inductor/pattern_matcher.py
+++ cute_kernels/cute_inductor/pattern_matcher.py
@@ -1,10 +1,15 @@
 import importlib
 import os
+import textwrap
+from pathlib import Path
 from typing import Any, Callable, Iterable, Sequence, Union
 
+import torch
 import torch.utils._pytree as pytree
 from torch._inductor.pattern_matcher import (
     Match,
+    PatternExpr,
+    PatternPrettyPrinter,
     ReplaceFn,
     SearchFn,
     TraceFn,
@@ -12,9 +17,85 @@ from torch._inductor.pattern_matcher import (
     _PassDictsType,
     _return_true,
     _serialize_pattern,
+    _serialized_patterns,
+    _TargetExpr,
+    gen_pattern,
     register_replacement,
 )
 from torch._subclasses import FakeTensor
+
+
+def _serialize_pattern(
+    unique_name: str,
+    search_fn: SearchFn,
+    example_inputs: Sequence[Any],
+    trace_fn: TraceFn,
+    scalar_workaround: Union[dict[str, Union[float, int]], None],
+    directory: Path,
+) -> PatternExpr:
+    def get_file_template() -> str:
+        auto_generated_msg = textwrap.dedent(
+            """\
+            # This is an auto-generated file. Please do not modify it by hand.
+            # To re-generate, run:
+            # cd ~/pytorch && python torchgen/fuse/gen_patterns.py
+            """
+        )
+
+        file_template = textwrap.dedent(
+            """\
+            # mypy: ignore-errors
+
+            # noqa: F401, E501
+            {msg}
+            import torch
+            import torch._inductor
+            import operator
+
+            aten = torch.ops.aten
+            prims = torch.ops.prims
+
+            """
+        ).format(msg=auto_generated_msg)
+
+        pattern_matcher_imports = []
+        for name in dir(torch._inductor.pattern_matcher):
+            attr = getattr(torch._inductor.pattern_matcher, name)
+            if isinstance(attr, type) and issubclass(attr, (PatternExpr, _TargetExpr)):
+                pattern_matcher_imports.append(name)
+
+        formatted_imports = ",\n   ".join(pattern_matcher_imports)
+        formatted_imports = f"from torch._inductor.pattern_matcher import (\n   {formatted_imports},\n)\n"
+        return f"{file_template}{formatted_imports}"
+
+    if not directory.is_dir():
+        raise RuntimeError(f"Could not find serialized patterns directory at {directory}")
+
+    pattern_name = search_fn.__name__
+
+    from torch._functorch import config as functorch_config
+
+    with functorch_config.patch(functionalize_rng_ops=False):
+        pattern = gen_pattern(search_fn, example_inputs, trace_fn, scalar_workaround)
+
+    serialized_pattern = PatternPrettyPrinter.run(pattern, output_name=unique_name)
+    if pattern_name not in _serialized_patterns:
+        write_mode = "w"
+        _serialized_patterns.add(pattern_name)
+    else:
+        write_mode = "a"
+
+    file_template = get_file_template()
+
+    with open(directory / f"{pattern_name}.py", write_mode) as f:
+        if write_mode == "w":
+            f.write(file_template)
+        else:
+            f.write("\n\n")
+        f.write(serialized_pattern)
+        f.write("\n")
+
+    return pattern
 
 
 def gen_register_replacement(
@@ -32,17 +113,16 @@ def gen_register_replacement(
     # Make sure the example_inputs is materialized.
     example_inputs = tuple(example_inputs)
 
-    if "PYTORCH_GEN_PATTERNS" in os.environ:
+    pattern_name = search_fn.__name__
+    m = importlib.import_module(f"torch._inductor.fx_passes.serialized_patterns.{pattern_name}")
+
+    if not m or not hasattr(m, unique_name):
         pat = _serialize_pattern(unique_name, search_fn, example_inputs, trace_fn, scalar_workaround)
     else:
-        pattern_name = search_fn.__name__
-        m = importlib.import_module(f"torch._inductor.fx_passes.serialized_patterns.{pattern_name}")
-        if not m or not hasattr(m, unique_name):
-            log.warning(
-                "Precompiled pattern %r not found. Run torchgen/fuse/gen_patterns.py.",
-                unique_name,
-            )
-        pat = getattr(m, unique_name)
+        pat = getattr(m, unique_name, None)
+
+    if pat is None:
+        pat = _serialize_pattern(unique_name, search_fn, example_inputs, trace_fn, scalar_workaround)
 
     for arg in pytree.tree_iter(example_inputs):
         if isinstance(arg, FakeTensor) and arg.constant is not None:
