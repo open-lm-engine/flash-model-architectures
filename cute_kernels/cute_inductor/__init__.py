c9011f5d63868eac5a07c8f9a5f435c02e77298e
Mayank Mishra <mayank31398@gmail.com>
2025-03-13 01:07:20 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/cute_inductor/__init__.py
+++ cute_kernels/cute_inductor/__init__.py
@@ -1,2 +1,2 @@
 from .compiler import CuteInductor
-from .ops import add_tensor_replacement_config, scattermoe_replacement_config
+from .configs import add_tensor_replacement_config, scattermoe_replacement_config
