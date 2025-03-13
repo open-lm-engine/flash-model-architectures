a2245dfa039f8f0b391ff0bca65e1062a2c6e823
Mayank Mishra <mayank31398@gmail.com>
2025-03-13 05:14:36 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/cute_inductor/graphs/swiglu_inference.py
+++ cute_kernels/cute_inductor/graphs/swiglu_inference.py
@@ -5,32 +5,36 @@
 # To re-generate, run:
 # cd ~/pytorch && python torchgen/fuse/gen_patterns.py
 
+import operator
+
 import torch
 import torch._inductor
-import operator
+
 
 aten = torch.ops.aten
 prims = torch.ops.prims
 
 from torch._inductor.pattern_matcher import (
-   Arg,
-   CallFunction,
-   CallFunctionVarArgs,
-   CallMethod,
-   CallMethodVarArgs,
-   CallModule,
-   CallModuleVarArgs,
-   ExclusiveKeywordArg,
-   Ignored,
-   KeywordArg,
-   ListOf,
-   MultiOutputPattern,
-   PatternExpr,
-   RepeatedExpr,
-   _TargetArgsExpr,
-   _TargetExpr,
-   _TargetExprVarArgs,
+    Arg,
+    CallFunction,
+    CallFunctionVarArgs,
+    CallMethod,
+    CallMethodVarArgs,
+    CallModule,
+    CallModuleVarArgs,
+    ExclusiveKeywordArg,
+    Ignored,
+    KeywordArg,
+    ListOf,
+    MultiOutputPattern,
+    PatternExpr,
+    RepeatedExpr,
+    _TargetArgsExpr,
+    _TargetExpr,
+    _TargetExprVarArgs,
 )
-sigmoid_default = CallFunction(aten.sigmoid.default, KeywordArg('gate'))
-mul_Tensor = CallFunction(aten.mul.Tensor, KeywordArg('gate'), sigmoid_default)
-swiglu_inference = CallFunction(aten.mul.Tensor, KeywordArg('up'), mul_Tensor, _users=0)
+
+
+sigmoid_default = CallFunction(aten.sigmoid.default, KeywordArg("gate"))
+mul_Tensor = CallFunction(aten.mul.Tensor, KeywordArg("gate"), sigmoid_default)
+swiglu_inference = CallFunction(aten.mul.Tensor, KeywordArg("up"), mul_Tensor, _users=0)
