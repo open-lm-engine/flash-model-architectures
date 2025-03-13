f5cd1a4f9f396d0a0bb972351142b7f9a0107121
Mayank Mishra <mayank31398@gmail.com>
2025-03-13 05:53:15 +0000

    cleanup

---
+++ cute_kernels/cute_inductor/graphs/add_tensor_inference.py
@@ -0,0 +1,34 @@
+# mypy: ignore-errors
+
+# noqa: F401, E501
+# This is an auto-generated file. Please do not modify it by hand.
+# To re-generate, run:
+# cd ~/pytorch && python torchgen/fuse/gen_patterns.py
+
+import torch
+import torch._inductor
+import operator
+
+aten = torch.ops.aten
+prims = torch.ops.prims
+
+from torch._inductor.pattern_matcher import (
+   Arg,
+   CallFunction,
+   CallFunctionVarArgs,
+   CallMethod,
+   CallMethodVarArgs,
+   CallModule,
+   CallModuleVarArgs,
+   ExclusiveKeywordArg,
+   Ignored,
+   KeywordArg,
+   ListOf,
+   MultiOutputPattern,
+   PatternExpr,
+   RepeatedExpr,
+   _TargetArgsExpr,
+   _TargetExpr,
+   _TargetExprVarArgs,
+)
+add_tensor_inference = CallFunction(aten.add.Tensor, KeywordArg('x'), KeywordArg('y'), _users=0)
---
+++ cute_kernels/cute_inductor/graphs/add_tensor_training.py
@@ -0,0 +1,38 @@
+# mypy: ignore-errors
+
+# noqa: F401, E501
+# This is an auto-generated file. Please do not modify it by hand.
+# To re-generate, run:
+# cd ~/pytorch && python torchgen/fuse/gen_patterns.py
+
+import torch
+import torch._inductor
+import operator
+
+aten = torch.ops.aten
+prims = torch.ops.prims
+
+from torch._inductor.pattern_matcher import (
+   Arg,
+   CallFunction,
+   CallFunctionVarArgs,
+   CallMethod,
+   CallMethodVarArgs,
+   CallModule,
+   CallModuleVarArgs,
+   ExclusiveKeywordArg,
+   Ignored,
+   KeywordArg,
+   ListOf,
+   MultiOutputPattern,
+   PatternExpr,
+   RepeatedExpr,
+   _TargetArgsExpr,
+   _TargetExpr,
+   _TargetExprVarArgs,
+)
+add_Tensor = CallFunction(aten.add.Tensor, KeywordArg('x'), KeywordArg('y'))
+add_tensor_training = MultiOutputPattern([add_Tensor,
+  KeywordArg('tangents_1'),
+  KeywordArg('tangents_1')
+])
