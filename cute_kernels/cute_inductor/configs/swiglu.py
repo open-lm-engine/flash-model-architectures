1d3233c174957ac1201d612f410e996697872058
Mayank Mishra <mayank31398@gmail.com>
2025-03-13 05:37:29 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/cute_inductor/configs/swiglu.py
+++ cute_kernels/cute_inductor/configs/swiglu.py
@@ -1,4 +1,5 @@
 import torch
+from torch._inductor.pattern_matcher import Match
 
 from ...kernels import swiglu_cute, swiglu_torch
 from ..config import ReplacementConfig
@@ -7,6 +8,22 @@ from .add_tensor import _extra_check, _get_example_inputs
 
 def _replacement_function(gate: torch.Tensor, up: torch.Tensor) -> torch.Tensor:
     return swiglu_cute(gate, up)
+
+
+def _extra_check(match: Match) -> bool:
+    if len(match.kwargs) != 2:
+        return False
+
+    gate = match.kwargs["gate"].meta["val"]
+    up = match.kwargs["up"].meta["val"]
+
+    return (
+        isinstance(gate, torch.Tensor)
+        and isinstance(up, torch.Tensor)
+        and gate.size() == up.size()
+        and gate.dtype == up.dtype
+        and gate.device == up.device
+    )
 
 
 swiglu_replacement_config = ReplacementConfig(
