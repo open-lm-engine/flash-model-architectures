42cb96e02aaf6e289b8fbe8951f94b310c273a4b
Mayank Mishra <mayank31398@gmail.com>
2025-03-17 19:08:54 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/gemm/cuda_implementation/naive_kernel.cu
+++ cute_kernels/kernels/gemm/cuda_implementation/naive_kernel.cu
@@ -2,10 +2,15 @@
 #include <cuda_runtime.h>
 #include <torch/extension.h>
 
-#include "include/dtypes/dtypes.h"
+#include "include/dtypes.h"
 #include "include/math.h"
 #include "include/threads.h"
 #include "index.h"
+
+namespace ck = cute_kernels;
+
+using uint32 = ck::uint32;
+using fp32 = ck::fp32;
 
 template <typename scalar_t>
 __global__ void _naive_gemm_cuda_kernel(const scalar_t *A,
@@ -19,8 +24,8 @@ __global__ void _naive_gemm_cuda_kernel(const scalar_t *A,
                                         const uint32 M,
                                         const uint32 K,
                                         const uint32 N) {
-    const uint32 i = get_thread_id_along_axis(blockDim.y, blockIdx.y, threadIdx.y);
-    const uint32 j = get_thread_id_along_axis(blockDim.x, blockIdx.x, threadIdx.x);
+    const uint32 i = ck::get_thread_id_along_axis(blockDim.y, blockIdx.y, threadIdx.y);
+    const uint32 j = ck::get_thread_id_along_axis(blockDim.x, blockIdx.x, threadIdx.x);
 
     if (i < M && j < N) {
         fp32 accumulator = 0;
@@ -61,7 +66,7 @@ void naive_gemm_cuda(const torch::Tensor &A,
                      const uint32 &BLOCK_SIZE_N) {
     TORCH_CHECK((BLOCK_SIZE_M * BLOCK_SIZE_N) % WARP_SIZE == 0);
 
-    dim3 NUM_BLOCKS = dim3(ceil_divide<uint32>(N, BLOCK_SIZE_N), ceil_divide<uint32>(M, BLOCK_SIZE_M), 1);
+    dim3 NUM_BLOCKS = dim3(ck::ceil_divide<uint32>(N, BLOCK_SIZE_N), ck::ceil_divide<uint32>(M, BLOCK_SIZE_M), 1);
     dim3 BLOCK_SIZE = dim3(BLOCK_SIZE_N, BLOCK_SIZE_M, 1);
 
     AT_DISPATCH_CUSTOM_FLOAT_TYPES(A.scalar_type(), "naive_gemm_cuda_kernel", ([&] {
