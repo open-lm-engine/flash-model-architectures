d447867866d2053b83fa16363312c8533efa6797
Mayank Mishra <mayank31398@gmail.com>
2025-03-17 23:10:27 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/include/cute_kernels.h
+++ cute_kernels/include/cute_kernels.h
@@ -2,11 +2,7 @@
 
 #include "activations.h"
 #include "checks.h"
-#include "dtypes/bf16.h"
-#include "dtypes/fp16.h"
-#include "dtypes/fp32.h"
-#include "dtypes/fp64.h"
-#include "dtypes/uint32.h"
+#include "dtypes.h"
 #include "launch.h"
 #include "math.h"
 #include "shared_memory.h"
