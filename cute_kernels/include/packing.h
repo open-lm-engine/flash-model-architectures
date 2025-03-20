1dc08b44ecb33d8409e7a80114e41efc94a3f898
Mayank Mishra <mayank31398@gmail.com>
2025-03-20 15:08:00 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/include/packing.h
+++ cute_kernels/include/packing.h
@@ -9,19 +9,19 @@
 // This is a bit similar to the use of float4 in the case of 32-bit floats, but
 // supports arbitrary precision.
 
-template <class dtype>
+template <class T>
 struct alignas(16) Packed128 {
     Packed128() = default;
 
-    static constexpr const uint32 size = sizeof(int32_4) / sizeof(dtype);
-    dtype payload[size];
+    static constexpr const uint32 size = sizeof(int32_4) / sizeof(T);
+    T payload[size];
 
-    __device__ explicit Packed128(int4 bits) {
-        static_assert(sizeof(bits) == sizeof(payload), "Size mismatch.");
+    __device__ explicit Packed128(int32_4 bits) {
+        static_assert(sizeof(bits) == sizeof(payload), "size mismatch");
         memcpy(&payload, &bits, sizeof(bits));
     }
 
-    __device__ static Packed128 constant(dtype value) {
+    __device__ static Packed128 constant(T value) {
         Packed128 result;
         for (int k = 0; k < size; ++k) {
             result.payload[k] = value;
@@ -32,42 +32,42 @@ struct alignas(16) Packed128 {
     __device__ static Packed128 zeros() { return constant(0.f); }
     __device__ static Packed128 ones() { return constant(1.f); }
 
-    __device__ dtype& operator[](int index) { return payload[index]; }
-    __device__ const dtype& operator[](int index) const { return payload[index]; }
+    __device__ T& operator[](int index) { return payload[index]; }
+    __device__ const T& operator[](int index) const { return payload[index]; }
     __device__ int4 get_bits() const {
         int4 bits;
-        static_assert(sizeof(bits) == sizeof(payload), "Size mismatch.");
+        static_assert(sizeof(bits) == sizeof(payload), "size mismatch");
         memcpy(&bits, &payload, sizeof(bits));
         return bits;
     }
 };
 
 // load a Packed128 from an aligned memory address
-template <class dtype>
-__device__ Packed128<dtype> load128(const dtype* address) {
-    return Packed128<dtype>{*reinterpret_cast<const int4*>(address)};
+template <class T>
+__device__ Packed128<T> load128(const T* address) {
+    return Packed128<T>{*reinterpret_cast<const int4*>(address)};
 }
 
 // load a Packed128 from an aligned memory address with streaming cache hint
-template <class dtype>
-__device__ Packed128<dtype> load128cs(const dtype* address) {
-    return Packed128<dtype>{__ldcs(reinterpret_cast<const int4*>(address))};
+template <class T>
+__device__ Packed128<T> load128cs(const T* address) {
+    return Packed128<T>{__ldcs(reinterpret_cast<const int4*>(address))};
 }
 
 // store a Packed128 to an aligned memory address
-template <class dtype>
-__device__ void store128(dtype* target, Packed128<dtype> value) {
+template <class T>
+__device__ void store128(T* target, Packed128<T> value) {
     *reinterpret_cast<int4*>(target) = value.get_bits();
 }
 
 // store a Packed128 to an aligned memory address with streaming cache hint
-template <class dtype>
-__device__ void store128cs(dtype* target, Packed128<dtype> value) {
+template <class T>
+__device__ void store128cs(T* target, Packed128<T> value) {
     __stcs(reinterpret_cast<int4*>(target), value.get_bits());
 }
 
 // store a Packed128 to an aligned memory address while caching in L2 but bypassing L1
-template <class dtype>
-__device__ void store128cg(dtype* target, Packed128<dtype> value) {
+template <class T>
+__device__ void store128cg(T* target, Packed128<T> value) {
     __stcg(reinterpret_cast<int4*>(target), value.get_bits());
 }
