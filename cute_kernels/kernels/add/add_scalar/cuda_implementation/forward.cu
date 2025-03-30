7e102bdee8309ecbe5c3381a5a8f75a7e463472c
Mayank Mishra <mayank31398@gmail.com>
2025-03-30 17:52:51 -0400

    fix init
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/add/add_scalar/cuda_implementation/forward.cu
+++ cute_kernels/kernels/add/add_scalar/cuda_implementation/forward.cu
@@ -31,7 +31,8 @@ __global__ void _add_scalar_cuda_kernel(const scalar_t *x, const fp32 y, scalar_
             if constexpr (std::is_same_v<scalar_t, fp32>) {
                 output_buffer[i] = x_vec[i] + y;
             } else {
-                using T2 = dtype::nv_dtype2;
+                using T2 = typename dtype::nv_dtype2;
+
                 T2 x2 = dtype::make2(x_vec[0], x_vec[1]);
                 T2 y2 = dtype::make2(y);
                 x2 = __hadd2(x2, y2);
