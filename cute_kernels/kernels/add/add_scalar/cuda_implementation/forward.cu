4fddb05a58c6cb9571e5f445ca5e4200b10f4700
Mayank Mishra <mayank31398@gmail.com>
2025-03-31 17:44:09 -0400

    fix init
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/add/add_scalar/cuda_implementation/forward.cu
+++ cute_kernels/kernels/add/add_scalar/cuda_implementation/forward.cu
@@ -16,7 +16,7 @@ __global__ void _add_scalar_cuda_kernel(const scalar_t *x, const fp32 y, scalar_
     using dtype = ck::DType<scalar_t>;
     constexpr uint32 num_elements_per_thread = ck_mem::Packed128<scalar_t>::size;
 
-    const uint32 thread_id = ck::get_global_thread_id();
+    const uint32 thread_id = blockIdx.x * blockDim.x + threadIdx.x;
     const uint32 num_vector_elements = num_elements / num_elements_per_thread;
 
     if (thread_id < num_vector_elements) {
