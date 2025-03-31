1d25b68c4a5ee1ff0b7667c014fc181fed7668c1
Mayank Mishra <mayank31398@gmail.com>
2025-03-31 13:54:04 -0400

    fix init
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/include/dtypes/bf16.h
+++ cute_kernels/include/dtypes/bf16.h
@@ -26,7 +26,6 @@ namespace cute_kernels {
 
         inline __device__ static nv_dtype2 make2(const nv_dtype &value) { return __bfloat162bfloat162(value); }
         inline __device__ static nv_dtype2 make2(const nv_dtype &x, const nv_dtype &y) { return make_bfloat162(x, y); }
-        inline __device__ static nv_dtype2 make2(const nv_dtype *array) { return make_bfloat162(array[0], array[1]); }
     };
 
     template <>
