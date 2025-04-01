e427bb039d630d70ab8b7318061fbe09581421bc
Mayank Mishra <mayank31398@gmail.com>
2025-04-01 02:28:40 -0400

    fix init
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/continuous_count/cuda_implementation/forward.cu
+++ cute_kernels/kernels/continuous_count/cuda_implementation/forward.cu
@@ -53,10 +53,12 @@ inline __device__ void _update_local_count(const scalar_t *x,
                                            const uint32 &global_thread_id,
                                            const uint32 &total_threads) {
     ck_mem::Packed128Array<scalar_t> x_vec = ck_mem::Packed128Array<scalar_t>(x);
-    const uint32 num_vector_elements = num_elements / x_vec.size;
+
+    constexpr num_elements_per_thread = x_vec.size;
+    const uint32 num_vector_elements = num_elements / num_elements_per_thread;
 
     for (uint32 i = global_thread_id; i < num_vector_elements; i += total_threads) {
-        for (uint32 j = 0; j < x_vec.size; j++) {
+        for (uint32 j = 0; j < num_elements_per_thread; j++) {
             atomicAdd(&shared_memory[x_vec[j]], 1);
         }
     }
