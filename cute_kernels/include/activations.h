52c9b186dafb3e69e7a5b6b58907605277b1f0a7
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2025-04-07 15:07:44 -0400

    fix thread divergence in sigmoid (#183)
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/include/activations.h
+++ cute_kernels/include/activations.h
@@ -2,17 +2,17 @@
 
 #include "dtypes.h"
 
+#define MAX_EXP_FP32 88.3762626647949
+#define MIN_EXP_FP32 -88.3762626647949
+
 namespace cute_kernels {
     template <typename input_T, typename output_T>
     __device__ output_T sigmoid(const input_T &x) {
         fp32 x_fp32 = DType<input_T>::upcast(x);
+        x_fp32 = fmaxf(MIN_EXP_FP32, x_fp32);
+        x_fp32 = fminf(MAX_EXP_FP32, x_fp32);
 
-        if (x >= 0) {
-            x_fp32 = 1 / (1 + expf(-x_fp32));
-        } else {
-            x_fp32 = expf(x_fp32);
-            x_fp32 = x_fp32 / (1 + x_fp32);
-        }
+        x_fp32 = 1 / (1 + expf(-x_fp32));
 
         return DType<output_T>::downcast(x_fp32);
     }
