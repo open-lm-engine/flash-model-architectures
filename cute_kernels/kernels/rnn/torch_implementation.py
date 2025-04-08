0d27c6b3534fe4e6f5be3c308946f09ad71d382e
Mayank Mishra <mayank31398@gmail.com>
2025-04-08 01:26:05 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/rnn/torch_implementation.py
+++ cute_kernels/kernels/rnn/torch_implementation.py
@@ -4,36 +4,41 @@ import torch.nn.functional as F
 
 
 @torch.compile
-def rnn_torch(input: torch.Tensor, weight: torch.Tensor, input_state: torch.Tensor | None = None) -> torch.Tensor:
-    # input -> (B, S, N, H)
+def rnn_torch(
+    input: torch.Tensor, weight: torch.Tensor, input_state: torch.Tensor | None = None, return_all_states: bool = True
+) -> torch.Tensor:
     B, S, N, H = input.size()
-    output = torch.empty_like(input)
+
+    output = None
+    if return_all_states:
+        output = torch.empty_like(input)
 
     if input_state is None:
         input_state = torch.zeros(B, N, H, device=input.device, dtype=torch.float32)
     else:
         input_state = input_state.float()
 
-    # input_state -> (B, N, H)
-    input_state = input_state.unsqueeze(-2)
-    # input_state -> (B, N, 1, H)
-
-    # weight -> (N, H, H)
     weight = weight.unsqueeze(0)
-    # weight -> (1, N, H, H)
     input = input.unsqueeze(-2)
+
     # input -> (B, S, N, 1, H)
+    # weight -> (1, N, H, H)
+    # input_state -> (B, N, H)
 
     for s in range(S):
+        input_state = input_state.unsqueeze(-2)
         # (B, N, 1, H) @ (1, N, H, H) + (B, N, 1, H)
         input_state = input_state.type_as(weight)
         input_state = input_state @ weight + input[:, s, ...]
+
         input_state = input_state.float()
         input_state = F.tanh(input_state)
+        input_state = input_state.squeeze(-2)
 
-        output[:, s, ...] = input_state.squeeze(-2)
+        if return_all_states:
+            output[:, s, ...] = input_state
 
-    return output
+    return output, input_state
 
 
 class RNNTorch(nn.Module):
