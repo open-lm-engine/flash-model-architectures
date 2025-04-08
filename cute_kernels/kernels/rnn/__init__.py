884723cf91a2a19379efc7534c2d8fa50a033b3d
Mayank Mishra <mayank31398@gmail.com>
2025-04-08 00:03:27 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/rnn/__init__.py
+++ cute_kernels/kernels/rnn/__init__.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 
 from ...cutotune import CutoTuneParameter
@@ -9,15 +10,12 @@ class _RNN_Cute(torch.autograd.Function):
     @staticmethod
     def forward(
         ctx,
-        input: torch.Tensor,
-        input_weight: torch.Tensor,
+        x: torch.Tensor,
         state_weight: torch.Tensor,
-        output_weight: torch.Tensor,
         input_state: torch.Tensor | None,
         BLOCK_SIZE_B: int,
         BLOCK_SIZE_H: int,
     ) -> tuple[torch.Tensor, torch.Tensor]:
-        x = F.linear(input, input_weight)
         y = torch.empty_like(x)
 
         rnn_forward_triton(
@@ -29,8 +27,7 @@ class _RNN_Cute(torch.autograd.Function):
             BLOCK_SIZE_H=BLOCK_SIZE_H,
         )
 
-        output = F.linear(y, output_weight)
-        return output
+        return y
 
     @staticmethod
     def backward(ctx, output_grad: torch.Tensor) -> tuple[torch.Tensor]: ...
@@ -38,11 +35,30 @@ class _RNN_Cute(torch.autograd.Function):
 
 def rnn_cute(
     input: torch.Tensor,
-    input_weight: torch.Tensor,
     state_weight: torch.Tensor,
-    output_weight: torch.Tensor,
     input_state: torch.Tensor | None = None,
     BLOCK_SIZE_B: int = CutoTuneParameter(),
     BLOCK_SIZE_H: int = CutoTuneParameter(),
 ) -> torch.Tensor:
-    return _RNN_Cute.apply(input, input_weight, state_weight, output_weight, input_state, BLOCK_SIZE_B, BLOCK_SIZE_H)
+    return _RNN_Cute.apply(input, state_weight, input_state, BLOCK_SIZE_B, BLOCK_SIZE_H)
+
+
+class RNNCute(nn.Module):
+    def __init__(
+        self, input_size: int, state_size: int, output_size: int, num_heads: int, add_bias: bool = True
+    ) -> None:
+        self.input_size = input_size
+        self.state_size = state_size
+        self.output_size = output_size
+        self.num_heads = num_heads
+
+        self.input_weight = nn.Parameter(torch.empty(self.num_heads * self.state_size, self.input_size))
+        self.state_weight = nn.Parameter(torch.empty(self.num_heads, self.state_size, self.state_size))
+        self.state_bias = nn.Parameter(torch.empty(self.num_heads, self.state_size)) if add_bias else None
+        self.output_weight = nn.Parameter(torch.empty(self.output_size, self.num_heads * self.state_size))
+
+    def forward(self, x: torch.Tensor, input_state: torch.Tensor | None = None) -> torch.Tensor:
+        x = F.linear(x, self.input_weight)
+        x = rnn_cute(input=x, state_weight=self.state_weight, state_bias=self.state_bias, input_state=input_state)
+        x = F.linear(x, self.output_weight)
+        return x
