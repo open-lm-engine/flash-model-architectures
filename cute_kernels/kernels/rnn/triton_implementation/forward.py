82712a31c5d6dcbd2419a7afdc6bdc7053bee5d1
Mayank Mishra <mayank31398@gmail.com>
2025-04-07 20:18:22 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/rnn/triton_implementation/forward.py
+++ cute_kernels/kernels/rnn/triton_implementation/forward.py
@@ -1,24 +1,80 @@
+import torch
 import triton
 import triton.language as tl
+
+from ....constants import LIBRARY_NAME
+from ....math import ceil_divide
+from ....utils import cute_op
+
+
+_KERNEL_NAME = "rnn_forward_triton"
 
 
 @triton.jit
 def _rnn_forward_triton_kernel(
-    input_ptr,
-    starting_state_ptr,
-    weight_input_ptr,
-    weight_state_ptr,
+    x_ptr,
+    W_ptr,
     bias_ptr,
+    y_ptr,
+    input_state_ptr,
     B,
     S,
+    N,
     H,
     I,
     BLOCK_SIZE_B: tl.constexpr,
+    BLOCK_SIZE_H: tl.constexpr,
     BLOCK_SIZE_I: tl.constexpr,
+    allow_tf32: tl.constexpr,
 ):
-    num_programs = tl.num_programs(axis=0)
+    pid_b = tl.program_id(axis=0)
+    pid_n = tl.program_id(axis=1)
+    pid_i = tl.program_id(axis=2)
 
-    num_programs_bs = B * S
+    indices_b = pid_b * BLOCK_SIZE_B + tl.arange(0, BLOCK_SIZE_B)
+    indices_i = pid_i * BLOCK_SIZE_I + tl.arange(0, BLOCK_SIZE_I)
+
+    mask_b = indices_b < B
+    mask_i = indices_i < I
+
+    for s in range(S):
+        accumulator = tl.zeros((BLOCK_SIZE_B, BLOCK_SIZE_I), dtype=tl.float32)
+
+        for h in range(tl.cdiv(H, BLOCK_SIZE_H)):
+            indices_h = h * BLOCK_SIZE_H + tl.arange(0, BLOCK_SIZE_H)
+            mask_h = indices_h < H
+
+            indices = pid_n * I * H + indices_i[:, None] * H + indices_h[None, :]
+            mask = mask_i[:, None] & mask_h[None, :]
+
+            W_ptrs = W_ptr + indices
+            # W -> (BLOCK_SIZE_I, BLOCK_SIZE_H)
+            W = tl.load(W_ptrs, mask=mask)
+
+            indices = indices_b[:, None] * S * N * H + s * N * H + pid_i * H + indices_h[None, :]
+            mask = mask_b[:, None] & mask_h[None, :]
+
+            x_ptrs = x_ptr + indices
+            # x -> (BLOCK_SIZE_B, BLOCK_SIZE_H)
+            x = tl.load(x_ptrs, mask=mask)
+
+            accumulator += tl.dot(x, W.T, allow_tf32=allow_tf32, out_dtype=tl.float32)
 
 
-def rnn_forward_triton(): ...
+@cute_op(f"{LIBRARY_NAME}::{_KERNEL_NAME}", mutates_args={"y", "output_state"})
+def rnn_forward_triton(
+    x: torch.Tensor,
+    weight: torch.Tensor,
+    bias: torch.Tensor,
+    y: torch.Tensor,
+    input_state: torch.Tensor | None,
+    output_state: torch.Tensor,
+    BLOCK_SIZE_B: int,
+    BLOCK_SIZE_N: int,
+) -> None:
+    B, S, N, H = x.size()
+
+    with torch.device(x.device):
+        _rnn_forward_triton_kernel[ceil_divide(B, BLOCK_SIZE_B), N, ceil_divide(I, BLOCK_SIZE_I)](
+            input_ptr=x, W_ptr=weight, bias_ptr=bias, y_ptr=y, input_state_ptr=input_state
+        )
