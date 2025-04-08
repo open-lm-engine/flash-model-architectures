e065355f7749a31cddb18f6a8837df9b21be1e9b
Mayank Mishra <mayank31398@gmail.com>
2025-04-08 14:53:08 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/triton_math.py
+++ cute_kernels/triton_math.py
@@ -8,8 +8,13 @@ MIN_EXP_FP32 = -88.3762626647949
 
 @triton.jit
 def sigmoid(x):
+    dtype = x.dtype
+
     x = x.to(tl.float32)
     x = max(MIN_EXP_FP32, x)
     x = min(MAX_EXP_FP32, x)
     x = 1 / (1 + tl.exp(-x))
+
+    x = x.to(dtype)
+
     return x
