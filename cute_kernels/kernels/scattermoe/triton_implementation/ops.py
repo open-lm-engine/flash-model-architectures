e6e3675c36c00120679af9e6d6cc931a7727c2a9
Mayank Mishra <mayank31398@gmail.com>
2025-04-13 17:33:55 -0400

    op
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/scattermoe/triton_implementation/ops.py
+++ cute_kernels/kernels/scattermoe/triton_implementation/ops.py
@@ -28,6 +28,7 @@ def expert_boundaries(sorted_experts_idxs: torch.Tensor, k: int) -> torch.Tensor
     return expert_boundaries_end
 
 
+@cute_op(f"{LIBRARY_NAME}::scatter2scatter", mutates_args={"out"})
 def scatter2scatter(
     X: torch.Tensor,
     W: torch.Tensor,
@@ -47,7 +48,7 @@ def scatter2scatter(
         ceil_divide(sorted_expert_idxs.size(0), meta["BLOCK_M"]) * ceil_divide(meta["N"], meta["BLOCK_N"]),
     )
 
-    with torch.cuda.device(X.device):
+    with torch.device(X.device):
         scatter2scatter_triton_kernel[grid](
             # X_ptr, stride_xm, stride_xk,
             X,
@@ -77,10 +78,11 @@ def scatter2scatter(
         )
 
 
+@cute_op(f"{LIBRARY_NAME}::group_bwd_W", mutates_args={"DW"})
 def group_bwd_W(DY: torch.Tensor, X: torch.Tensor, expert_offsets: torch.Tensor, DW: torch.Tensor, E: int) -> None:
     grid = lambda meta: (E * ceil_divide(meta["K"], meta["BLOCK_K"]), ceil_divide(meta["N"], meta["BLOCK_N"]))
 
-    with torch.cuda.device(X.device):
+    with torch.device(X.device):
         groupXtY_triton_kernel[grid](
             # DY_ptr, stride_dym, stride_dyk,
             DY,
@@ -106,6 +108,7 @@ def group_bwd_W(DY: torch.Tensor, X: torch.Tensor, expert_offsets: torch.Tensor,
         )
 
 
+@cute_op(f"{LIBRARY_NAME}::group", mutates_args={"out"})
 def group(
     A: torch.Tensor,
     sorted_expert_idxs: torch.Tensor,
@@ -119,7 +122,7 @@ def group(
 
     grid = lambda meta: (triton.cdiv(meta["N"], meta["BLOCK_N"]),)
 
-    with torch.cuda.device(A.device):
+    with torch.device(A.device):
         group_triton_kernel[grid](
             # A_ptr, stride_an, stride_ai,
             A,
