ba0b761d6501d91ec577f717f39b3c41f49591f9
Mayank Mishra <mayank31398@gmail.com>
2025-04-12 22:46:50 -0400

    op
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/swiglu_unchunked/forward.py
+++ cute_kernels/kernels/swiglu_unchunked/forward.py
@@ -1,9 +1,10 @@
 import torch
 
 from ...cutotune import cutotune
-from ...math import divide_if_divisible
+from ...math import ceil_divide, divide_if_divisible
+from ...utils import get_num_elements_and_hidden_size
 from .parameters import get_cutotune_parameters
-from .triton_implementation import swiglu_unchunked_forward_triton
+from .triton_implementation import _swiglu_unchunked_forward_triton_kernel
 
 
 @cutotune(**get_cutotune_parameters())
@@ -12,7 +13,17 @@ def _forward(x: torch.Tensor, kernel_backend: str, BLOCK_SIZE_B: int, BLOCK_SIZE
     output = torch.empty(*x.size()[:-1], divide_if_divisible(H, 2), device=x.device, dtype=x.dtype)
 
     if kernel_backend == "triton":
-        swiglu_unchunked_forward_triton(x=x, output=output, BLOCK_SIZE_B=BLOCK_SIZE_B, BLOCK_SIZE_H=BLOCK_SIZE_H)
+        B, H = get_num_elements_and_hidden_size(x)
+
+        with torch.device(x.device):
+            _swiglu_unchunked_forward_triton_kernel[(ceil_divide(B, BLOCK_SIZE_B), ceil_divide(H, BLOCK_SIZE_H))](
+                x_ptr=x,
+                output_ptr=output,
+                B=B,
+                H=H,
+                BLOCK_SIZE_B=BLOCK_SIZE_B,
+                BLOCK_SIZE_H=BLOCK_SIZE_H,
+            )
     else:
         raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
 
