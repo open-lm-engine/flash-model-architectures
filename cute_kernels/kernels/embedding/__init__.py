5fefef1838cf23f188c5e20ac0fb2abbf40fc53b
Mayank Mishra <mayank31398@gmail.com>
2025-04-14 03:12:20 -0400

    op
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/embedding/__init__.py
+++ cute_kernels/kernels/embedding/__init__.py
@@ -1,38 +1,40 @@
 import torch
 
 from ...cutotune import CutoTuneParameter
+from ...math import ceil_divide
 from ...utils import ensure_contiguous
-from .backward import _backward
-from .forward import _forward
 from .torch_implementation import embedding_torch
+from .triton_implementation import _embedding_backward_triton_kernel, _embedding_forward_triton_kernel
 
 
 class _Embedding_Cute(torch.autograd.Function):
     @staticmethod
     @ensure_contiguous
-    def forward(
-        ctx,
-        input_ids: torch.Tensor,
-        weight: torch.Tensor,
-        kernel_backend_forward: str,
-        kernel_backend_backward: str,
-        BLOCK_SIZE_B_forward: int,
-        BLOCK_SIZE_B_backward: int,
-        BLOCK_SIZE_H_forward: int,
-        BLOCK_SIZE_H_backward: int,
-    ) -> torch.Tensor:
-        output = _forward(
-            input_ids=input_ids,
-            weight=weight,
-            kernel_backend=kernel_backend_forward,
-            BLOCK_SIZE_B=BLOCK_SIZE_B_forward,
-            BLOCK_SIZE_H=BLOCK_SIZE_H_forward,
-        )
+    def forward(ctx, input_ids: torch.Tensor, weight: torch.Tensor) -> torch.Tensor:
+        num_elements = input_ids.numel()
+        hidden_size = weight.size(-1)
+
+        output = torch.empty(num_elements, hidden_size, dtype=weight.dtype, device=input_ids.device)
+
+        num_elements = input_ids.numel()
+        hidden_size = weight.size(-1)
+
+        with torch.device(input_ids.device):
+            _embedding_forward_triton_kernel[
+                (ceil_divide(num_elements, BLOCK_SIZE_B), ceil_divide(hidden_size, BLOCK_SIZE_H))
+            ](
+                x_ptr=input_ids,
+                weight_ptr=weight,
+                output_ptr=output,
+                B=num_elements,
+                H=hidden_size,
+                BLOCK_SIZE_B=BLOCK_SIZE_B,
+                BLOCK_SIZE_H=BLOCK_SIZE_H,
+            )
+
+        output = output.view(*input_ids.size(), hidden_size)
 
         ctx.save_for_backward(input_ids, weight)
-        ctx.kernel_backend_backward = kernel_backend_backward
-        ctx.BLOCK_SIZE_B_backward = BLOCK_SIZE_B_backward
-        ctx.BLOCK_SIZE_H_backward = BLOCK_SIZE_H_backward
 
         return output
 
@@ -40,18 +42,28 @@ class _Embedding_Cute(torch.autograd.Function):
     @ensure_contiguous
     def backward(ctx, output_grad: torch.Tensor) -> torch.Tensor:
         input_ids, weight = ctx.saved_tensors
-        kernel_backend_backward = ctx.kernel_backend_backward
-        BLOCK_SIZE_B_backward = ctx.BLOCK_SIZE_B_backward
-        BLOCK_SIZE_H_backward = ctx.BLOCK_SIZE_H_backward
+        weight_grad = torch.zeros_like(weight)
 
-        weight_grad = _backward(
-            input_ids=input_ids,
-            weight=weight,
-            output_grad=output_grad,
-            kernel_backend=kernel_backend_backward,
-            BLOCK_SIZE_B=BLOCK_SIZE_B_backward,
-            BLOCK_SIZE_H=BLOCK_SIZE_H_backward,
-        )
+        num_elements = input_ids.numel()
+        hidden_size = weight_grad.size(-1)
+
+        accumulate_in_fp32 = weight_grad.dtype == torch.bfloat16
+        if accumulate_in_fp32:
+            weight_grad = weight_grad.float()
+
+        with torch.device(input_ids.device):
+            _embedding_backward_triton_kernel[
+                (ceil_divide(num_elements, BLOCK_SIZE_B), ceil_divide(hidden_size, BLOCK_SIZE_H))
+            ](
+                x_ptr=input_ids,
+                output_grad_ptr=output_grad,
+                weight_grad_ptr=weight_grad,
+                B=num_elements,
+                H=hidden_size,
+                accumulate_in_fp32=accumulate_in_fp32,
+                BLOCK_SIZE_B=BLOCK_SIZE_B,
+                BLOCK_SIZE_H=BLOCK_SIZE_H,
+            )
 
         return None, weight_grad, *[None] * 6
 
