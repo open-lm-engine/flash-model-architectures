68f9e23eb1f1952e42b1ccc69e0e6c46b4e84261
Mayank Mishra <mayank31398@gmail.com>
2025-04-14 02:59:36 -0400

    op
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/embedding/backward.py
+++ cute_kernels/kernels/embedding/backward.py
@@ -1,6 +1,7 @@
 import torch
 
-from .triton_implementation import embedding_backward_triton
+from ...math import ceil_divide
+from .triton_implementation import _embedding_backward_triton_kernel
 
 
 def _backward(
@@ -14,13 +15,26 @@ def _backward(
     weight_grad = torch.zeros_like(weight)
 
     if kernel_backend == "triton":
-        embedding_backward_triton(
-            input_ids=input_ids,
-            output_grad=output_grad,
-            weight_grad=weight_grad,
-            BLOCK_SIZE_B=BLOCK_SIZE_B,
-            BLOCK_SIZE_H=BLOCK_SIZE_H,
-        )
+        num_elements = input_ids.numel()
+        hidden_size = weight_grad.size(-1)
+
+        accumulate_in_fp32 = weight_grad.dtype == torch.bfloat16
+        if accumulate_in_fp32:
+            weight_grad = weight_grad.float()
+
+        with torch.device(input_ids.device):
+            _embedding_backward_triton_kernel[
+                (ceil_divide(num_elements, BLOCK_SIZE_B), ceil_divide(hidden_size, BLOCK_SIZE_H))
+            ](
+                x_ptr=input_ids,
+                output_grad_ptr=output_grad,
+                weight_grad_ptr=weight_grad,
+                B=num_elements,
+                H=hidden_size,
+                accumulate_in_fp32=accumulate_in_fp32,
+                BLOCK_SIZE_B=BLOCK_SIZE_B,
+                BLOCK_SIZE_H=BLOCK_SIZE_H,
+            )
     else:
         raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
 
