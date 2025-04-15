9860add823f45bf74d7a7650ffcafc99ce7c830c
Mayank Mishra <mayank31398@gmail.com>
2025-04-14 23:09:16 -0400

    op
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/rnn/__init__.py
+++ cute_kernels/kernels/rnn/__init__.py
@@ -1,8 +1,9 @@
 import torch
 
+from ...math import ceil_divide, get_next_power_of_2
 from ...utils import ensure_contiguous
 from .torch_implementation import rnn_torch
-from .triton_implementation import rnn_backward_triton, rnn_forward_triton
+from .triton_implementation import _rnn_forward_triton_kernel, rnn_backward_triton
 
 
 class _RNN_Cute(torch.autograd.Function):
@@ -17,15 +18,30 @@ class _RNN_Cute(torch.autograd.Function):
         BLOCK_SIZE_B_backward: int,
     ) -> tuple[torch.Tensor, torch.Tensor]:
         output = torch.empty_like(input)
+        B, S, N, H = input.size()
 
-        rnn_forward_triton(
-            input=input,
-            weight=weight,
-            output=output,
-            input_state=input_state,
-            allow_tf32=True,
-            BLOCK_SIZE_B=BLOCK_SIZE_B_forward,
-        )
+        BLOCK_SIZE_H = get_next_power_of_2(H)
+        BLOCK_SIZE_H = max(16, BLOCK_SIZE_H)
+
+        with torch.cuda.device(input.device):
+            _rnn_forward_triton_kernel[ceil_divide(B, BLOCK_SIZE_B_forward), N](
+                input_ptr=input,
+                input_stride_b=input.stride(0),
+                input_stride_s=input.stride(1),
+                input_stride_n=input.stride(2),
+                weight_ptr=weight,
+                weight_stride_n=weight.stride(0),
+                weight_stride_h=weight.stride(1),
+                has_input_state=input_state is not None,
+                input_state_ptr=input_state,
+                output_ptr=output,
+                B=B,
+                S=S,
+                H=H,
+                allow_tf32=True,
+                BLOCK_SIZE_B=BLOCK_SIZE_B_forward,
+                BLOCK_SIZE_H=BLOCK_SIZE_H,
+            )
 
         ctx.save_for_backward(weight, output, input_state)
         ctx.BLOCK_SIZE_B_backward = BLOCK_SIZE_B_backward
