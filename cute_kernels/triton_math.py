efe465271aeebc96dced94d496c4fb7fe48bdcaa
Mayank Mishra <mayank31398@gmail.com>
2025-04-14 23:10:43 -0400

    op
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/triton_math.py
+++ cute_kernels/triton_math.py
@@ -2,12 +2,8 @@ import triton
 import triton.language as tl
 
 
-MAX_EXP_FP32: tl.constexpr = 88.3762626647949
-MIN_EXP_FP32: tl.constexpr = -88.3762626647949
-
-
 @triton.jit
-def clamp(x):
+def clamp(x, MAX_EXP_FP32: tl.constexpr = 88.3762626647949, MIN_EXP_FP32: tl.constexpr = -88.3762626647949):
     x = max(MIN_EXP_FP32, x)
     x = min(MAX_EXP_FP32, x)
     return x
