a6d1029d12023d03109f77bffeb718f5ecd925ec
Mayank Mishra <mayank31398@gmail.com>
2025-04-15 12:47:51 -0400

    op
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/rnn/__init__.py
+++ cute_kernels/kernels/rnn/__init__.py
@@ -14,9 +14,9 @@ class _RNN_Cute(torch.autograd.Function):
         input: torch.Tensor,
         weight: torch.Tensor,
         input_state: torch.Tensor | None,
-        BLOCK_SIZE_B_forward: int,
         BLOCK_SIZE_B_backward: int,
     ) -> tuple[torch.Tensor, torch.Tensor]:
+        BLOCK_SIZE_B = 32
         output = torch.empty_like(input)
 
         rnn_forward_triton(
@@ -25,7 +25,7 @@ class _RNN_Cute(torch.autograd.Function):
             output=output,
             input_state=input_state,
             allow_tf32=True,
-            BLOCK_SIZE_B=BLOCK_SIZE_B_forward,
+            BLOCK_SIZE_B=BLOCK_SIZE_B,
         )
 
         ctx.save_for_backward(weight, output, input_state)
@@ -37,9 +37,10 @@ class _RNN_Cute(torch.autograd.Function):
     @ensure_contiguous
     def backward(ctx, output_grad: torch.Tensor) -> tuple[torch.Tensor]:
         weight, output, input_state = ctx.saved_tensors
-
         input_grad = torch.empty_like(output)
         weight_grad = torch.empty_like(weight)
+
+        BLOCK_SIZE_B = 32
 
         rnn_backward_triton(
             weight=weight,
@@ -49,17 +50,11 @@ class _RNN_Cute(torch.autograd.Function):
             input_grad=input_grad,
             weight_grad=weight_grad,
             allow_tf32=True,
-            BLOCK_SIZE_B=ctx.BLOCK_SIZE_B_backward,
+            BLOCK_SIZE_B=BLOCK_SIZE_B,
         )
 
         return input_grad, weight_grad, *[None] * 3
 
 
-def rnn_cute(
-    input: torch.Tensor,
-    weight: torch.Tensor,
-    input_state: torch.Tensor | None = None,
-    BLOCK_SIZE_B_forward: int = 32,
-    BLOCK_SIZE_B_backward: int = 32,
-) -> torch.Tensor:
-    return _RNN_Cute.apply(input, weight, input_state, BLOCK_SIZE_B_forward, BLOCK_SIZE_B_backward)
+def rnn_cute(input: torch.Tensor, weight: torch.Tensor, input_state: torch.Tensor | None = None) -> torch.Tensor:
+    return _RNN_Cute.apply(input, weight, input_state)
