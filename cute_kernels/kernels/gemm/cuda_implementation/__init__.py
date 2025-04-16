3164b755ec0647dfb377ddfc5b8a5ada8dd444a1
Mayank Mishra <mayank31398@gmail.com>
2025-04-15 21:48:37 -0400

    op
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/gemm/cuda_implementation/__init__.py
+++ cute_kernels/kernels/gemm/cuda_implementation/__init__.py
@@ -1,9 +1,7 @@
 import torch
 
 from ....constants import LIBRARY_NAME
-from ....cutotune import CutoTuneConfig, cutotune, get_cartesian_product_cutotune_configs
 from ....jit import cpp_jit
-from ....math import get_powers_of_2
 from ....utils import cute_op
 
 
@@ -13,15 +11,6 @@ _CUTLASS_KERNEL_NAME = "cutlass_gemm_cuda"
 _CUTLASS_TENSORCORE_MMA_KERNEL_NAME = "cutlass_tensorcore_mma_gemm_cuda"
 
 
-@cutotune(
-    get_cartesian_product_cutotune_configs(
-        BLOCK_SIZE_M=get_powers_of_2(4, 32),
-        BLOCK_SIZE_N=get_powers_of_2(4, 32),
-        condition=lambda **kwargs: kwargs["BLOCK_SIZE_M"] * kwargs["BLOCK_SIZE_N"] >= 32,
-    ),
-    default_config=CutoTuneConfig(dict(BLOCK_SIZE_M=16, BLOCK_SIZE_N=16)),
-    triggers={"A.dtype", "is_A_transposed", "is_B_transposed"},
-)
 @cute_op(f"{LIBRARY_NAME}::{_NAIVE_KERNEL_NAME}", mutates_args={"output"})
 @cpp_jit(_NAIVE_KERNEL_NAME)
 def naive_gemm_cuda(
@@ -41,11 +30,6 @@ def naive_gemm_cuda(
 ) -> None: ...
 
 
-@cutotune(
-    get_cartesian_product_cutotune_configs(BLOCK_SIZE=get_powers_of_2(8, 32)),
-    default_config=CutoTuneConfig(dict(BLOCK_SIZE=32)),
-    triggers={"A.dtype"},
-)
 @cute_op(f"{LIBRARY_NAME}::{_SHARED_MEMORY_KERNEL_NAME}", mutates_args={"output"})
 @cpp_jit(_SHARED_MEMORY_KERNEL_NAME)
 def shared_memory_gemm_cuda(
