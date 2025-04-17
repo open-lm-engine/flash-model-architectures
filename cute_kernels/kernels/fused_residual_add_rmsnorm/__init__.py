7a49d74df60bee5980a0b322ce3d8cda5ce1bb1a
Mayank Mishra <mayank31398@gmail.com>
2025-04-17 15:25:34 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/fused_residual_add_rmsnorm/__init__.py
+++ cute_kernels/kernels/fused_residual_add_rmsnorm/__init__.py
@@ -34,25 +34,21 @@ class _FusedResidualAddRMSNorm_Cute(torch.autograd.Function):
         if eps is None:
             eps = torch.finfo(x.dtype).eps
 
-        num_elements, hidden_size = get_num_elements_and_hidden_size(x)
+        B, H = get_num_elements_and_hidden_size(x)
 
         output = torch.empty_like(x)
         added_x_residual = torch.empty_like(x)
-        rmsnorm_denominator = (
-            None if memory_efficient else torch.empty(num_elements, device=x.device, dtype=torch.float32)
-        )
+        rmsnorm_denominator = None if memory_efficient else torch.empty(B, device=x.device, dtype=torch.float32)
 
         BLOCK_SIZE_B = 1
-        BLOCK_SIZE_H = get_next_power_of_2(hidden_size)
+        BLOCK_SIZE_H = get_next_power_of_2(H)
         assert BLOCK_SIZE_H <= MAX_TRITON_BLOCK_SIZE
 
-        num_elements, hidden_size = get_num_elements_and_hidden_size(x)
-
-        if BLOCK_SIZE_H < hidden_size:
+        if BLOCK_SIZE_H < H:
             raise ValueError(f"hidden_size should be more than the BLOCK_SIZE_H")
 
         with torch.cuda.device(x.device):
-            _fused_residual_add_rmsnorm_forward_triton_kernel[(ceil_divide(num_elements, BLOCK_SIZE_B),)](
+            _fused_residual_add_rmsnorm_forward_triton_kernel[(ceil_divide(B, BLOCK_SIZE_B),)](
                 x_ptr=x,
                 residual_ptr=residual,
                 has_weight=weight is not None,
@@ -64,8 +60,8 @@ class _FusedResidualAddRMSNorm_Cute(torch.autograd.Function):
                 added_x_residual_ptr=added_x_residual,
                 has_rmsnorm_denominator=rmsnorm_denominator is not None,
                 rmsnorm_denominator_ptr=rmsnorm_denominator,
-                B=num_elements,
-                H=hidden_size,
+                B=B,
+                H=H,
                 BLOCK_SIZE_B=BLOCK_SIZE_B,
                 BLOCK_SIZE_H=BLOCK_SIZE_H,
             )
