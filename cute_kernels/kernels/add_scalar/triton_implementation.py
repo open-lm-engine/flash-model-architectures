346c735e255e4ea993e4769adbf04e333a0e8555
Mayank Mishra <mayank31398@gmail.com>
2025-04-18 02:02:56 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/add_scalar/triton_implementation.py
+++ cute_kernels/kernels/add_scalar/triton_implementation.py
@@ -3,7 +3,7 @@ import triton.language as tl
 
 
 @triton.jit
-def _add_scalar_triton(x_ptr, y, output_ptr, indices, mask):
+def _add_scalar(x_ptr, y, output_ptr, indices, mask):
     x = tl.load(x_ptr + indices, mask=mask)
     tl.store(output_ptr + indices, x + y, mask=mask)
 
@@ -16,6 +16,6 @@ def _add_scalar_triton_kernel(x_ptr, y, output_ptr, N, BLOCK_SIZE: tl.constexpr)
     indices = BLOCK_ID * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
 
     if BLOCK_ID < NUM_BLOCKS - 1:
-        _add_scalar_triton(x_ptr=x_ptr, y=y, output_ptr=output_ptr, indices=indices, mask=None)
+        _add_scalar(x_ptr=x_ptr, y=y, output_ptr=output_ptr, indices=indices, mask=None)
     else:
-        _add_scalar_triton(x_ptr=x_ptr, y=y, output_ptr=output_ptr, indices=indices, mask=indices < N)
+        _add_scalar(x_ptr=x_ptr, y=y, output_ptr=output_ptr, indices=indices, mask=indices < N)
--- cute_kernels/kernels/add_tensor/triton_implementation.py
+++ cute_kernels/kernels/add_tensor/triton_implementation.py
@@ -3,7 +3,7 @@ import triton.language as tl
 
 
 @triton.jit
-def _add_tensor_triton(x_ptr, y_ptr, output_ptr, indices, mask):
+def _add_tensor(x_ptr, y_ptr, output_ptr, indices, mask):
     x = tl.load(x_ptr + indices, mask=mask)
     y = tl.load(y_ptr + indices, mask=mask)
     tl.store(output_ptr + indices, x + y, mask=mask)
@@ -17,6 +17,6 @@ def _add_tensor_triton_kernel(x_ptr, y_ptr, output_ptr, N, BLOCK_SIZE: tl.conste
     indices = BLOCK_ID * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
 
     if BLOCK_ID < NUM_BLOCKS - 1:
-        _add_tensor_triton(x_ptr=x_ptr, y_ptr=y_ptr, output_ptr=output_ptr, indices=indices, mask=None)
+        _add_tensor(x_ptr=x_ptr, y_ptr=y_ptr, output_ptr=output_ptr, indices=indices, mask=None)
     else:
-        _add_tensor_triton(x_ptr=x_ptr, y_ptr=y_ptr, output_ptr=output_ptr, indices=indices, mask=indices < N)
+        _add_tensor(x_ptr=x_ptr, y_ptr=y_ptr, output_ptr=output_ptr, indices=indices, mask=indices < N)
