b07f7b779f9968860110dfd9c33ea74a901d7295
Mayank Mishra <mayank31398@gmail.com>
2025-04-17 22:31:43 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/add_tensor/triton_implementation.py
+++ cute_kernels/kernels/add_tensor/triton_implementation.py
@@ -3,15 +3,23 @@ import triton.language as tl
 
 
 @triton.jit
-def _add_tensor_triton_kernel(x_ptr, y_ptr, output_ptr, N, BLOCK_SIZE: tl.constexpr):
-    pid = tl.program_id(axis=0)
-
-    indices = pid * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
-    mask = indices < N
-
+def _add_tensor_triton(x_ptr, y_ptr, output_ptr, indices, mask):
     x = tl.load(x_ptr + indices, mask=mask)
     y = tl.load(y_ptr + indices, mask=mask)
 
     output = x + y
 
     tl.store(output_ptr + indices, output, mask=mask)
+
+
+@triton.jit
+def _add_tensor_triton_kernel(x_ptr, y_ptr, output_ptr, N, BLOCK_SIZE: tl.constexpr):
+    BLOCK_ID = tl.program_id(axis=0)
+    NUM_BLOCKS = tl.num_programs(axis=0)
+
+    indices = BLOCK_ID * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+
+    if BLOCK_ID < NUM_BLOCKS - 1:
+        _add_tensor_triton(x_ptr=x_ptr, y_ptr=y_ptr, output_ptr=output_ptr, indices=indices, mask=None)
+    else:
+        _add_tensor_triton(x_ptr=x_ptr, y_ptr=y_ptr, output_ptr=output_ptr, indices=indices, mask=indices < N)
