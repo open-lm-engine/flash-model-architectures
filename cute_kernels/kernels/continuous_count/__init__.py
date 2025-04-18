865bfa7d42476f918edc5fa7aab6b3aa109d0463
Mayank Mishra <mayank31398@gmail.com>
2025-04-18 16:09:11 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/continuous_count/__init__.py
+++ cute_kernels/kernels/continuous_count/__init__.py
@@ -6,6 +6,18 @@ from .torch_implementation import continuous_count_torch
 
 @torch.no_grad()
 def continuous_count_cute(x: torch.Tensor, size: int) -> torch.Tensor:
+    """counts the number of occurances of the values [0, 1, ..., `size`) in the input tensor
+    (`size` is excluded). NOTE: the user is responsible for ensuring that the values lie in
+    the valid range, any values outside this range are ignored and not counted.
+
+    Args:
+        x (torch.Tensor): input tensor
+        size (int): values [0, 1, ..., `size`) are counted (`size` is excluded)
+
+    Returns:
+        torch.Tensor: output tensor
+    """
+
     if size == 1:
         return torch.tensor([x.numel()], dtype=torch.uint32, device=x.device)
 
