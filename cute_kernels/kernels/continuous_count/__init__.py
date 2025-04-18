2f6c36122c907e5c6d62ce05fdde615f8599262e
Mayank Mishra <mayank31398@gmail.com>
2025-04-17 22:43:10 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/continuous_count/__init__.py
+++ cute_kernels/kernels/continuous_count/__init__.py
@@ -13,15 +13,18 @@ def continuous_count_cute(x: torch.Tensor, size: int) -> torch.Tensor:
     assert x.dim() == 1, "x should be 1-dimensional"
     assert x.dtype in [torch.int32, torch.long]
 
+    BLOCK_SIZE = 1024
+    THREAD_BLOCK_CLUSTER_SIZE = 8
+
     output = torch.empty(size, dtype=torch.uint32, device=x.device)
 
     continuous_count_cuda(
         x=x,
         output=output,
         sm_count=get_sm_count(x.device),
-        thread_block_cluster_size=8,
+        thread_block_cluster_size=THREAD_BLOCK_CLUSTER_SIZE,
         size=size,
-        BLOCK_SIZE=1024,
+        BLOCK_SIZE=BLOCK_SIZE,
     )
 
     return output
