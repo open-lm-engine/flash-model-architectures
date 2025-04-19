686fdc9777e883e8352e4bc5d1142ec621808dcb
Mayank Mishra <mayank31398@gmail.com>
2025-04-19 19:27:21 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/kernels/swiglu_test.py
+++ tests/kernels/swiglu_test.py
@@ -24,7 +24,9 @@ class SwigluTest(TestCommons):
         x_kernel, x_expected = self.get_random_duplicated_tensors(size, device=device, dtype=dtype)
         y_kernel, y_expected = self.get_random_duplicated_tensors(size, device=device, dtype=dtype)
 
-        z_kernel = function(x_kernel, y_kernel, kernel_backend=kernel_backend)
+        z_kernel = function(
+            x_kernel, y_kernel, kernel_backend_forward=kernel_backend, kernel_backend_backward=kernel_backend
+        )
         z_expected = swiglu_torch(x_expected, y_expected)
 
         z_kernel.mean().backward()
