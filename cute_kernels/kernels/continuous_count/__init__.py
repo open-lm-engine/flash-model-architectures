4d7c0a33b8e5292f87215a5031c791ed04c5f3b4
Mayank Mishra <mayank31398@gmail.com>
2025-04-18 23:59:39 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/continuous_count/__init__.py
+++ cute_kernels/kernels/continuous_count/__init__.py
@@ -8,17 +8,16 @@ from .torch_implementation import continuous_count_torch
 def continuous_count_cute(
     x: torch.Tensor, size: int, *, BLOCK_SIZE: int = 1024, THREAD_BLOCK_CLUSTER_SIZE: int = 8
 ) -> torch.Tensor:
-    """counts the number of occurances of the values [0, 1, ..., `size`) in the input tensor
-    (`size` is excluded). NOTE: the user is responsible for ensuring that the values lie in
-    the valid range, any values outside this range are ignored and not counted.
+    """counts the number of occurances of the values [0, 1, ..., `size`) in the input tensor (`size` is excluded).
+        NOTE: the user is responsible for ensuring that the values lie in the valid range, any values outside this
+        range are ignored and not counted.
 
     Args:
         x (torch.Tensor): input tensor
         size (int): values [0, 1, ..., `size`) are counted (`size` is excluded)
         BLOCK_SIZE (int, optional): block size for CUDA backend. Defaults to 1024.
-        THREAD_BLOCK_CLUSTER_SIZE (int, optional): thread block cluster size refers to the
-            size of the cluster for hierarchical accumulation, 1 means no thread block
-            clusters. Defaults to 8.
+        THREAD_BLOCK_CLUSTER_SIZE (int, optional): thread block cluster size refers to the size of the cluster for
+            hierarchical accumulation, 1 means no thread block clusters. Defaults to 8.
 
     Returns:
         torch.Tensor: output tensor
