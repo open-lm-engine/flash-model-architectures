dcdbacb826739a267cff9b7cd674c9b1d5ab3aeb
Mayank Mishra <mayank31398@gmail.com>
2025-04-20 18:16:10 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/rnn/triton_implementation/forward.py
+++ cute_kernels/kernels/rnn/triton_implementation/forward.py
@@ -45,12 +45,10 @@ def _rnn_forward_triton_kernel(
         input_state = tl.zeros((BLOCK_SIZE_B, BLOCK_SIZE_H), dtype=input_ptr.dtype.element_ty)
 
     for _ in range(S):
-        input_state = tl.dot(input_state, weight, allow_tf32=allow_tf32).to(input_state.dtype)
-
         input_ptrs = input_ptr + indices
         input = tl.load(input_ptrs, mask=mask_bh, other=0)
 
-        input_state += input
+        input_state = tl.dot(input_state, weight, input, allow_tf32=allow_tf32).to(input_state.dtype)
         input_state = tanh(input_state)
 
         output_ptrs = output_ptr + indices
