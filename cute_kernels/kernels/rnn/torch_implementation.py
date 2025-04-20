eae462a0775539f26b30c963c7fcd2e1e814a144
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2025-04-20 01:07:07 -0400

    RNN varlen torch (#196)
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/rnn/torch_implementation.py
+++ cute_kernels/kernels/rnn/torch_implementation.py
@@ -7,35 +7,75 @@ def rnn_torch(
     weight: torch.Tensor,
     input_state: torch.Tensor | None = None,
     gradient_clipping: float | None = None,
+    cu_seqlens: torch.Tensor | None = None,
+    max_seqlen: torch.Tensor | int | None = None,
 ) -> torch.Tensor:
     if gradient_clipping is not None:
         raise NotImplementedError("rnn_torch doesn't support gradient_clipping")
 
-    B, S, N, H = input.size()
     output = torch.empty_like(input)
 
-    if input_state is None:
-        input_state = torch.zeros(B, N, H, device=input.device, dtype=input.dtype)
+    if cu_seqlens is None:
+        assert max_seqlen is None
+        B, S, N, H = input.size()
 
-    weight = weight.unsqueeze(0)
-    input = input.unsqueeze(-2)
+        if input_state is None:
+            input_state = torch.zeros(B, N, H, device=input.device, dtype=input.dtype)
 
-    # input -> (B, S, N, 1, H)
-    # weight -> (1, N, H, H)
-    # input_state -> (B, N, H)
+        weight = weight.unsqueeze(0)
+        input = input.unsqueeze(-2)
 
-    for s in range(S):
-        input_state = input_state.unsqueeze(-2)
+        # input -> (B, S, N, 1, H)
+        # weight -> (1, N, H, H)
+        # input_state -> (B, N, H)
 
-        # (B, N, 1, H) @ (1, N, H, H) + (B, N, 1, H)
-        input_state = input_state @ weight + input[:, s, ...]
+        for s in range(S):
+            input_state = input_state.unsqueeze(-2)
 
-        input_state = input_state.float()
-        input_state = F.tanh(input_state)
-        input_state = input_state.type_as(input)
+            # (B, N, 1, H) @ (1, N, H, H) + (B, N, 1, H)
+            input_state = input_state @ weight + input[:, s, ...]
 
-        input_state = input_state.squeeze(-2)
+            input_state = input_state.float()
+            input_state = F.tanh(input_state)
+            input_state = input_state.type_as(input)
 
-        output[:, s, ...] = input_state
+            input_state = input_state.squeeze(-2)
+
+            output[:, s, ...] = input_state
+    else:
+        assert max_seqlen is not None
+        B = cu_seqlens.numel() - 1
+        _, N, H = input.size()
+
+        if input_state is None:
+            input_state = torch.zeros(B, N, H, device=input.device, dtype=input.dtype)
+
+        weight = weight.unsqueeze(0)
+        input = input.unsqueeze(-2)
+
+        # input -> (cu_seqlens[-1], N, 1, H)
+        # weight -> (1, N, H, H)
+        # input_state -> (B, N, H)
+
+        offset = cu_seqlens[:-1]
+
+        for s in range(max_seqlen):
+            mask = offset < cu_seqlens[1:]
+            new_state = input_state.unsqueeze(-2)
+
+            # don't update the finished sequences
+            # (B, N, 1, H) @ (1, N, H, H) + (B, N, 1, H)
+            new_state = new_state[mask] @ weight + input[offset[mask], ...]
+
+            new_state = new_state.float()
+            new_state = F.tanh(new_state)
+            new_state = new_state.type_as(input)
+
+            new_state = new_state.squeeze(-2)
+
+            output[offset[mask], ...] = new_state
+            input_state[mask] = new_state
+
+            offset = offset + 1
 
     return output
--- tests/kernels/rnn_test.py
+++ tests/kernels/rnn_test.py
@@ -51,3 +51,45 @@ class RNNTest(TestCommons):
         self.assert_equal_tensors(y_kernel, y_expected, False, atol_float32=6e-3, rtol_float32=0)
         self.assert_equal_tensors(x_kernel.grad, x_expected.grad, False, atol_float32=6e-3, rtol_float32=0)
         self.assert_equal_tensors(weight_kernel.grad, weight_expected.grad, False, atol_float32=6e-3, rtol_float32=0)
+
+    @parameterized.expand(
+        TestCommons.make_args_matrix(
+            [torch.device("cuda")],
+            [torch.float32],
+            [[0, 7, 19, 27, 93]],  # cu_seqlens
+            [64],  # state_size
+            [4],  # num_heads
+        )
+    )
+    def test_rnn_varlen_torch(
+        self,
+        device: torch.device,
+        dtype: torch.dtype,
+        cu_seqlens: list[int],
+        state_size: int,
+        num_heads: int,
+    ) -> None:
+        set_seed(_SEED)
+
+        batch_size = len(cu_seqlens) - 1
+        total_length = sum(cu_seqlens)
+        cu_seqlens = torch.tensor(cu_seqlens, device=device)
+        max_seqlen = (cu_seqlens[1:] - cu_seqlens[:-1]).max()
+
+        x_kernel, x_expected = self.get_random_duplicated_tensors(
+            (batch_size, total_length, num_heads, state_size), device=device, dtype=dtype, std=0.01
+        )
+        weight_kernel, weight_expected = self.get_random_duplicated_tensors(
+            (num_heads, state_size, state_size), device=device, dtype=dtype, std=0.01
+        )
+
+        x_kernel = x_kernel.view(-1, num_heads, state_size)
+        y_kernel = rnn_torch(x_kernel, weight_kernel, cu_seqlens=cu_seqlens, max_seqlen=max_seqlen)
+        y_kernel = y_kernel.view(batch_size, -1, num_heads, state_size)
+
+        y_expected = []
+        for i in range(batch_size):
+            y_expected.append(rnn_torch(x_expected[cu_seqlens[i] : cu_seqlens[i + 1]], weight_expected))
+        y_expected = torch.cat(y_expected)
+
+        self.assert_equal_tensors(y_kernel, y_expected, False, atol_float32=7e-2, rtol_float32=0)
