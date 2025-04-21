d2ecbadf0fb1bca7c8a1f42f5ed8a8e7917e44ac
Mayank Mishra <mayank31398@gmail.com>
2025-04-20 22:47:42 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/swiglu/triton_implementation/backward.py
+++ cute_kernels/kernels/swiglu/triton_implementation/backward.py
@@ -5,14 +5,7 @@ from ....triton_math import sigmoid
 
 
 @triton.jit
-def _swiglu_backward_triton_kernel(
-    gate_ptr, up_ptr, output_grad_ptr, gate_grad_ptr, up_grad_ptr, N, BLOCK_SIZE: tl.constexpr
-):
-    pid = tl.program_id(axis=0)
-
-    indices = pid * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
-    mask = indices < N
-
+def _swiglu_backward(gate_ptr, up_ptr, output_grad_ptr, gate_grad_ptr, up_grad_ptr, indices, mask):
     gate = tl.load(gate_ptr + indices, mask=mask).to(tl.float32)
     up = tl.load(up_ptr + indices, mask=mask)
     output_grad = tl.load(output_grad_ptr + indices, mask=mask)
@@ -25,3 +18,34 @@ def _swiglu_backward_triton_kernel(
 
     tl.store(gate_grad_ptr + indices, gate_grad, mask=mask)
     tl.store(up_grad_ptr + indices, up_grad, mask=mask)
+
+
+@triton.jit
+def _swiglu_backward_triton_kernel(
+    gate_ptr, up_ptr, output_grad_ptr, gate_grad_ptr, up_grad_ptr, N, BLOCK_SIZE: tl.constexpr
+):
+    BLOCK_ID = tl.program_id(axis=0)
+    NUM_BLOCKS = tl.num_programs(axis=0)
+
+    indices = BLOCK_ID * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+
+    if BLOCK_ID < NUM_BLOCKS - 1:
+        _swiglu_backward(
+            gate_ptr=gate_ptr,
+            up_ptr=up_ptr,
+            output_grad_ptr=output_grad_ptr,
+            gate_grad_ptr=gate_grad_ptr,
+            up_grad_ptr=up_grad_ptr,
+            indices=indices,
+            mask=None,
+        )
+    else:
+        _swiglu_backward(
+            gate_ptr=gate_ptr,
+            up_ptr=up_ptr,
+            output_grad_ptr=output_grad_ptr,
+            gate_grad_ptr=gate_grad_ptr,
+            up_grad_ptr=up_grad_ptr,
+            indices=indices,
+            mask=indices < N,
+        )
