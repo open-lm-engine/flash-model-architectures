e12168941f175bae328706379801f7aa03a984e8
Mayank Mishra <mayank31398@gmail.com>
2025-04-22 02:56:58 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

---
+++ tests/kernels/pack_sequence_test.py
@@ -0,0 +1,38 @@
+from typing import Callable
+
+import torch
+from parameterized import parameterized
+
+from cute_kernels import KernelBackend, pack_sequence_cute, pack_sequence_torch
+
+from ..test_commons import TestCommons
+
+
+class PackSequenceTest(TestCommons):
+    @parameterized.expand(
+        TestCommons.make_args_matrix(
+            [(7, 1000, 12, 14)],  # size
+            [[0, 70, 170, 295, 393, 412, 515, 691]],  # cu_seqlens
+            [torch.device("cuda")],  # device
+            [TestCommons.get_dtypes()],  # dtype
+            [KernelBackend.cuda, KernelBackend.triton],  # kernel_backend
+            [pack_sequence_cute, torch.compile(pack_sequence_cute, fullgraph=True)],  # function
+        )
+    )
+    def test_pack_sequence(
+        self,
+        size: tuple[int],
+        cu_seqlens: list[int],
+        device: torch.device,
+        dtype: torch.dtype,
+        kernel_backend: KernelBackend,
+        function: Callable,
+    ) -> None:
+        x_kernel, x_expected = self.get_random_duplicated_tensors(size, device=device, dtype=dtype)
+        cu_seqlens = torch.tensor(cu_seqlens, device=device)
+        max_seqlen = (cu_seqlens[1:] - cu_seqlens[:-1]).max()
+
+        z_kernel = function(x_kernel, cu_seqlens, max_seqlen)
+        z_expected = pack_sequence_torch(x_expected, cu_seqlens, max_seqlen)
+
+        self.assert_equal_tensors(z_kernel, z_expected, True)
