45fd8b41885e6580090f2905d348aedbe84e38f4
Mayank Mishra <mayank31398@gmail.com>
2025-04-21 22:31:57 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/swiglu_unchunked/triton_implementation/forward.py
+++ cute_kernels/kernels/swiglu_unchunked/triton_implementation/forward.py
@@ -8,11 +8,11 @@ from ....triton_math import sigmoid
 def _swiglu_unchunked_forward_triton_kernel(
     x_ptr, output_ptr, B, H, BLOCK_SIZE_B: tl.constexpr, BLOCK_SIZE_H: tl.constexpr
 ):
-    pid_b = tl.program_id(axis=0)
-    pid_h = tl.program_id(axis=1)
+    BLOCK_ID_B = tl.program_id(axis=0)
+    BLOCK_ID_H = tl.program_id(axis=1)
 
-    indices_b = pid_b * BLOCK_SIZE_B + tl.arange(0, BLOCK_SIZE_B)
-    indices_h = pid_h * BLOCK_SIZE_H + tl.arange(0, BLOCK_SIZE_H)
+    indices_b = BLOCK_ID_B * BLOCK_SIZE_B + tl.arange(0, BLOCK_SIZE_B)
+    indices_h = BLOCK_ID_H * BLOCK_SIZE_H + tl.arange(0, BLOCK_SIZE_H)
 
     half_H = H >> 1
 
@@ -21,12 +21,11 @@ def _swiglu_unchunked_forward_triton_kernel(
     mask_bh = mask_b[:, None] & mask_h[None, :]
 
     up_ptrs = x_ptr + indices_b[:, None] * H + indices_h[None, :]
-    up = tl.load(up_ptrs, mask=mask_bh)
-
     gate_ptrs = up_ptrs + (H >> 1)
-    gate = tl.load(gate_ptrs, mask=mask_bh).to(tl.float32)
+    output_ptrs = output_ptr + indices_b[:, None] * half_H + indices_h[None, :]
 
+    up = tl.load(up_ptrs, mask=mask_bh)
+    gate = tl.load(gate_ptrs, mask=mask_bh).to(tl.float32)
     output = up * gate * sigmoid(gate)
 
-    output_ptrs = output_ptr + indices_b[:, None] * half_H + indices_h[None, :]
     tl.store(output_ptrs, output, mask=mask_bh)
