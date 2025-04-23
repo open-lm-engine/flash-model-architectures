d520e2243a23d698cb9fe3a8d1b294839da84804
Mayank Mishra <mayank31398@gmail.com>
2025-04-23 01:53:29 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/add_scalar/cuda_implementation/__init__.py
+++ cute_kernels/kernels/add_scalar/cuda_implementation/__init__.py
@@ -5,9 +5,6 @@ from ....jit import cpp_jit
 from ....utils import cute_op
 
 
-_KERNEL_NAME = "add_scalar_cuda"
-
-
-@cute_op(f"{LIBRARY_NAME}::{_KERNEL_NAME}", mutates_args={"output"})
-@cpp_jit(_KERNEL_NAME)
+@cute_op(f"{LIBRARY_NAME}::add_scalar_cuda", mutates_args={"output"})
+@cpp_jit("add_scalar_cuda")
 def add_scalar_cuda(x: torch.Tensor, y: float, output: torch.Tensor, BLOCK_SIZE: int) -> None: ...
