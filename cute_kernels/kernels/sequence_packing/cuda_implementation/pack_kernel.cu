519b2e6cb5e0620f4680d46ff57566885550e042
Mayank Mishra <mayank31398@gmail.com>
2025-04-25 13:39:53 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/sequence_packing/cuda_implementation/pack_kernel.cu
+++ cute_kernels/kernels/sequence_packing/cuda_implementation/pack_kernel.cu
@@ -12,36 +12,36 @@ using uint32 = ck::uint32;
 using uint64 = ck::uint64;
 
 template <typename integer_t>
-inline __device__ void _load_seqlens(const integer_t *seqlens, integer_t *seqlens_shared, const uint32 &B) {
+inline __device__ void _load_seqlens(const integer_t *cu_seqlens, integer_t *cu_seqlens_shared, const uint32 &B) {
     constexpr uint32 num_elements_per_thread = ck_mem::get_num_elements_for_vector_load_stores<integer_t>();
     const uint32 B_vec = B / num_elements_per_thread;
 
     for (uint32 i = threadIdx.x; i < B_vec; i += blockDim.x) {
-        uint32 *cu_seqlens_loaded = ck_mem::load_128_bits<integer_t>(seqlens, i);
+        uint32 *cu_seqlens_loaded = ck_mem::load_128_bits<integer_t>(cu_seqlens, i);
 
         for (uint32 j = 0; j < num_elements_per_thread; j++) {
-            seqlens_shared[i * num_elements_per_thread + j] = cu_seqlens_loaded[j];
+            cu_seqlens_shared[i * num_elements_per_thread + j] = cu_seqlens_loaded[j];
         }
     }
 
     // use first warp to load remaining elements
     const uint32 index = B_vec * num_elements_per_thread + threadIdx.x;
     if (index < B) {
-        seqlens_shared[index] = seqlens[index];
+        cu_seqlens_shared[index] = cu_seqlens[index];
     }
 }
 
 template <typename scalar_t, typename integer_t, bool is_max_seqlen_tensor, std::string padding_side>
 __global__ void pack_sequence_cuda_kernel(const scalar_t *x,
                                           scalar_t *output,
-                                          const uint32 *seqlens,
+                                          const uint32 *cu_seqlens,
                                           const uint32 *max_seqlen_tensor,
                                           const uint32 max_seqlen,
                                           const uint32 B,
                                           const uint32 S,
                                           const uint32 N) {
     __shared__ integer_t max_seqlen_shared;
-    integer_t *seqlens_shared = ck_mem::get_dynamic_shared_memory<integer_t>();
+    integer_t *cu_seqlens_shared = ck_mem::get_dynamic_shared_memory<integer_t>();
 
     // load max_seqlen into shared memory using 1st thread of each threadblock
     if (threadIdx.x == 0) {
@@ -52,30 +52,27 @@ __global__ void pack_sequence_cuda_kernel(const scalar_t *x,
         }
     }
 
-    _load_seqlens<integer_t>(seqlens, seqlens_shared, B);
+    _load_cu_seqlens<integer_t>(seqlens, cu_seqlens_shared, B);
 
     __syncthreads();
 
     const uint32 s = blockIdx.x;
     const uint32 b = blockIdx.y;
-    const uint32 seqlens_b = seqlens_shared[b];
+    const uint32 seqlens_b = cu_seqlens_shared[b + 1] - cu_seqlens_shared[b];
 
-    bool is_pad_token = false;
     if (padding_side == "left") {
         if (s < S - seqlens_b) {
-            is_pad_token = true;
+            return;
+        } else {
+            return;
         }
     } else if (padding_side == "right") {
-        if (s > seqlens_b) {
-            is_pad_token = true;
+        if (s >= seqlens_b) {
+            return;
+        } else {
+            return;
         }
     }
-
-    if (is_pad_token) {
-        return;
-    }
-
-    for
 }
 
 void pack_sequence_cuda(const torch::Tensor &x,
@@ -118,7 +115,7 @@ void pack_sequence_cuda(const torch::Tensor &x,
             TORCH_CHECK(num_elements % N_per_thread == 0);
 
             if (max_seqlen_tensor.has_value()) {
-                pack_sequence_cuda_kernel<scalar_t, uint32, true>
+                pack_sequence_cuda_kernel<scalar_t, uint32, true, "left">
                     <<<NUM_BLOCKS, BLOCK_SIZE, shared_memory_size>>>(x.data_ptr<scalar_t>(),
                                                                      output.data_ptr<scalar_t>(),
                                                                      seqlens.data_ptr<uint32>(),
@@ -128,7 +125,7 @@ void pack_sequence_cuda(const torch::Tensor &x,
                                                                      S,
                                                                      N);
             } else {
-                pack_sequence_cuda_kernel<scalar_t, uint32, false>
+                pack_sequence_cuda_kernel<scalar_t, uint32, false, "left">
                     <<<NUM_BLOCKS, BLOCK_SIZE, shared_memory_size>>>(x.data_ptr<scalar_t>(),
                                                                      output.data_ptr<scalar_t>(),
                                                                      seqlens.data_ptr<uint32>(),
