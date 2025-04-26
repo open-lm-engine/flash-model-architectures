090f64d8895b3f43425cf0da2ba8248108350939
Mayank Mishra <mayank31398@gmail.com>
2025-04-26 08:07:04 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/fused_residual_add_rmsnorm/__init__.py
+++ cute_kernels/kernels/fused_residual_add_rmsnorm/__init__.py
@@ -1,8 +1,6 @@
 import torch
 
-from ...constants import MAX_TRITON_BLOCK_SIZE
-from ...math import ceil_divide, get_next_power_of_2
-from ...utils import ensure_contiguous, get_num_elements_and_hidden_size, get_sm_count
+from ...utils import ensure_contiguous, get_num_elements_and_hidden_size
 from .torch_implementation import fused_residual_add_rmsnorm_torch
 from .triton_implementation import (
     fused_residual_add_rmsnorm_backward_triton,
@@ -65,33 +63,19 @@ class _FusedResidualAddRMSNorm_Cute(torch.autograd.Function):
         residual_grad = torch.empty_like(added_x_residual)
         weight_grad = None if weight is None else torch.zeros_like(weight, dtype=torch.float32)
 
-        B, H = get_num_elements_and_hidden_size(added_x_residual)
-        multiplier = ctx.multiplier
-        BLOCK_SIZE_B = ctx.BLOCK_SIZE_B_backward
-
-        sm_count = get_sm_count(added_x_residual.device)
-        num_programs = min(sm_count, ceil_divide(B, BLOCK_SIZE_B))
-
-        with torch.cuda.device(added_x_residual.device):
-            fused_residual_add_rmsnorm_backward_triton[num_programs,](
-                added_x_residual_ptr=added_x_residual,
-                has_weight=weight is not None,
-                weight_ptr=weight,
-                output_grad_ptr=output_grad,
-                added_x_residual_grad_ptr=added_x_residual_grad,
-                x_grad_ptr=x_grad,
-                residual_grad_ptr=residual_grad,
-                weight_grad_ptr=weight_grad,
-                eps=ctx.eps,
-                has_multiplier=multiplier is not None and multiplier != 1,
-                multiplier=multiplier,
-                has_rmsnorm_denominator=rmsnorm_denominator is not None,
-                rmsnorm_denominator_ptr=rmsnorm_denominator,
-                B=B,
-                H=H,
-                BLOCK_SIZE_B=BLOCK_SIZE_B,
-                BLOCK_SIZE_H=ctx.BLOCK_SIZE_H,
-            )
+        fused_residual_add_rmsnorm_backward_triton(
+            added_x_residual=added_x_residual,
+            weight=weight,
+            output_grad=output_grad,
+            added_x_residual_grad=added_x_residual_grad,
+            rmsnorm_denominator=rmsnorm_denominator,
+            x_grad=x_grad,
+            residual_grad=residual_grad,
+            weight_grad=weight_grad,
+            eps=ctx.eps,
+            multiplier=ctx.multiplier,
+            BLOCK_SIZE_B=ctx.BLOCK_SIZE_B_backward,
+        )
 
         if weight_grad is not None:
             weight_grad = weight_grad.type_as(weight)
