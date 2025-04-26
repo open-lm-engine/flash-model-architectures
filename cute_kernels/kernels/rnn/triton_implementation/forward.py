6285aa497246707744de49b2a4892c3e2490e595
Mayank Mishra <mayank31398@gmail.com>
2025-04-26 14:56:14 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/rnn/triton_implementation/forward.py
+++ cute_kernels/kernels/rnn/triton_implementation/forward.py
@@ -72,11 +72,12 @@ def rnn_forward_triton(
     BLOCK_SIZE_B: int,
 ) -> None:
     B, S, N, H = input.size()
+
     BLOCK_SIZE_H = get_next_power_of_2(H)
     BLOCK_SIZE_H = max(16, BLOCK_SIZE_H)
 
     with torch.device(input.device):
-        rnn_forward_triton[ceil_divide(B, BLOCK_SIZE_B), N](
+        rnn_forward_triton_kernel[ceil_divide(B, BLOCK_SIZE_B), N](
             input_ptr=input,
             input_stride_b=input.stride(0),
             input_stride_s=input.stride(1),
