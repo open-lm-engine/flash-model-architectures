562365c3c378e887b2f413fcef22d8b5d02878f6
Mayank Mishra <mayank31398@gmail.com>
2025-04-26 20:16:21 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/jit.py
+++ cute_kernels/jit.py
@@ -69,7 +69,7 @@ def _get_cpp_function(function_name: str, source_files: list[str], build_directo
             verbose=True,
         )
 
-    rmtree(build_directory, ignore_errors=True)
+        rmtree(build_directory, ignore_errors=True)
 
     return getattr(module, function_name)
 
