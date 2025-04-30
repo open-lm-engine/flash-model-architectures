646300e2aa382968aade62042ea9206d1ddc15c5
Mayank Mishra <mayank31398@gmail.com>
2025-04-30 19:26:14 -0400

    cleanup

--- cute_kernels/include/dispatch.h
+++ cute_kernels/include/dispatch.h
@@ -2,12 +2,14 @@
 
 #include <torch/extension.h>
 
-#define AT_DISPATCH_CUSTOM_FLOAT_TYPES(TYPE, NAME, ...)                            \
-    AT_DISPATCH_SWITCH(TYPE,                                                       \
-                       NAME,                                                       \
-                       AT_DISPATCH_CASE(at::ScalarType::Half, __VA_ARGS__)         \
-                           AT_DISPATCH_CASE(at::ScalarType::BFloat16, __VA_ARGS__) \
-                               AT_DISPATCH_CASE(at::ScalarType::Float, __VA_ARGS__))
+#define DISPATCH_CASE(enum_type, scalar_name, ...) AT_PRIVATE_CASE_TYPE_USING_HINT(enum_type, scalar_name, __VA_ARGS__)
+
+#define AT_DISPATCH_CUSTOM_FLOAT_TYPES(TYPE, NAME, SCALAR_NAME, ...)                         \
+    AT_DISPATCH_SWITCH(TYPE,                                                                 \
+                       NAME,                                                                 \
+                       DISPATCH_CASE(at::ScalarType::Half, SCALAR_NAME, __VA_ARGS__)         \
+                           DISPATCH_CASE(at::ScalarType::BFloat16, SCALAR_NAME, __VA_ARGS__) \
+                               DISPATCH_CASE(at::ScalarType::Float, SCALAR_NAME, __VA_ARGS__))
 
 #define AT_DISPATCH_CUSTOM_INT_TYPES(TYPE, NAME, ...)                     \
     AT_DISPATCH_SWITCH(TYPE,                                              \
