bd40e6fcba1a2db893acc33a5d91ebb7bbdc2b22
Mayank Mishra <mayank31398@gmail.com>
2025-04-30 18:58:35 -0400

    cleanup

--- cute_kernels/include/dtypes/alias.h
+++ cute_kernels/include/dtypes/alias.h
@@ -12,14 +12,13 @@
                            AT_DISPATCH_CASE(at::ScalarType::BFloat16, __VA_ARGS__) \
                                AT_DISPATCH_CASE(at::ScalarType::Float, __VA_ARGS__))
 
+#define AT_DISPATCH_CUSTOM_INT_TYPES(TYPE, NAME, ...)                     \
+    AT_DISPATCH_SWITCH(TYPE,                                              \
+                       NAME,                                              \
+                       AT_DISPATCH_CASE(at::ScalarType::Int, __VA_ARGS__) \
+                           AT_DISPATCH_CASE(at::ScalarType::Long, __VA_ARGS__))
+
 namespace cute_kernels {
-#define _AT_DISPATCH_CASE_CUSTOM_INT_TYPES(...)        \
-    AT_DISPATCH_CASE(at::ScalarType::Int, __VA_ARGS__) \
-    AT_DISPATCH_CASE(at::ScalarType::Long, __VA_ARGS__)
-
-#define AT_DISPATCH_CUSTOM_INT_TYPES(TYPE, NAME, ...) \
-    AT_DISPATCH_SWITCH(TYPE, NAME, _AT_DISPATCH_CASE_CUSTOM_INT_TYPES(__VA_ARGS__))
-
     // define dtype aliases
     using fp64 = double;
     using fp64_2 = double2;
