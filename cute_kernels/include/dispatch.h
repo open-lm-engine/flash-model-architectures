87023b898189d7f43fcc7d5e1563690a401708c2
Mayank Mishra <mayank31398@gmail.com>
2025-04-30 19:30:26 -0400

    cleanup

--- cute_kernels/include/dispatch.h
+++ cute_kernels/include/dispatch.h
@@ -11,8 +11,8 @@
                            DISPATCH_CASE(at::ScalarType::BFloat16, SCALAR_NAME, __VA_ARGS__) \
                                DISPATCH_CASE(at::ScalarType::Float, SCALAR_NAME, __VA_ARGS__))
 
-#define DISPATCH_INT_KERNEL(TYPE, NAME, ...)                              \
-    AT_DISPATCH_SWITCH(TYPE,                                              \
-                       NAME,                                              \
-                       AT_DISPATCH_CASE(at::ScalarType::Int, __VA_ARGS__) \
-                           AT_DISPATCH_CASE(at::ScalarType::Long, __VA_ARGS__))
+#define DISPATCH_INT_KERNEL(TYPE, NAME, SCALAR_NAME, ...)                           \
+    AT_DISPATCH_SWITCH(TYPE,                                                        \
+                       NAME,                                                        \
+                       DISPATCH_CASE(at::ScalarType::Int, SCALAR_NAME, __VA_ARGS__) \
+                           DISPATCH_CASE(at::ScalarType::Long, SCALAR_NAME, __VA_ARGS__))
