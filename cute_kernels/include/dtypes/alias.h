bf007d6614eb24e795d4f0e1c6b285c828a3a10e
Mayank Mishra <mayank31398@gmail.com>
2025-04-30 18:17:47 -0400

    cleanup

--- cute_kernels/include/dtypes/alias.h
+++ cute_kernels/include/dtypes/alias.h
@@ -6,20 +6,20 @@
 #include <torch/extension.h>
 
 namespace cute_kernels {
-#define AT_DISPATCH_CASE_CUSTOM_FLOAT_TYPES(...)            \
+#define _AT_DISPATCH_CASE_CUSTOM_FLOAT_TYPES(...)           \
     AT_DISPATCH_CASE(at::ScalarType::Half, __VA_ARGS__)     \
     AT_DISPATCH_CASE(at::ScalarType::BFloat16, __VA_ARGS__) \
     AT_DISPATCH_CASE(at::ScalarType::Float, __VA_ARGS__)
 
 #define AT_DISPATCH_CUSTOM_FLOAT_TYPES(TYPE, NAME, ...) \
-    AT_DISPATCH_SWITCH(TYPE, NAME, AT_DISPATCH_CASE_CUSTOM_FLOAT_TYPES(__VA_ARGS__))
+    AT_DISPATCH_SWITCH(TYPE, NAME, _AT_DISPATCH_CASE_CUSTOM_FLOAT_TYPES(__VA_ARGS__))
 
-#define AT_DISPATCH_CASE_CUSTOM_INT_TYPES(...)         \
+#define _AT_DISPATCH_CASE_CUSTOM_INT_TYPES(...)        \
     AT_DISPATCH_CASE(at::ScalarType::Int, __VA_ARGS__) \
     AT_DISPATCH_CASE(at::ScalarType::Long, __VA_ARGS__)
 
 #define AT_DISPATCH_CUSTOM_INT_TYPES(TYPE, NAME, ...) \
-    AT_DISPATCH_SWITCH(TYPE, NAME, AT_DISPATCH_CASE_CUSTOM_INT_TYPES(__VA_ARGS__))
+    AT_DISPATCH_SWITCH(TYPE, NAME, _AT_DISPATCH_CASE_CUSTOM_INT_TYPES(__VA_ARGS__))
 
     // define dtype aliases
     using fp64 = double;
