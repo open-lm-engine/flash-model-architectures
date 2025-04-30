0349f222e64e6925a545cd7d05b8630bec927fc6
Mayank Mishra <mayank31398@gmail.com>
2025-04-30 18:57:37 -0400

    cleanup

--- cute_kernels/include/dtypes/alias.h
+++ cute_kernels/include/dtypes/alias.h
@@ -5,15 +5,14 @@
 #include <cuda_runtime.h>
 #include <torch/extension.h>
 
+#define AT_DISPATCH_CUSTOM_FLOAT_TYPES(TYPE, NAME, ...)                            \
+    AT_DISPATCH_SWITCH(TYPE,                                                       \
+                       NAME,                                                       \
+                       AT_DISPATCH_CASE(at::ScalarType::Half, __VA_ARGS__)         \
+                           AT_DISPATCH_CASE(at::ScalarType::BFloat16, __VA_ARGS__) \
+                               AT_DISPATCH_CASE(at::ScalarType::Float, __VA_ARGS__))
+
 namespace cute_kernels {
-#define _AT_DISPATCH_CASE_CUSTOM_FLOAT_TYPES(...)           \
-    AT_DISPATCH_CASE(at::ScalarType::Half, __VA_ARGS__)     \
-    AT_DISPATCH_CASE(at::ScalarType::BFloat16, __VA_ARGS__) \
-    AT_DISPATCH_CASE(at::ScalarType::Float, __VA_ARGS__)
-
-#define AT_DISPATCH_CUSTOM_FLOAT_TYPES(TYPE, NAME, ...) \
-    AT_DISPATCH_SWITCH(TYPE, NAME, _AT_DISPATCH_CASE_CUSTOM_FLOAT_TYPES(__VA_ARGS__))
-
 #define _AT_DISPATCH_CASE_CUSTOM_INT_TYPES(...)        \
     AT_DISPATCH_CASE(at::ScalarType::Int, __VA_ARGS__) \
     AT_DISPATCH_CASE(at::ScalarType::Long, __VA_ARGS__)
