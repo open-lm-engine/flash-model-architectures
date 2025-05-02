846ac2024d5e91802297e22e9854afdc7bbdbed4
Mayank Mishra <mayank31398@gmail.com>
2025-05-02 02:39:56 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/swiglu_unchunked/__init__.py
+++ cute_kernels/kernels/swiglu_unchunked/__init__.py
@@ -3,7 +3,7 @@ import torch
 from ...math import ceil_divide, divide_if_divisible
 from ...utils import ensure_contiguous, get_num_elements_and_hidden_size
 from .torch_implementation import swiglu_unchunked_torch
-from .triton_implementation import swiglu_unchunked_backward_triton, swiglu_unchunked_forward_triton_kernel
+from .triton_implementation import swiglu_unchunked_backward_triton_kernel, swiglu_unchunked_forward_triton_kernel
 
 
 class _SwigluUnchunked_Cute(torch.autograd.Function):
@@ -44,13 +44,20 @@ class _SwigluUnchunked_Cute(torch.autograd.Function):
         x = ctx.saved_tensors[0]
         x_grad = torch.empty_like(x)
 
-        swiglu_unchunked_backward_triton(
-            x=x,
-            output_grad=output_grad,
-            x_grad=x_grad,
-            BLOCK_SIZE_B=ctx.BLOCK_SIZE_B_backward,
-            BLOCK_SIZE_H=ctx.BLOCK_SIZE_H_backward,
-        )
+        B, H = get_num_elements_and_hidden_size(x)
+        BLOCK_SIZE_B = ctx.BLOCK_SIZE_B_backward
+        BLOCK_SIZE_H = ctx.BLOCK_SIZE_H_backward
+
+        with torch.device(x.device):
+            swiglu_unchunked_backward_triton_kernel[ceil_divide(B, BLOCK_SIZE_B), ceil_divide(H, BLOCK_SIZE_H)](
+                x_ptr=x,
+                output_grad_ptr=output_grad,
+                x_grad_ptr=x_grad,
+                B=B,
+                H=H,
+                BLOCK_SIZE_B=BLOCK_SIZE_B,
+                BLOCK_SIZE_H=BLOCK_SIZE_H,
+            )
 
         return x_grad, *[None] * 4
 
