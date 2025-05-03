3ee059e3fa2d2e6e88082de5947352b966506a28
Mayank Mishra <mayank31398@gmail.com>
2025-05-03 17:12:35 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/rnn/triton_implementation/forward/kernel.py
+++ cute_kernels/kernels/rnn/triton_implementation/forward/kernel.py
@@ -17,6 +17,7 @@ def rnn_forward_triton_kernel(
     weight_stride_n,
     has_input_state: tl.constexpr,
     input_state_ptr,
+    input_state_stride_b,
     output_ptr,
     B,
     S,
@@ -37,13 +38,13 @@ def rnn_forward_triton_kernel(
     weight_ptrs = weight_ptr + pid_n * weight_stride_n + indices_h[:, None] * H + indices_h[None, :]
     weight = tl.load(weight_ptrs, mask=mask_h[:, None] & mask_h[None, :], other=0)
 
-    indices = indices_b[:, None] * input_stride_b + pid_n * H + indices_h[None, :]
-
     if has_input_state:
-        input_state_ptrs = input_state_ptr + indices
+        input_state_ptrs = input_state_ptr + indices_b[:, None] * input_state_stride_b + pid_n * H + indices_h[None, :]
         input_state = tl.load(input_state_ptrs, mask=mask_bh)
     else:
         input_state = tl.zeros((BLOCK_SIZE_B, BLOCK_SIZE_H), dtype=input_ptr.dtype.element_ty)
+
+    indices = indices_b[:, None] * input_stride_b + pid_n * H + indices_h[None, :]
 
     for _ in range(S):
         input_ptrs = input_ptr + indices
@@ -80,8 +81,9 @@ def rnn_forward_triton(
             input_stride_s=input.stride(1),
             weight_ptr=weight,
             weight_stride_n=weight.stride(0),
-            has_input_state=input_state is not None,
+            has_input_state=has_input_state,
             input_state_ptr=input_state,
+            input_state_stride_b=input_state.stride(0) if has_input_state else None,
             output_ptr=output,
             B=B,
             S=S,
