846a13981bbf5143bf8bee700ca6c7442fb5d880
Mayank Mishra <mayank31398@gmail.com>
2025-05-03 17:46:06 -0400

    update cute-kernels
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/rnn/triton_implementation/backward/varlen_kernel.py
+++ cute_kernels/kernels/rnn/triton_implementation/backward/varlen_kernel.py
@@ -13,15 +13,12 @@ from .kernel import _tanh_backward
 def rnn_varlen_backward_triton_kernel(
     weight_ptr,
     weight_stride_n,
-    weight_stride_h,
     output_ptr,
+    output_stride_b,
+    output_stride_s,
     has_input_state: tl.constexpr,
     input_state_ptr,
     input_state_stride_b,
-    input_state_stride_n,
-    output_stride_b,
-    output_stride_s,
-    output_stride_n,
     output_grad_ptr,
     input_grad_ptr,
     weight_grad_ptr,
@@ -46,16 +43,15 @@ def rnn_varlen_backward_triton_kernel(
     input_state_grad = tl.zeros((BLOCK_SIZE_B, BLOCK_SIZE_H), dtype=weight_ptr.dtype.element_ty)
     weight_grad = tl.zeros((BLOCK_SIZE_H, BLOCK_SIZE_H), dtype=tl.float32)
 
-    weight_ptrs = weight_ptr + pid_n * weight_stride_n + indices_h[:, None] * weight_stride_h + indices_h[None, :]
+    weight_ptrs = weight_ptr + pid_n * weight_stride_n + indices_h[:, None] * H + indices_h[None, :]
     weight = tl.load(weight_ptrs, mask=mask_h[:, None] & mask_h[None, :], other=0)
 
-    indices = (
-        indices_b[:, None] * output_stride_b + (S - 1) * output_stride_s + pid_n * output_stride_n + indices_h[None, :]
-    )
+    indices = indices_b[:, None] * output_stride_b + (S - 1) * output_stride_s + pid_n * H + indices_h[None, :]
 
     output_ptrs = output_ptr + indices
     output = tl.load(output_ptrs, mask=mask_bh, other=0)
 
+    # backward counting reduces 1 instruction since we need to compare s == 0, otherwise we have to compare s == S - 1
     for s in range(S - 1, -1, -1):
         output_grad_ptrs = output_grad_ptr + indices
         output_grad = tl.load(output_grad_ptrs, mask=mask_bh, other=0)
@@ -73,10 +69,7 @@ def rnn_varlen_backward_triton_kernel(
         if s == 0:
             if has_input_state:
                 output_ptrs = (
-                    input_state_ptr
-                    + indices_b[:, None] * input_state_stride_b
-                    + pid_n * input_state_stride_n
-                    + indices_h[None, :]
+                    input_state_ptr + indices_b[:, None] * input_state_stride_b + pid_n * H + indices_h[None, :]
                 )
                 output_prev = tl.load(output_ptrs, mask=mask_bh, other=0)
             else:
@@ -90,9 +83,7 @@ def rnn_varlen_backward_triton_kernel(
 
         indices -= output_stride_s
 
-    weight_grad_ptrs = (
-        weight_grad_ptr + pid_n * weight_stride_n + indices_h[:, None] * weight_stride_h + indices_h[None, :]
-    )
+    weight_grad_ptrs = weight_grad_ptr + pid_n * weight_stride_n + indices_h[:, None] * H + indices_h[None, :]
     tl.store(weight_grad_ptrs, weight_grad, mask=mask_h[:, None] & mask_h[None, :])
 
 
@@ -116,15 +107,12 @@ def rnn_varlen_backward_triton(
         rnn_backward_triton_kernel[ceil_divide(B, BLOCK_SIZE_B), N](
             weight_ptr=weight,
             weight_stride_n=weight.stride(0),
-            weight_stride_h=weight.stride(1),
             output_ptr=output,
             output_stride_b=output.stride(0),
             output_stride_s=output.stride(1),
-            output_stride_n=output.stride(2),
             has_input_state=input_state is not None,
             input_state_ptr=input_state,
             input_state_stride_b=None if input_state is None else input_state.stride(0),
-            input_state_stride_n=None if input_state is None else input_state.stride(1),
             output_grad_ptr=output_grad,
             input_grad_ptr=input_grad,
             weight_grad_ptr=weight_grad,
