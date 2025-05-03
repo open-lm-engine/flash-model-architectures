d424c1bc0d3040e793694e441dd26be5eed93c22
Mayank Mishra <mayank31398@gmail.com>
2025-05-03 14:53:26 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/rnn/__init__.py
+++ cute_kernels/kernels/rnn/__init__.py
@@ -31,25 +31,10 @@ class _RNN_Cute(torch.autograd.Function):
 
         if cu_seqlens is None:
             assert max_seqlen is None
-            B, S = input.size()[:2]
 
-            with torch.cuda.device(input.device):
-                rnn_forward_triton_kernel[ceil_divide(B, BLOCK_SIZE_B_forward), N](
-                    input_ptr=input,
-                    input_stride_b=input.stride(0),
-                    input_stride_s=input.stride(1),
-                    weight_ptr=weight,
-                    weight_stride_n=weight.stride(0),
-                    has_input_state=input_state is not None,
-                    input_state_ptr=input_state,
-                    input_state_stride_b=None if input_state is None else input_state.stride(0),
-                    output_ptr=output,
-                    B=B,
-                    S=S,
-                    H=H,
-                    BLOCK_SIZE_B=BLOCK_SIZE_B_forward,
-                    BLOCK_SIZE_H=BLOCK_SIZE_H,
-                )
+            rnn_forward_triton(
+                input=input, weight=weight, input_state=input_state, output=output, BLOCK_SIZE_B=BLOCK_SIZE_B_forward
+            )
         else:
             assert max_seqlen is not None
             B = cu_seqlens.numel() - 1
@@ -94,32 +79,16 @@ class _RNN_Cute(torch.autograd.Function):
         BLOCK_SIZE_H = ctx.BLOCK_SIZE_H
 
         if cu_seqlens is None:
-            B, S = output.size()[:2]
-
-            with torch.cuda.device(output.device):
-                rnn_backward_triton_kernel[ceil_divide(B, BLOCK_SIZE_B), N](
-                    weight_ptr=weight,
-                    weight_stride_n=weight.stride(0),
-                    weight_stride_h=weight.stride(1),
-                    output_ptr=output,
-                    has_input_state=input_state is not None,
-                    input_state_ptr=input_state,
-                    input_state_stride_b=None if input_state is None else input_state.stride(0),
-                    input_state_stride_n=None if input_state is None else input_state.stride(1),
-                    output_stride_b=output.stride(0),
-                    output_stride_s=output.stride(1),
-                    output_stride_n=output.stride(2),
-                    output_grad_ptr=output_grad,
-                    input_grad_ptr=input_grad,
-                    weight_grad_ptr=weight_grad,
-                    has_gradient_clipping=gradient_clipping is not None,
-                    gradient_clipping=gradient_clipping,
-                    B=B,
-                    S=S,
-                    H=H,
-                    BLOCK_SIZE_B=BLOCK_SIZE_B,
-                    BLOCK_SIZE_H=BLOCK_SIZE_H,
-                )
+            rnn_backward_triton(
+                weight=weight,
+                output=output,
+                input_state=input_state,
+                output_grad=output_grad,
+                input_grad=input_grad,
+                weight_grad=weight_grad,
+                gradient_clipping=gradient_clipping,
+                BLOCK_SIZE_B=BLOCK_SIZE_B,
+            )
         else:
             B = cu_seqlens.numel() - 1
 
