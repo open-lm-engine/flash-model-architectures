5d730c66c2dcc97d720ac0c2d310360810b27d47
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2025-05-05 01:35:20 -0400

    rename files (#219)
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/rnn/triton_implementation/__init__.py
+++ cute_kernels/kernels/rnn/triton_implementation/__init__.py
@@ -1,2 +1,4 @@
-from .backward import rnn_backward_triton, rnn_varlen_backward_triton
-from .forward import rnn_forward_triton, rnn_varlen_forward_triton
+from .backward import rnn_backward_triton
+from .forward import rnn_forward_triton
+from .varlen_backward import rnn_varlen_backward_triton
+from .varlen_forward import rnn_varlen_forward_triton
--- cute_kernels/kernels/rnn/triton_implementation/forward/__init__.py
+++
@@ -1,2 +0,0 @@
-from .kernel import rnn_forward_triton
-from .varlen_kernel import rnn_varlen_forward_triton
