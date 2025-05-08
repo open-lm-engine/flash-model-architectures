d63e623e24ab2ccd548d87b61420d3fecefc5478
Mayank Mishra <mayank31398@gmail.com>
2025-05-08 00:06:59 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/sequence_packing/torch_implementation.py
+++ cute_kernels/kernels/sequence_packing/torch_implementation.py
@@ -1,47 +1,77 @@
 import torch
 
 
-def pack_sequence_torch(x: torch.Tensor, cu_seqlens: torch.Tensor, padding_side: str = "left") -> torch.Tensor:
-    B, S = x.size()[:2]
-    other_dims = x.shape[2:]
+def pack_sequence_torch(
+    inputs: torch.Tensor | list[torch.Tensor], cu_seqlens: torch.Tensor, padding_side: str = "left"
+) -> torch.Tensor | list[torch.Tensor]:
+    is_list = isinstance(inputs, (list, tuple))
+    if not is_list:
+        inputs = [inputs]
 
-    sum_seqlens = cu_seqlens[-1]
-    seqlens = cu_seqlens[1:] - cu_seqlens[:-1]
-    batch_indices = torch.arange(B, device=x.device).repeat_interleave(seqlens)
+    outputs = []
 
-    if padding_side == "left":
-        pad_tokens = S - seqlens
-        seq_indices = torch.cat([torch.arange(sl, S, device=x.device) for sl in pad_tokens])
-    elif padding_side == "right":
-        seq_indices = torch.cat([torch.arange(sl, device=x.device) for sl in seqlens])
-    else:
-        raise ValueError(f"unexpected padding_side ({padding_side})")
+    for x in inputs:
+        B, S = x.size()[:2]
+        other_dims = x.shape[2:]
 
-    unpadded = torch.zeros(sum_seqlens, *other_dims, dtype=x.dtype, device=x.device)
-    unpadded = x[batch_indices, seq_indices]
+        sum_seqlens = cu_seqlens[-1]
+        seqlens = cu_seqlens[1:] - cu_seqlens[:-1]
+        batch_indices = torch.arange(B, device=x.device).repeat_interleave(seqlens)
 
-    return unpadded
+        if padding_side == "left":
+            pad_tokens = S - seqlens
+            seq_indices = torch.cat([torch.arange(sl, S, device=x.device) for sl in pad_tokens])
+        elif padding_side == "right":
+            seq_indices = torch.cat([torch.arange(sl, device=x.device) for sl in seqlens])
+        else:
+            raise ValueError(f"unexpected padding_side ({padding_side})")
+
+        unpadded = torch.zeros(sum_seqlens, *other_dims, dtype=x.dtype, device=x.device)
+        unpadded = x[batch_indices, seq_indices]
+
+        outputs.append(unpadded)
+
+    if not is_list:
+        outputs = outputs[0]
+
+    return outputs
 
 
 def unpack_sequence_torch(
-    x: torch.Tensor, cu_seqlens: torch.Tensor, desired_shape: tuple[int], padding_side: str = "left"
-) -> torch.Tensor:
-    B, S = desired_shape[:2]
-    assert cu_seqlens.size(0) - 1 == B
-    assert desired_shape[2:] == x.size()[1:]
+    inputs: torch.Tensor | list[torch.Tensor],
+    cu_seqlens: torch.Tensor,
+    desired_shape: tuple[int],
+    padding_side: str = "left",
+) -> torch.Tensor | list[torch.Tensor]:
 
-    seqlens = cu_seqlens[1:] - cu_seqlens[:-1]
-    batch_indices = torch.arange(B, device=x.device).repeat_interleave(seqlens)
+    is_list = isinstance(inputs, (list, tuple))
+    if not is_list:
+        inputs = [inputs]
 
-    if padding_side == "left":
-        pad_tokens = S - seqlens
-        seq_indices = torch.cat([torch.arange(sl, S, device=x.device) for sl in pad_tokens])
-    elif padding_side == "right":
-        seq_indices = torch.cat([torch.arange(sl, device=x.device) for sl in seqlens])
-    else:
-        raise ValueError(f"unexpected padding_side ({padding_side})")
+    outputs = []
 
-    padded = torch.zeros(desired_shape, dtype=x.dtype, device=x.device)
-    padded[batch_indices, seq_indices] = x
+    for x in inputs:
+        B, S = desired_shape[:2]
+        assert cu_seqlens.size(0) - 1 == B
+        assert desired_shape[2:] == x.size()[1:]
 
-    return padded
+        seqlens = cu_seqlens[1:] - cu_seqlens[:-1]
+        batch_indices = torch.arange(B, device=x.device).repeat_interleave(seqlens)
+
+        if padding_side == "left":
+            pad_tokens = S - seqlens
+            seq_indices = torch.cat([torch.arange(sl, S, device=x.device) for sl in pad_tokens])
+        elif padding_side == "right":
+            seq_indices = torch.cat([torch.arange(sl, device=x.device) for sl in seqlens])
+        else:
+            raise ValueError(f"unexpected padding_side ({padding_side})")
+
+        padded = torch.zeros(desired_shape, dtype=x.dtype, device=x.device)
+        padded[batch_indices, seq_indices] = x
+
+        outputs.append(padded)
+
+    if not is_list:
+        outputs = outputs[0]
+
+    return outputs
