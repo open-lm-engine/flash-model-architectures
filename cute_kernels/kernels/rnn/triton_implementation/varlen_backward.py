49397fe2ad96d18741580c52aed13cb86172f311
Mayank Mishra <mayank31398@gmail.com>
2025-05-11 15:17:11 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/rnn/triton_implementation/varlen_backward.py
+++ cute_kernels/kernels/rnn/triton_implementation/varlen_backward.py
@@ -10,7 +10,7 @@ from ....utils import cute_op
 
 @triton.jit
 def _load_input_state(
-    has_input_state,
+    HAS_INPUT_STATE,
     input_state_ptr,
     input_state_stride_b,
     pid_n,
@@ -22,7 +22,7 @@ def _load_input_state(
     BLOCK_SIZE_H,
     dtype,
 ):
-    if has_input_state:
+    if HAS_INPUT_STATE:
         output_ptrs = input_state_ptr + indices_b[:, None] * input_state_stride_b + pid_n * H + indices_h[None, :]
         output_prev = tl.load(output_ptrs, mask=mask_bh, other=0)
     else:
@@ -37,18 +37,18 @@ def rnn_varlen_backward_triton_kernel(
     weight_stride_n,
     output_ptr,
     output_stride_t,
-    has_input_state: tl.constexpr,
+    HAS_INPUT_STATE: tl.constexpr,
     input_state_ptr,
     input_state_stride_b,
     output_grad_ptr,
     cu_seqlens_ptr,
-    is_max_seqlen_tensor: tl.constexpr,
+    IS_MAX_SEQLEN_TENSOR: tl.constexpr,
     max_seqlen_ptr,
     input_grad_ptr,
     weight_grad_ptr,
-    has_gradient_clipping: tl.constexpr,
+    HAS_GRADIENT_CLIPPING: tl.constexpr,
     gradient_clipping,
-    activation_function: tl.constexpr,
+    ACTIVATION_FUNCTION: tl.constexpr,
     relu_negative_slope,
     B,
     H,
@@ -77,7 +77,7 @@ def rnn_varlen_backward_triton_kernel(
     start = tl.load(cu_seqlens_ptrs, mask=mask_b[:, None])
     end = tl.load(cu_seqlens_ptrs + 1, mask=mask_b[:, None])
 
-    if is_max_seqlen_tensor:
+    if IS_MAX_SEQLEN_TENSOR:
         max_seqlen = tl.load(max_seqlen_ptr)
     else:
         max_seqlen = max_seqlen_ptr
@@ -91,21 +91,21 @@ def rnn_varlen_backward_triton_kernel(
 
     # backward counting reduces 1 instruction since we need to compare s == 0, otherwise we have to compare s == S - 1
     for _ in range(max_seqlen - 1, -1, -1):
+        if HAS_GRADIENT_CLIPPING:
+            input_state_grad = clamp(input_state_grad, min_value=-gradient_clipping, max_value=gradient_clipping)
+
         unfinished = end >= start
         mask = unfinished & mask_h[None, :]
 
         output_grad_ptrs = output_grad_ptr + indices
         output_grad = tl.load(output_grad_ptrs, mask=mask, other=0)
 
-        if has_gradient_clipping:
-            input_state_grad = clamp(input_state_grad, min_value=-gradient_clipping, max_value=gradient_clipping)
-
         input_grad = output_grad + input_state_grad
-        if activation_function == "leaky_relu":
+        if ACTIVATION_FUNCTION == "leaky_relu":
             input_grad *= leaky_relu_backward(output, relu_negative_slope)
-        elif activation_function == "sigmoid":
+        elif ACTIVATION_FUNCTION == "sigmoid":
             input_grad *= sigmoid_backward(output)
-        elif activation_function == "tanh":
+        elif ACTIVATION_FUNCTION == "tanh":
             input_grad *= tanh_backward(output)
 
         input_grad_ptrs = input_grad_ptr + indices
@@ -119,7 +119,7 @@ def rnn_varlen_backward_triton_kernel(
         output_prev = tl.where(
             start == end,
             _load_input_state(
-                has_input_state=has_input_state,
+                HAS_INPUT_STATE=HAS_INPUT_STATE,
                 input_state_ptr=input_state_ptr,
                 input_state_stride_b=input_state_stride_b,
                 pid_n=pid_n,
@@ -174,18 +174,18 @@ def rnn_varlen_backward_triton(
             weight_stride_n=weight.stride(0),
             output_ptr=output,
             output_stride_t=output.stride(0),
-            has_input_state=has_input_state,
+            HAS_INPUT_STATE=has_input_state,
             input_state_ptr=input_state if has_input_state else None,
             input_state_stride_b=input_state.stride(0) if has_input_state else None,
             output_grad_ptr=output_grad,
             cu_seqlens_ptr=cu_seqlens,
-            is_max_seqlen_tensor=is_max_seqlen_tensor,
+            IS_MAX_SEQLEN_TENSOR=is_max_seqlen_tensor,
             max_seqlen_ptr=max_seqlen_tensor if is_max_seqlen_tensor else max_seqlen,
             input_grad_ptr=input_grad,
             weight_grad_ptr=weight_grad,
-            has_gradient_clipping=gradient_clipping is not None,
+            HAS_GRADIENT_CLIPPING=gradient_clipping is not None,
             gradient_clipping=gradient_clipping,
-            activation_function=activation_function,
+            ACTIVATION_FUNCTION=activation_function,
             relu_negative_slope=relu_negative_slope,
             B=B,
             H=H,
