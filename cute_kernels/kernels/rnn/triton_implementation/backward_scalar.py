55c6212257b8e22ca32b1239efdf494b67feb9b8
Mayank Mishra <mayank31398@gmail.com>
2025-05-12 23:37:09 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/rnn/triton_implementation/backward_scalar.py
+++ cute_kernels/kernels/rnn/triton_implementation/backward_scalar.py
@@ -4,20 +4,18 @@ import triton.language as tl
 
 from ....constants import LIBRARY_NAME
 from ....math import ceil_divide
-from ....triton_math import clamp, leaky_relu_backward, sigmoid_backward, tanh_backward
+from ....triton_math import clamp
 from ....utils import cute_op
+from .utils import _activation_backward
 
 
 @triton.jit
 def _rnn_backward_update(
     output, weight, output_grad, weight_grad, output_prev, ACTIVATION_FUNCTION: tl.constexpr, relu_negative_slope
 ):
-    if ACTIVATION_FUNCTION == "leaky_relu":
-        input_grad = output_grad * leaky_relu_backward(output, relu_negative_slope)
-    elif ACTIVATION_FUNCTION == "sigmoid":
-        input_grad = output_grad * sigmoid_backward(output)
-    elif ACTIVATION_FUNCTION == "tanh":
-        input_grad = output_grad * tanh_backward(output)
+    input_grad = _activation_backward(
+        y=output, grad=output_grad, ACTIVATION_FUNCTION=ACTIVATION_FUNCTION, relu_negative_slope=relu_negative_slope
+    )
 
     input_state_grad = input_grad * weight
     weight_grad += tl.sum(output_prev * input_grad, axis=0)
--- cute_kernels/kernels/rnn/triton_implementation/utils.py
+++ cute_kernels/kernels/rnn/triton_implementation/utils.py
@@ -1,6 +1,6 @@
 import triton
 
-from ....triton_math import leaky_relu, sigmoid, tanh
+from ....triton_math import leaky_relu, leaky_relu_backward, sigmoid, sigmoid_backward, tanh, tanh_backward
 
 
 @triton.jit
@@ -13,3 +13,15 @@ def _activation(x, ACTIVATION_FUNCTION, relu_negative_slope):
         x = tanh(x)
 
     return x
+
+
+@triton.jit
+def _activation_backward(y, grad, ACTIVATION_FUNCTION, relu_negative_slope):
+    if ACTIVATION_FUNCTION == "leaky_relu":
+        grad *= leaky_relu_backward(y, relu_negative_slope)
+    elif ACTIVATION_FUNCTION == "sigmoid":
+        grad *= sigmoid_backward(y)
+    elif ACTIVATION_FUNCTION == "tanh":
+        grad *= tanh_backward(y)
+
+    return grad
