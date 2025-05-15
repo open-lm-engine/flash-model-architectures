7837f402c67ca0ad14c15ea97e28f9d6151896ba
Mayank Mishra <mayank31398@gmail.com>
2025-05-14 21:55:35 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/kernels/gru_test.py
+++ tests/kernels/gru_test.py
@@ -278,7 +278,7 @@ class GRUTest(TestCommons):
             TestCommons.get_dtypes(),
             [[0, 7, 19, 27, 93]],  # cu_seqlens
             [256],  # state_size
-            [4],  # num_heads
+            [4, 256],  # num_heads
             [False, True],  # has_input_state
         )
     )
