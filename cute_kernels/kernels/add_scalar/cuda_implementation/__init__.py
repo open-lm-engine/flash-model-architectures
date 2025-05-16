38d220d25e3d124c4c4b8884f96d4c40833a4cd1
Mayank Mishra <mayank31398@gmail.com>
2025-05-16 15:25:48 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/add_scalar/cuda_implementation/__init__.py
+++ cute_kernels/kernels/add_scalar/cuda_implementation/__init__.py
@@ -6,7 +6,10 @@ from ....jit import cpp_jit
 from ....utils import cute_op
 
 
-@cutotune(configs=[], default_config=CutoTuneConfig({"BLOCK_SIZE": 1024}))
+_CUTOTUNE_CONFIG = CutoTuneConfig({"BLOCK_SIZE": 1024})
+
+
+@cutotune(configs=[_CUTOTUNE_CONFIG], default_config=_CUTOTUNE_CONFIG)
 @cute_op(f"{LIBRARY_NAME}::add_scalar_cuda", mutates_args={"output"})
 @cpp_jit()
 def add_scalar_cuda(x: torch.Tensor, y: float, output: torch.Tensor, BLOCK_SIZE: int) -> None: ...
