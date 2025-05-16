e8b5def38d3ac614c1bf7685c5fa6b75dc678d82
Mayank Mishra <mayank31398@gmail.com>
2025-05-16 15:39:30 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/add_scalar/__init__.py
+++ cute_kernels/kernels/add_scalar/__init__.py
@@ -1,5 +1,6 @@
 import torch
 
+from ...cutotune import CutoTuneParameter
 from ...kernel_backend import KernelBackend, is_cuda_kernel_backend_allowed, is_triton_kernel_backend_allowed
 from ...utils import is_nvidia_gpu
 from .cuda_implementation import add_scalar_cuda
@@ -9,11 +10,11 @@ from .triton_implementation import add_scalar_triton
 
 class _AddScalar_Cute(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, x: torch.Tensor, y: float, kernel_backend: KernelBackend, BLOCK_SIZE_CUDA: int) -> torch.Tensor:
+    def forward(ctx, x: torch.Tensor, y: float, kernel_backend: KernelBackend) -> torch.Tensor:
         output = torch.empty_like(x)
 
         if is_cuda_kernel_backend_allowed(kernel_backend) and is_nvidia_gpu() and x.is_cuda:
-            add_scalar_cuda(x=x, y=y, output=output, BLOCK_SIZE=BLOCK_SIZE_CUDA)
+            add_scalar_cuda(x=x, y=y, output=output, BLOCK_SIZE=CutoTuneParameter())
         elif is_triton_kernel_backend_allowed(kernel_backend):
             add_scalar_triton(x=x, y=y, output=output)
         else:
@@ -26,16 +27,13 @@ class _AddScalar_Cute(torch.autograd.Function):
         return output_grad, *[None] * 5
 
 
-def add_scalar_cute(
-    x: torch.Tensor, y: float, *, kernel_backend: KernelBackend = KernelBackend.cuda, BLOCK_SIZE_CUDA: int = 1024
-) -> torch.Tensor:
+def add_scalar_cute(x: torch.Tensor, y: float, *, kernel_backend: KernelBackend = KernelBackend.cuda) -> torch.Tensor:
     """adds a float value to a tensor
 
     Args:
         x (torch.Tensor): input tensor
         y (float): float value to add to `x`
         kernel_backend (KernelBackend, optional): kernel backend to prioritize. Defaults to KernelBackend.cuda.
-        BLOCK_SIZE_CUDA (int, optional): block size for CUDA backend. Defaults to 1024.
 
     Returns:
         torch.Tensor: output tensor
@@ -44,4 +42,4 @@ def add_scalar_cute(
     if y == 0:
         return x
 
-    return _AddScalar_Cute.apply(x, y, kernel_backend, BLOCK_SIZE_CUDA)
+    return _AddScalar_Cute.apply(x, y, kernel_backend)
