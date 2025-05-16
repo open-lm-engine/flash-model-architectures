2cceed5d5d1e7e9f6499d526338b97c49630a325
Mayank Mishra <mayank31398@gmail.com>
2025-05-16 15:51:03 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/add_tensor/cuda_implementation/__init__.py
+++ cute_kernels/kernels/add_tensor/cuda_implementation/__init__.py
@@ -1,10 +1,15 @@
 import torch
 
 from ....constants import LIBRARY_NAME
+from ....cutotune import CutoTuneConfig, cutotune
 from ....jit import cpp_jit
 from ....utils import cute_op
 
 
+_CUTOTUNE_CONFIG = CutoTuneConfig({"BLOCK_SIZE": 1024})
+
+
+@cutotune(configs=[_CUTOTUNE_CONFIG], default_config=_CUTOTUNE_CONFIG)
 @cute_op(f"{LIBRARY_NAME}::add_tensor_cuda", mutates_args={"output"})
 @cpp_jit()
 def add_tensor_cuda(x: torch.Tensor, y: torch.Tensor, output: torch.Tensor, BLOCK_SIZE: int) -> None: ...
