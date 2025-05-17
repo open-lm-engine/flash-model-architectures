9d01a1eea47e817268f8715db2133edfc4fa06df
Mayank Mishra <mayank31398@gmail.com>
2025-05-17 19:30:21 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/continuous_count/__init__.py
+++ cute_kernels/kernels/continuous_count/__init__.py
@@ -10,8 +10,8 @@ def continuous_count_cute(
     x: torch.Tensor,
     size: int,
     *,
-    BLOCK_SIZE: int | CutoTuneParameter = CutoTuneParameter(),
     THREAD_BLOCK_CLUSTER_SIZE: int | CutoTuneParameter = CutoTuneParameter(),
+    BLOCK_SIZE: int | CutoTuneParameter = CutoTuneParameter(),
 ) -> torch.Tensor:
     """counts the number of occurances of the values [0, 1, ..., `size`) in the input tensor (`size` is excluded).
         NOTE: the user is responsible for ensuring that the values lie in the valid range, any values outside this
@@ -20,10 +20,10 @@ def continuous_count_cute(
     Args:
         x (torch.Tensor): input tensor
         size (int): values [0, 1, ..., `size`) are counted (`size` is excluded)
-        BLOCK_SIZE (int | CutoTuneParameter, optional): block size for CUDA backend. Defaults to CutoTuneParameter().
         THREAD_BLOCK_CLUSTER_SIZE (int | CutoTuneParameter, optional): thread block cluster size refers to the size
             of the cluster for hierarchical accumulation, 1 means no thread block clusters. Defaults to
             CutoTuneParameter().
+        BLOCK_SIZE (int | CutoTuneParameter, optional): block size for CUDA backend. Defaults to CutoTuneParameter().
 
     Returns:
         torch.Tensor: output tensor
