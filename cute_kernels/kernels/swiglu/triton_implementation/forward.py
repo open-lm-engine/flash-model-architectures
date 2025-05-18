acf0fa272d6c181ddf0659d1f66a2edede7a0835
Mayank Mishra <mayank31398@gmail.com>
2025-05-18 05:22:41 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/swiglu/triton_implementation/forward.py
+++ cute_kernels/kernels/swiglu/triton_implementation/forward.py
@@ -49,7 +49,15 @@ def swiglu_forward_triton(
 
 @triton.jit
 def swiglu_packed_forward_triton_kernel(
-    gate_ptr, up_ptr, output_ptr, B, H, BLOCK_SIZE_B: tl.constexpr, BLOCK_SIZE_H: tl.constexpr
+    gate_ptr,
+    gate_stride_b,
+    up_ptr,
+    output_ptr,
+    output_stride_b,
+    B,
+    H,
+    BLOCK_SIZE_B: tl.constexpr,
+    BLOCK_SIZE_H: tl.constexpr,
 ):
     BLOCK_ID_B = tl.program_id(axis=0)
     BLOCK_ID_H = tl.program_id(axis=1)
@@ -63,15 +71,17 @@ def swiglu_packed_forward_triton_kernel(
     mask_h = indices_h < half_H
     mask_bh = mask_b[:, None] & mask_h[None, :]
 
-    up_ptrs = up_ptr + indices_b[:, None] * H + indices_h[None, :]
+    indices = indices_b[:, None] * gate_stride_b + indices_h[None, :]
+
+    up_ptrs = up_ptr + indices
     up = tl.load(up_ptrs, mask=mask_bh)
 
-    gate_ptrs = gate_ptr + indices_b[:, None] * H + indices_h[None, :]
+    gate_ptrs = gate_ptr + indices
     gate = tl.load(gate_ptrs, mask=mask_bh).to(tl.float32)
 
     output = up * gate * sigmoid(gate)
 
-    tl.store(output_ptr + indices_b[:, None] * half_H + indices_h[None, :], output, mask=mask_bh)
+    tl.store(output_ptr + indices_b[:, None] * output_stride_b + indices_h[None, :], output, mask=mask_bh)
 
 
 @cute_op(f"{LIBRARY_NAME}::swiglu_packed_forward_triton", mutates_args={"output"})
@@ -82,6 +92,7 @@ def swiglu_packed_forward_triton(x: torch.Tensor, output: torch.Tensor, BLOCK_SI
     with torch.device(x.device):
         swiglu_packed_forward_triton_kernel[ceil_divide(B, BLOCK_SIZE_B), ceil_divide(H, BLOCK_SIZE_H)](
             gate_ptr=gate,
+            gate_stride_b=gate.stride(0),
             up_ptr=up,
             output_ptr=output,
             B=B,
