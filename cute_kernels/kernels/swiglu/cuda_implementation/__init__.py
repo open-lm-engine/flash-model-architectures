a66faaacb7ea9ba97ff9b7d3413ed667e7425ee2
Mayank Mishra <mayank31398@gmail.com>
2025-05-19 14:44:58 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/swiglu/cuda_implementation/__init__.py
+++ cute_kernels/kernels/swiglu/cuda_implementation/__init__.py
@@ -1,7 +1,9 @@
 import torch
 
 from ....constants import LIBRARY_NAME
+from ....cutotune import CutoTuneConfig, CutoTuneParameter, cutotune
 from ....jit import cpp_jit
+from ....math import get_powers_of_2
 from ....utils import cute_op
 
 
@@ -9,6 +11,7 @@ _FORWARD_KERNEL_NAME = "swiglu_forward_cuda"
 _BACKWARD_KERNEL_NAME = "swiglu_backward_cuda"
 
 
+@cutotune(configs=[CutoTuneConfig({"BLOCK_SIZE": BLOCK_SIZE}) for BLOCK_SIZE in get_powers_of_2(128, 1024)])
 @cute_op(f"{LIBRARY_NAME}::{_FORWARD_KERNEL_NAME}", mutates_args={"output"})
 @cpp_jit()
 def swiglu_forward_cuda(gate: torch.Tensor, up: torch.Tensor, output: torch.Tensor, BLOCK_SIZE: int) -> None: ...
