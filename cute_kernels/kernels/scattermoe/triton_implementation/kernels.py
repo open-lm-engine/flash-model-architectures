0a3dbbffc331f225782ba20d17d4c858c9eff5b0
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2025-05-18 22:53:43 -0400

    fix scattermoe (#253)
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/scattermoe/triton_implementation/kernels.py
+++ cute_kernels/kernels/scattermoe/triton_implementation/kernels.py
@@ -39,31 +39,22 @@ def scatter2scatter_triton_kernel(
     N_BLOCK_COUNT = tl.cdiv(N, BLOCK_N)
     M_block_id = pid // N_BLOCK_COUNT
     N_block_id = pid % N_BLOCK_COUNT
-    M_range = tl.arange(0, BLOCK_M)
-    N_range = tl.arange(0, BLOCK_N)
-    # block_start_idx = tl.load(block_start_idx_ptr + M_block_id)
 
-    # M_block = tl.max_contiguous(M_block_id * BLOCK_M + M_range, BLOCK_M)
-    M_block = M_block_id * BLOCK_M + M_range
-    N_block = N_block_id * BLOCK_N + N_range
+    M_block = M_block_id * BLOCK_M + tl.arange(0, BLOCK_M)
+    N_block = N_block_id * BLOCK_N + tl.arange(0, BLOCK_N)
     N_mask = N_block < N
     M_boundary_mask = M_block < (FAN_OUT * M)
     E_idxs = tl.load(expert_idxs_ptr + M_block, mask=M_boundary_mask, other=E)
 
     no_k_mask = K % BLOCK_K == 0
-    no_n_mask = N % BLOCK_N == 0
 
     acc = tl.zeros((BLOCK_M, BLOCK_N), dtype=tl.float32)
     E_first_idx = tl.min(E_idxs)
     E_last_idx = tl.minimum(tl.max(E_idxs), E - 1)
     M_idx = tl.load(grouped_idx_ptr + M_block, mask=M_boundary_mask).to(tl.int32)
 
-    # iters = E_last_idx - E_first_idx + 1
-    # for i in range(iters):
-    #     E_idx = i + E_first_idx
     for E_idx in range(E_first_idx, E_last_idx + 1):
         E_mask = E_idxs == E_idx
-        # E_M_idx = tl.where(E_mask, M_idx, 0)
         E_M_idx = M_idx
         if x_grouped:
             M_in_idx = M_block
@@ -86,7 +77,6 @@ def scatter2scatter_triton_kernel(
             K,
             acc,
             no_k_mask,
-            no_n_mask,
             BLOCK_K,
         )
     if y_grouped:
@@ -115,7 +105,6 @@ def compute_expert_block(
     K,
     acc,
     no_k_mask,
-    no_n_mask,
     BLOCK_K,
 ):
 
@@ -127,10 +116,7 @@ def compute_expert_block(
     for K_block_id in range(iters):
         if no_k_mask:
             x = tl.load(X_blk_ptrs, mask=E_mask[:, None])
-            if no_n_mask or K_block_id < (iters - 1):
-                w = tl.load(W_blk_ptrs)
-            else:
-                w = tl.load(W_blk_ptrs, mask=N_mask[None, :])
+            w = tl.load(W_blk_ptrs, mask=N_mask[None, :])
         else:
             K_mask = (K_block_id * BLOCK_K + K_block) < K
             x = tl.load(X_blk_ptrs, mask=E_mask[:, None] & K_mask[None, :])
--- tests/kernels/scattermoe_test.py
+++ tests/kernels/scattermoe_test.py
@@ -21,8 +21,18 @@ class ScatterMoETest(TestCommons):
             [True, False],  # is_glu
             [True, False],  # is_compiling
         )
+        + TestCommons.make_args_matrix(
+            [torch.device("cuda")],
+            TestCommons.get_dtypes(),
+            [128],  # num_experts
+            [8],  # num_experts_per_tok
+            [576],  # hidden_size
+            [256],  # intermediate_size
+            [True, False],  # is_glu
+            [True, False],  # is_compiling
+        )
     )
-    def test_scattermoe_triton(
+    def test_scattermoe(
         self,
         device: torch.device,
         dtype: torch.dtype,
