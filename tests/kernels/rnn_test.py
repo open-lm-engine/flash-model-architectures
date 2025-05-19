86247caaf7e01eb27f9907bac08f171e4bc3cdec
Mayank Mishra <mayank31398@gmail.com>
2025-05-18 23:19:28 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/kernels/rnn_test.py
+++ tests/kernels/rnn_test.py
@@ -193,7 +193,15 @@ class RNNTest(TestCommons):
         y_expected.sum().backward()
 
         self.assert_equal_tensors(
-            y_kernel, y_expected, False, atol_float32=3e-6, rtol_float32=0, atol_float16=2e-3, rtol_float16=0
+            y_kernel,
+            y_expected,
+            False,
+            atol_float32=3e-6,
+            rtol_float32=0,
+            atol_float16=2e-3,
+            rtol_float16=0,
+            atol_bfloat16=1.6e-5,
+            rtol_bfloat16=0,
         )
         self.assert_equal_tensors(
             x_kernel.grad, x_expected.grad, False, atol_float32=2e-3, rtol_float32=0, atol_float16=2e-3, rtol_float16=0
