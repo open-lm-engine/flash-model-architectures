65cbcfda52b9985a15cf653086da758b23a7178c
Mayank Mishra <mayank31398@gmail.com>
2025-05-20 14:03:59 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

---
+++ cute_kernels/kernels/matrix_transpose/triton_implementation.py
@@ -0,0 +1,40 @@
+# **************************************************
+# Copyright (c) 2025, Mayank Mishra
+# **************************************************
+
+import torch
+import triton
+import triton.language as tl
+
+from ...constants import LIBRARY_NAME
+from ...math import ceil_divide
+from ...utils import cute_op
+
+
+@triton.jit
+def matrix_transpose_triton_kernel(x_ptr, y_ptr, M, N, BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr):
+    BLOCK_ID_M = tl.program_id(axis=0)
+    BLOCK_ID_N = tl.program_id(axis=1)
+
+    indices_m = BLOCK_ID_M * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+    indices_n = BLOCK_ID_N * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    indices = indices_m[:, None] & indices_n[None, :]
+
+    mask_m = indices_m < M
+    mask_n = indices_n < N
+    mask = mask_m[:, None] & mask_n[None, :]
+
+    x = tl.load(x_ptr + indices, mask=mask)
+    tl.store(y_ptr + indices.T, x.T, mask=mask.T)
+
+
+@cute_op(f"{LIBRARY_NAME}::matrix_transpose_triton", mutates_args={"output"})
+def matrix_transpose_triton(x: torch.Tensor, output: torch.Tensor) -> None:
+    M, N = x.size()
+    BLOCK_SIZE_M = 128
+    BLOCK_SIZE_N = 128
+
+    with torch.device(x.device):
+        matrix_transpose_triton_kernel[(ceil_divide(M, BLOCK_SIZE_M), ceil_divide(N, BLOCK_SIZE_N))](
+            x_ptr=x, y_ptr=output, M=M, N=N, BLOCK_SIZE_M=BLOCK_SIZE_M, BLOCK_SIZE_N=BLOCK_SIZE_N
+        )
