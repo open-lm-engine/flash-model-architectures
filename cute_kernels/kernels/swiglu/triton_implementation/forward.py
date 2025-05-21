277e5d05e45e2164c625becc9706c0b1a55682e3
Mayank Mishra <mayank31398@gmail.com>
2025-05-21 17:52:11 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/swiglu/triton_implementation/forward.py
+++ cute_kernels/kernels/swiglu/triton_implementation/forward.py
@@ -39,27 +39,33 @@ def swiglu_forward_triton_kernel(
     indices_b = BLOCK_ID_B * BLOCK_SIZE_B + tl.arange(0, BLOCK_SIZE_B)
     indices_h = BLOCK_ID_H * BLOCK_SIZE_H + tl.arange(0, BLOCK_SIZE_H)
 
-    half_H = H >> 1
-
     mask_b = indices_b < B
-    mask_h = indices_h < half_H
+    mask_h = indices_h < H
+
+    indices_gate = indices_b[:, None] * gate_stride_b + indices_h[None, :]
+    mask = mask_b[:, None] & mask_h[None, :]
+
+    indices_output = indices_b[:, None] * output_stride_b + indices_h[None, :]
 
     _swiglu(
         gate_ptr=gate_ptr,
         up_ptr=up_ptr,
         output_ptr=output_ptr,
-        indices_gate=indices_b[:, None] * gate_stride_b + indices_h[None, :],
-        indices_output=indices_b[:, None] * output_stride_b + indices_h[None, :],
-        mask=mask_b[:, None] & mask_h[None, :],
+        indices_gate=indices_gate,
+        indices_output=indices_output,
+        mask=mask,
     )
 
 
 @cute_op(f"{LIBRARY_NAME}::swiglu_forward_triton", mutates_args={"output"})
 def swiglu_forward_triton(gate: torch.Tensor, up: torch.Tensor, output: torch.Tensor) -> None:
     B, H = get_num_elements_and_hidden_size(gate)
-    H *= 2
-    BLOCK_SIZE_B = 64
-    BLOCK_SIZE_H = 64
+    BLOCK_SIZE_B = 32
+    BLOCK_SIZE_H = 32
+
+    gate = gate.view(-1, H).contiguous()
+    up = up.view(-1, H).contiguous()
+    output = output.view(-1, H).contiguous()
 
     with torch.device(gate.device):
         swiglu_forward_triton_kernel[ceil_divide(B, BLOCK_SIZE_B), ceil_divide(H, BLOCK_SIZE_H)](
