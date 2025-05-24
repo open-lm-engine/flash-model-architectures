687441a04f7be9e8e8ea683ef4ac5ea35664b125
Mayank Mishra <mayank31398@gmail.com>
2025-05-24 16:26:19 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/bmm/__init__.py
+++ cute_kernels/kernels/bmm/__init__.py
@@ -18,8 +18,6 @@ def gemm_cute(
     is_B_transposed: bool = False,
     alpha: float = 1,
     beta: float = 1,
-    *,
-    kernel_backend: str = "triton",
 ) -> torch.Tensor:
     """computes `alpha` * (`A` @ `B`) + `beta` * `C`
 
@@ -31,7 +29,6 @@ def gemm_cute(
         is_B_transposed (bool, optional): whether B has shape N x K. Defaults to False.
         alpha (float, optional): alpha. Defaults to 1.
         beta (float, optional): beta. Defaults to 1.
-        kernel_backend (str, optional): kernel backend to use. Defaults to triton.
 
     Raises:
         ValueError: if unexpected `kernel_backend` is passed
