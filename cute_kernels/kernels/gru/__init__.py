1d8d101dc3fafe40fe12993497a57bec7f458880
Mayank Mishra <mayank31398@gmail.com>
2025-05-26 19:35:49 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/gru/__init__.py
+++ cute_kernels/kernels/gru/__init__.py
@@ -55,23 +55,17 @@ class _GRU_Cute(torch.autograd.Function):
 
         if cu_seqlens is None:
             assert max_seqlen is None
-
-            if H == 1:
-                assert False
-            else:
-                gru_forward_triton(**kwargs)
+            gru_forward_triton(**kwargs)
         else:
             assert max_seqlen is not None
             is_max_seqlen_tensor = isinstance(max_seqlen, torch.Tensor)
 
-            kwargs["cu_seqlens"] = cu_seqlens
-            kwargs["max_seqlen_tensor"] = max_seqlen if is_max_seqlen_tensor else None
-            kwargs["max_seqlen"] = None if is_max_seqlen_tensor else max_seqlen
-
-            if H == 1:
-                assert False
-            else:
-                gru_varlen_forward_triton(**kwargs)
+            gru_varlen_forward_triton(
+                **kwargs,
+                cu_seqlens=cu_seqlens,
+                max_seqlen_tensor=max_seqlen if is_max_seqlen_tensor else None,
+                max_seqlen=None if is_max_seqlen_tensor else max_seqlen,
+            )
 
         ctx.save_for_backward(
             weight,
@@ -136,21 +130,16 @@ class _GRU_Cute(torch.autograd.Function):
         }
 
         if cu_seqlens is None:
-            if H == 1:
-                assert False
-            else:
-                gru_backward_triton(**kwargs)
+            gru_backward_triton(**kwargs)
         else:
             is_max_seqlen_tensor = isinstance(max_seqlen, torch.Tensor)
 
-            kwargs["cu_seqlens"] = cu_seqlens
-            kwargs["max_seqlen_tensor"] = max_seqlen if is_max_seqlen_tensor else None
-            kwargs["max_seqlen"] = None if is_max_seqlen_tensor else max_seqlen
-
-            if H == 1:
-                assert False
-            else:
-                gru_varlen_backward_triton(**kwargs)
+            gru_varlen_backward_triton(
+                **kwargs,
+                cu_seqlens=cu_seqlens,
+                max_seqlen_tensor=max_seqlen if is_max_seqlen_tensor else None,
+                max_seqlen=None if is_max_seqlen_tensor else max_seqlen,
+            )
 
         weight_grad = weight_grad.type_as(weight)
         forget_weight_grad = forget_weight_grad.type_as(forget_weight)
