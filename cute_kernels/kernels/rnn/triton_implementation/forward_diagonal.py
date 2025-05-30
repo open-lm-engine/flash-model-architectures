fcfee8974726c7ab9c371eb10cf3123c744f9dc9
Mayank Mishra <mayank31398@gmail.com>
2025-05-30 01:50:34 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/rnn/triton_implementation/forward_diagonal.py
+++ cute_kernels/kernels/rnn/triton_implementation/forward_diagonal.py
@@ -26,7 +26,7 @@ def _get_autotune_configs() -> list[triton.Config]:
 
 @triton.jit
 def _rnn_forward_update(h, W, x, ACTIVATION_FUNCTION, relu_negative_slope):
-    h = W[None, :] * h + x
+    h = W * h + x
     h = _activation(x=h, ACTIVATION_FUNCTION=ACTIVATION_FUNCTION, relu_negative_slope=relu_negative_slope)
     return h
 
@@ -55,7 +55,7 @@ def diagonal_rnn_forward_triton_kernel(
     mask_n = indices_n < N
     mask_bn = mask_b[:, None] & mask_n[None, :]
 
-    W = tl.load(W_ptr + indices_n, mask=mask_n)
+    W = tl.load(W_ptr + indices_n, mask=mask_n)[None, :]
 
     if h_ptr is None:
         h = tl.zeros((BLOCK_SIZE_B, BLOCK_SIZE_N), dtype=x_ptr.dtype.element_ty)
