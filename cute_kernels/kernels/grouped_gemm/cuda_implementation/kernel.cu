ec11e29fc45824f886b61446883b4f240e237475
Mayank Mishra <mayank31398@gmail.com>
2025-05-31 06:08:54 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/grouped_gemm/cuda_implementation/kernel.cu
+++ cute_kernels/kernels/grouped_gemm/cuda_implementation/kernel.cu
@@ -218,24 +218,8 @@ struct Options {
 
 /// Helper to initialize a block of device data
 template <class Element>
-bool initialize_block(cutlass::DeviceAllocation<Element> &block, uint64_t seed = 2023) {
-    Element scope_max, scope_min;
-    int bits_input = cutlass::sizeof_bits<Element>::value;
-
-    if (bits_input == 1) {
-        scope_max = static_cast<Element>(2);
-        scope_min = static_cast<Element>(0);
-    } else if (bits_input <= 8) {
-        scope_max = static_cast<Element>(2);
-        scope_min = static_cast<Element>(-2);
-    } else {
-        scope_max = static_cast<Element>(8);
-        scope_min = static_cast<Element>(-8);
-    }
-
-    cutlass::reference::device::BlockFillRandomUniform(block.get(), block.size(), seed, scope_max, scope_min, 0);
-
-    return true;
+void initialize_block(cutlass::DeviceAllocation<Element> &block, uint64_t seed = 2023) {
+    cutlass::reference::device::BlockFillRandomUniform(block.get(), block.size(), seed, 8, -8, 0);
 }
 
 /// Allocates device-side data
