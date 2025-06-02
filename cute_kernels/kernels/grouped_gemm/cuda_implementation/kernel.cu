5b44a659e5d0735041940ec2a59d652bf86d4ac9
Mayank Mishra <mayank31398@gmail.com>
2025-06-02 15:38:22 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/grouped_gemm/cuda_implementation/kernel.cu
+++ cute_kernels/kernels/grouped_gemm/cuda_implementation/kernel.cu
@@ -467,3 +467,29 @@ void _grouped_gemm_cuda(const torch::Tensor &_A,
         std::cout << "  TFLOPS      : " << gflops / 1000.0 << std::endl;
     }
 }
+
+void grouped_gemm_cuda(const torch::Tensor &_A,
+                       const torch::Tensor &_B,
+                       const std::optional<torch::Tensor> &_C,
+                       torch::Tensor &_D,
+                       const torch::Tensor &M_array,
+                       const torch::Tensor &N_array,
+                       const torch::Tensor &K_array,
+                       const bool &is_A_transposed,
+                       const bool &is_B_transposed,
+                       const fp32 &alpha,
+                       const fp32 &beta) {
+    if (is_A_transposed) {
+        if (is_B_transposed) {
+            _grouped_gemm_cuda<true, true>(_A, _B, _C, _D, M_array, N_array, K_array, alpha, beta);
+        } else {
+            _grouped_gemm_cuda<true, false>(_A, _B, _C, _D, M_array, N_array, K_array, alpha, beta);
+        }
+    } else {
+        if (is_B_transposed) {
+            _grouped_gemm_cuda<false, true>(_A, _B, _C, _D, M_array, N_array, K_array, alpha, beta);
+        } else {
+            _grouped_gemm_cuda<false, false>(_A, _B, _C, _D, M_array, N_array, K_array, alpha, beta);
+        }
+    }
+}
--- cute_kernels/kernels/grouped_gemm/cuda_implementation/ops.cpp
+++ cute_kernels/kernels/grouped_gemm/cuda_implementation/ops.cpp
@@ -4,16 +4,6 @@
 
 #include <torch/extension.h>
 
-void _grouped_gemm_cuda(const torch::Tensor &_A,
-                        const torch::Tensor &_B,
-                        const std::optional<torch::Tensor> &_C,
-                        torch::Tensor &_D,
-                        const torch::Tensor &M_array,
-                        const torch::Tensor &N_array,
-                        const torch::Tensor &K_array,
-                        const float &alpha,
-                        const float &beta);
-
 void grouped_gemm_cuda(const torch::Tensor &_A,
                        const torch::Tensor &_B,
                        const std::optional<torch::Tensor> &_C,
@@ -24,20 +14,6 @@ void grouped_gemm_cuda(const torch::Tensor &_A,
                        const bool &is_A_transposed,
                        const bool &is_B_transposed,
                        const float &alpha,
-                       const float &beta) {
-    if (is_A_transposed) {
-        if (is_B_transposed) {
-            _grouped_gemm_cuda<true, true>(_A, _B, _C, _D, M_array, N_array, K_array, alpha, beta);
-        } else {
-            _grouped_gemm_cuda<true, false>(_A, _B, _C, _D, M_array, N_array, K_array, alpha, beta);
-        }
-    } else {
-        if (is_B_transposed) {
-            _grouped_gemm_cuda<false, true>(_A, _B, _C, _D, M_array, N_array, K_array, alpha, beta);
-        } else {
-            _grouped_gemm_cuda<false, false>(_A, _B, _C, _D, M_array, N_array, K_array, alpha, beta);
-        }
-    }
-}
+                       const float &beta);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) { m.def("grouped_gemm_cuda", &grouped_gemm_cuda, "grouped GEMM (CUDA)"); }
