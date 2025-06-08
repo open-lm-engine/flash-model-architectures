e59f0dbec4e38e9dc4d381feae75e6949217ac5e
Mayank Mishra <mayank31398@gmail.com>
2025-06-07 23:40:39 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- a.py
+++ a.py
@@ -12,74 +12,82 @@ K = 4096
 M = 4096
 N = 512
 
-is_A_transposed = False
-is_B_transposed = False
+for is_A_transposed in [False, True]:
+    for is_B_transposed in [False, True]:
+        A = torch.randint(
+            -8,
+            9,
+            (E, K, M) if is_A_transposed else (E, M, K),
+            device=torch.cuda.current_device(),
+            dtype=torch.bfloat16,
+        )
+        B = torch.randint(
+            -8,
+            9,
+            (E, N, K) if is_B_transposed else (E, K, N),
+            device=torch.cuda.current_device(),
+            dtype=torch.bfloat16,
+        )
 
-A = torch.randint(
-    -8, 9, (E, K, M) if is_A_transposed else (E, M, K), device=torch.cuda.current_device(), dtype=torch.bfloat16
-)
-B = torch.randint(
-    -8, 9, (E, N, K) if is_B_transposed else (E, K, N), device=torch.cuda.current_device(), dtype=torch.bfloat16
-)
+        M_array = torch.tensor([M] * E, device=torch.cuda.current_device(), dtype=torch.uint32)
+        N_array = torch.full_like(M_array, fill_value=N)
+        K_array = torch.full_like(M_array, fill_value=K)
 
-M_array = torch.tensor([M] * E, device=torch.cuda.current_device(), dtype=torch.uint32)
-N_array = torch.full_like(M_array, fill_value=N)
-K_array = torch.full_like(M_array, fill_value=K)
+        torch_profiler = torch.profiler.profile(
+            activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
+            schedule=torch.profiler.schedule(wait=5, warmup=5, active=1, repeat=1),
+            on_trace_ready=torch.profiler.tensorboard_trace_handler("tmp"),
+            record_shapes=True,
+        )
 
-torch_profiler = torch.profiler.profile(
-    activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
-    schedule=torch.profiler.schedule(wait=5, warmup=5, active=1, repeat=1),
-    on_trace_ready=torch.profiler.tensorboard_trace_handler("tmp"),
-    record_shapes=True,
-)
+        for i in range(10):
+            output = grouped_gemm_cute(
+                A=A,
+                B=B,
+                C=None,
+                M_array=M_array,
+                N_array=N_array,
+                K_array=K_array,
+                is_A_transposed=is_A_transposed,
+                is_B_transposed=is_B_transposed,
+            )
+            # torch_profiler.step()
 
-for i in range(10):
-    output = grouped_gemm_cute(
-        A=A,
-        B=B,
-        C=None,
-        M_array=M_array,
-        N_array=N_array,
-        K_array=K_array,
-        is_A_transposed=is_A_transposed,
-        is_B_transposed=is_B_transposed,
-    )
-    # torch_profiler.step()
+        s = torch.cuda.Event(enable_timing=True)
+        e = torch.cuda.Event(enable_timing=True)
 
-s = torch.cuda.Event(enable_timing=True)
-e = torch.cuda.Event(enable_timing=True)
+        s.record()
+        for i in range(10):
+            output = grouped_gemm_cute(
+                A=A,
+                B=B,
+                C=None,
+                M_array=M_array,
+                N_array=N_array,
+                K_array=K_array,
+                is_A_transposed=is_A_transposed,
+                is_B_transposed=is_B_transposed,
+            )
+            # torch_profiler.step()
+        e.record()
 
-s.record()
-for i in range(10):
-    output = grouped_gemm_cute(
-        A=A,
-        B=B,
-        C=None,
-        M_array=M_array,
-        N_array=N_array,
-        K_array=K_array,
-        is_A_transposed=is_A_transposed,
-        is_B_transposed=is_B_transposed,
-    )
-    # torch_profiler.step()
-e.record()
+        torch.cuda.synchronize()
 
-torch.cuda.synchronize()
+        t = s.elapsed_time(e) / 10 / 1e3
+        print(2 * M * N * K * E / t / 1e12)
 
-t = s.elapsed_time(e) / 10 / 1e3
-print(2 * M * N * K * E / t / 1e12)
+        D = []
+        for i in range(E):
+            a = A[i]
+            if is_A_transposed:
+                a = a.T
+            b = B[i]
+            if is_B_transposed:
+                b = b.T
 
-D = []
-for i in range(E):
-    a = A[i]
-    if is_A_transposed:
-        a = a.T
-    b = B[i]
-    if is_B_transposed:
-        b = b.T
+            D.append(a @ b)
 
-    D.append(a @ b)
+        D = torch.stack(D)
 
-D = torch.stack(D)
-
-print((output - D).abs().max())
+        print((output - D).abs().max())
+        print()
