77e60070225787e4822acdea22c3c505d6973d79
Mayank Mishra <mayank31398@gmail.com>
2025-06-10 00:02:27 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/moe/cuda_implementation/__init__.py
+++ cute_kernels/kernels/moe/cuda_implementation/__init__.py
@@ -3,12 +3,11 @@
 # **************************************************
 
 import torch
-import triton
-import triton.language as tl
 
 from ....math import ceil_divide
 from ....utils import ensure_contiguous
 from ...grouped_gemm import grouped_gemm_cute
+from .group_kernel import group_with_padding_triton_kernel
 from .padded_expert_frequency_kernel import padded_expert_frequency_triton_kernel
 
 
@@ -66,52 +65,6 @@ class _GroupedGemmExperts_Cute(torch.autograd.Function):
         weight_grad = grouped_gemm_cute(A=output_grad, B=x, C=None, is_A_transposed=True, is_B_transposed=False)
 
         return x_grad, weight_grad, None
-
-
-@triton.jit
-def group_with_padding_triton_kernel(
-    x_ptr,
-    expert_padding_offset_ptr,
-    sorted_idxs_ptr,
-    scattered_idxs_ptr,
-    y_ptr,
-    T,
-    H,
-    K,
-    BLOCK_SIZE_B: tl.constexpr,
-    BLOCK_SIZE_H: tl.constexpr,
-):
-    BLOCK_ID = tl.program_id(axis=0)
-    B = T * K
-
-    indices_b = BLOCK_ID * BLOCK_SIZE_B + tl.arange(0, BLOCK_SIZE_B)
-    mask_b = indices_b < B
-
-    scattered_idxs = tl.load(scattered_idxs_ptr + indices_b, mask=mask_b)
-
-    NUM_BLOCKS_H = tl.cdiv(H, BLOCK_SIZE_H)
-
-    x_ptrs = x_ptr + (scattered_idxs // K)[:, None] * H
-    y_ptrs = y_ptr + indices_b[:, None] * H
-
-    if expert_padding_offset_ptr is not None:
-        sorted_idxs = tl.load(sorted_idxs_ptr + indices_b, mask=mask_b)
-        expert_padding_offset = tl.load(expert_padding_offset_ptr + sorted_idxs)
-
-        y_ptrs += expert_padding_offset * H
-
-    for h in range(NUM_BLOCKS_H):
-        indices_h = h * BLOCK_SIZE_H + tl.arange(0, BLOCK_SIZE_H)
-
-        if h < NUM_BLOCKS_H - 1:
-            x = tl.load(x_ptrs + indices_h[None, :], mask=mask_b[:, None])
-            tl.store(y_ptrs + indices_h[None, :], x, mask=mask_b[:, None])
-        else:
-            mask_h = indices_h < H
-            mask_bh = mask_b[:, None] & mask_h[None, :]
-
-            x = tl.load(x_ptrs + indices_h[None, :], mask=mask_bh)
-            tl.store(y_ptrs + indices_h[None, :], x, mask=mask_bh)
 
 
 class _GroupWithPadding(torch.autograd.Function):
