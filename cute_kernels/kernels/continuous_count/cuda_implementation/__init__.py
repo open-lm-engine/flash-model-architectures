077c29b7acdbfcbf449fd39921f2f96e59fd7e9c
Mayank Mishra <mayank31398@gmail.com>
2025-06-11 01:26:24 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/continuous_count/cuda_implementation/__init__.py
+++ cute_kernels/kernels/continuous_count/cuda_implementation/__init__.py
@@ -10,13 +10,13 @@ from ....jit import cpp_jit
 from ....utils import cute_op
 
 
-@cutotune(
-    configs=[
-        CutoTuneConfig({"BLOCK_SIZE": 1024, "THREAD_BLOCK_CLUSTER_SIZE": thread_block_cluster_size})
-        for thread_block_cluster_size in [1, 2, 4, 8]
-    ],
-    triggers={"x.dtype"},
-)
+# @cutotune(
+#     configs=[
+#         CutoTuneConfig({"BLOCK_SIZE": 1024, "THREAD_BLOCK_CLUSTER_SIZE": thread_block_cluster_size})
+#         for thread_block_cluster_size in [1, 2, 4, 8]
+#     ],
+#     triggers={"x.dtype"},
+# )
 @cute_op(f"{LIBRARY_NAME}::continuous_count_cuda", mutates_args={"output"})
 @cpp_jit()
 def continuous_count_cuda(
