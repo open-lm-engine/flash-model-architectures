a33099192a9d11371dca31ab3c6d0f7cf96f52e8
Mayank Mishra <mayank31398@gmail.com>
2025-06-11 03:15:13 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/moe/cuda_implementation/padded_expert_frequency_kernel.py
+++ cute_kernels/kernels/moe/cuda_implementation/padded_expert_frequency_kernel.py
@@ -7,15 +7,13 @@ import triton
 import triton.language as tl
 
 from ....constants import LIBRARY_NAME
-from ....math import ceil_divide
+from ....math import get_next_power_of_2
 from ....utils import cute_op
 
 
 @triton.jit
 def padded_expert_frequency_triton_kernel(x_ptr, y_ptr, pad_to_multiple_of, N, BLOCK_SIZE: tl.constexpr):
-    BLOCK_ID = tl.program_id(axis=0)
-
-    indices = BLOCK_ID * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+    indices = tl.arange(0, BLOCK_SIZE)
     mask = indices < N
 
     x = tl.load(x_ptr + indices, mask=mask)
@@ -29,11 +27,11 @@ def padded_expert_frequency_triton(
     expert_frequency: torch.Tensor, output: torch.Tensor, pad_to_multiple_of: int
 ) -> None:
     E = expert_frequency.size(0)
-    BLOCK_SIZE = 4096
+    BLOCK_SIZE = get_next_power_of_2(E)
     NUM_WARPS = 32
 
     with torch.device(expert_frequency.device):
-        padded_expert_frequency_triton_kernel[ceil_divide(E, BLOCK_SIZE),](
+        padded_expert_frequency_triton_kernel[1,](
             x_ptr=expert_frequency,
             y_ptr=output,
             pad_to_multiple_of=pad_to_multiple_of,
