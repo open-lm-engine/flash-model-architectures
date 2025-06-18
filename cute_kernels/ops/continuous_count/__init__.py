8f5f4ec78e81c2433758d9a76f12e445b2639b1a
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2025-06-17 21:54:15 -0400

    rename C->E for continuous_count (#289)
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/ops/continuous_count/__init__.py
+++ cute_kernels/ops/continuous_count/__init__.py
@@ -47,7 +47,7 @@ def continuous_count_cute(
         output = bincount(x=x, minlength=size).to(torch.uint32)
     elif kernel_backend == KernelBackend.cuda:
         output = torch.empty(size, dtype=torch.uint32, device=x.device)
-        continuous_count_cuda(x=x, output=output, C=size, THREAD_BLOCK_CLUSTER_SIZE=1, BLOCK_SIZE=1024)
+        continuous_count_cuda(x=x, output=output, E=size, THREAD_BLOCK_CLUSTER_SIZE=1, BLOCK_SIZE=1024)
     else:
         raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
 
