4302b80bb3d0dea163022abc9253e950f3259fc8
Mayank Mishra <mayank31398@gmail.com>
2025-06-23 18:33:00 +0530

    checkpoint
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/modules/gru/__init__.py
+++ cute_kernels/modules/gru/__init__.py
@@ -202,21 +202,20 @@ def gru_cute(
 
         output = torch.empty_like(input)
 
-        input = input.unsqueeze(-2)
         forget_input = forget_input.unsqueeze(-2)
         reset_input = reset_input.unsqueeze(-2)
 
         if cu_seqlens is None:
             assert max_seqlen is None
-            B, S, N, _, H = input.size()
+            B, S, N, H = input.size()
 
             if input_state is None:
                 input_state = torch.zeros(B, N, H, device=input.device, dtype=input.dtype)
 
             input_state = input_state.unsqueeze(-2)
 
-            # input -> (B, S, N, 1, H)
-            # weight -> (1, N, H, H)
+            # input -> (B, S, N, H)
+            # weight -> (N, H, H)
             # input_state -> (B, N, 1, H)
 
             for s in range(S):
@@ -224,22 +223,22 @@ def gru_cute(
                 forget_gate = sigmoid(input_state @ forget_weight.unsqueeze(0) + forget_input[:, s])
                 reset_gate = sigmoid(input_state @ reset_weight.unsqueeze(0) + reset_input[:, s])
 
-                possible_new_state = tanh((input_state * reset_gate) @ weight.unsqueeze(0) + input[:, s])
+                possible_new_state = tanh((input_state * reset_gate) @ weight.unsqueeze(0) + input[:, s].unsqueeze(-2))
                 input_state = forget_gate * input_state + (1 - forget_gate) * possible_new_state
 
                 output[:, s] = input_state.squeeze(-2)
         else:
             assert max_seqlen is not None
             B = cu_seqlens.numel() - 1
-            _, N, _, H = input.size()
+            _, N, H = input.size()
 
             if input_state is None:
                 input_state = torch.zeros(B, N, H, device=input.device, dtype=input.dtype)
             else:
                 input_state = input_state.clone()
 
-            # input -> (cu_seqlens[-1], N, 1, H)
-            # weight -> (1, N, H, H)
+            # input -> (cu_seqlens[-1], N, H)
+            # weight -> (N, H, H)
             # input_state -> (B, N, H)
 
             start = cu_seqlens[:-1]
@@ -258,9 +257,11 @@ def gru_cute(
                 forget_gate = sigmoid(new_state @ forget_weight.unsqueeze(0) + forget_input[offset_unfinished])
                 reset_gate = sigmoid(new_state @ reset_weight.unsqueeze(0) + reset_input[offset_unfinished])
 
-                possible_new_state = tanh((new_state * reset_gate) @ weight.unsqueeze(0) + input[offset_unfinished])
-                new_state = forget_gate * new_state + (1 - forget_gate) * possible_new_state
+                possible_new_state = tanh(
+                    (new_state * reset_gate) @ weight.unsqueeze(0) + input[offset_unfinished].unsqueeze(-2)
+                )
 
+                new_state = forget_gate * new_state + (1 - forget_gate) * possible_new_state
                 new_state = new_state.squeeze(-2)
 
                 output[offset_unfinished] = new_state
