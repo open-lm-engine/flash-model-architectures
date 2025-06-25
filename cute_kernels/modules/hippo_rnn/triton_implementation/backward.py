87246af3acf706f17fdf60e424145ca55db4b3d8
Mayank Mishra <mayank31398@gmail.com>
2025-06-25 22:51:34 +0530

    checkpoint
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/modules/hippo_rnn/triton_implementation/backward.py
+++ cute_kernels/modules/hippo_rnn/triton_implementation/backward.py
@@ -133,7 +133,6 @@ def hippo_rnn_backward_triton_kernel(
 
         tl.store(dx_ptrs, dx, mask=mask_bh)
         y = y_prev
-        c = c_prev
 
     tl.atomic_add(dW_ptr + indices_W, dW, mask=mask_hh)
     tl.atomic_add(dWh_ptr + indices_Wh, dWh, mask=mask_dh)
