7a54e6568f8df5f197145b7149ca5f995a40c146
Mayank Mishra <mayank31398@gmail.com>
2025-06-25 23:31:15 +0530

    checkpoint
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/utils/__init__.py
+++ cute_kernels/utils/__init__.py
@@ -4,7 +4,7 @@
 
 from .contiguous import ensure_contiguous, ensure_same_strides
 from .custom_op import cute_op, enable_cute_tracing
-from .debugging import PrintGradient
+from .debugging import print_gradient
 from .device import device_synchronize, get_sm_count, is_hip, is_nvidia_gpu
 from .env import get_boolean_env_variable
 from .ptx import get_ptx_from_triton_kernel
