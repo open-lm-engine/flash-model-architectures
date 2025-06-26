895c0bf5c173334146b067430dd95c059722e5d3
Mayank Mishra <mayank31398@gmail.com>
2025-06-26 18:29:19 +0530

    fix
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/modules/hippo_rnn/triton_implementation/backward.py
+++ cute_kernels/modules/hippo_rnn/triton_implementation/backward.py
@@ -7,10 +7,21 @@ import triton
 import triton.language as tl
 
 from ....constants import LIBRARY_NAME
-from ....math import ceil_divide, get_next_power_of_2
+from ....math import ceil_divide, get_next_power_of_2, get_powers_of_2
 from ....triton_math import clamp, matmul, tanh_backward
 from ....utils import cute_op
-from .forward import _get_autotune_configs
+
+
+def _get_autotune_configs() -> list[triton.Config]:
+    configs = []
+    for num_warps in get_powers_of_2(1, 8):
+        for num_stages in range(1, 5):
+            for BLOCK_SIZE_B in [1] + get_powers_of_2(16, 32):
+                configs.append(
+                    triton.Config({"BLOCK_SIZE_B": BLOCK_SIZE_B}, num_stages=num_stages, num_warps=num_warps)
+                )
+
+    return configs
 
 
 @triton.autotune(
