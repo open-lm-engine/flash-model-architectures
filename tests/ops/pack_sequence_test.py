378d76632543273e7b9610b74fc1631439f1fdcd
Mayank Mishra <mayank31398@gmail.com>
2025-07-09 05:52:28 +0530

    pass output_shape
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/ops/pack_sequence_test.py
+++ tests/ops/pack_sequence_test.py
@@ -20,6 +20,7 @@ class PackSequenceTest(TestCommons):
             [[0, 70, 170, 295, 393, 412, 515, 691]],  # cu_seqlens
             [torch.device("cuda")],  # device
             TestCommons.get_dtypes(),  # dtype
+            [False, True],  # use_output_shape
             ["left", "right"],  # padding_side
             [KernelBackend.cuda, KernelBackend.triton],  # kernel_backend
             [pack_sequence_cute, torch.compile(pack_sequence_cute, fullgraph=True)],  # function
@@ -31,6 +32,7 @@ class PackSequenceTest(TestCommons):
         cu_seqlens: list[int],
         device: torch.device,
         dtype: torch.dtype,
+        use_output_shape: bool,
         padding_side: str,
         kernel_backend: KernelBackend,
         function: Callable,
@@ -38,10 +40,13 @@ class PackSequenceTest(TestCommons):
         x_kernel, x_expected = self.get_random_duplicated_tensors(size, device=device, dtype=dtype)
         cu_seqlens = torch.tensor(cu_seqlens, device=device, dtype=torch.uint32)
 
+        output_shape = (cu_seqlens[-1].item(), *size[2:]) if use_output_shape else None
+
         with torch._dynamo.config.patch(capture_scalar_outputs=True):
             z_kernel = function(
                 x_kernel,
                 cu_seqlens=cu_seqlens,
+                output_shape=output_shape,
                 padding_side=padding_side,
                 kernel_backend_forward=kernel_backend,
                 kernel_backend_backward=kernel_backend,
@@ -50,6 +55,7 @@ class PackSequenceTest(TestCommons):
         z_expected = pack_sequence_cute(
             x_expected,
             cu_seqlens=cu_seqlens.to(torch.int),
+            output_shape=output_shape,
             padding_side=padding_side,
             kernel_backend_forward=KernelBackend.torch,
             kernel_backend_backward=KernelBackend.torch,
