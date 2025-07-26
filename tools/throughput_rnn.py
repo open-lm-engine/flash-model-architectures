cff54a147b0309fe919af0a9a07b022080c96620
Mayank Mishra <mayank31398@gmail.com>
2025-07-27 00:34:22 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tools/throughput_rnn.py
+++ tools/throughput_rnn.py
@@ -18,7 +18,7 @@ N = 64
 H = 768
 
 with torch.device(torch.cuda.current_device()):
-    rnn = RNN(input_size=H, state_size=1024, output_size=H, num_heads=N, add_bias=False)
+    rnn = RNN(input_size=H, state_size=1024, output_size=H, num_heads=N, add_bias=False, gradient_clipping=None)
 
 headers = ["dtype", "torch", "kernel"]
 
