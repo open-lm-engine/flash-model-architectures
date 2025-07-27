22540b1ac99f49925a2bc024e2ebb4c0874f7ea6
Mayank Mishra <mayank31398@gmail.com>
2025-07-27 15:09:21 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/modules/hippo_rnn/triton_implementation/__init__.py
+++
@@ -1,6 +0,0 @@
-# **************************************************
-# Copyright (c) 2025, Mayank Mishra
-# **************************************************
-
-from .backward import hippo_rnn_backward_triton
-from .forward import hippo_rnn_forward_triton
