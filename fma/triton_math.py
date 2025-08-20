459d99b604f11c08e8f7f507e5dcef19205ea786
Mayank Mishra <mayank31398@gmail.com>
2025-08-20 06:25:39 -0700

    add replacer

--- fma/triton_math.py
+++ fma/triton_math.py
@@ -37,6 +37,17 @@ def sigmoid(
 
 
 @triton.jit
+def sigmoid_backward(y):
+    dtype = y.dtype
+
+    y = y.to(tl.float32)
+    y = y * (1 - y)
+    y = y.to(dtype)
+
+    return y
+
+
+@triton.jit
 def tanh(x, output_dtype: tl.constexpr = None):
     if output_dtype is None:
         output_dtype = x.dtype
@@ -46,27 +57,6 @@ def tanh(x, output_dtype: tl.constexpr = None):
     x = x.to(output_dtype)
 
     return x
-
-
-@triton.jit
-def relu(x):
-    return max(0, x)
-
-
-@triton.jit
-def leaky_relu(x, negative_slope):
-    return relu(x) + negative_slope * min(0, x)
-
-
-@triton.jit
-def sigmoid_backward(y):
-    dtype = y.dtype
-
-    y = y.to(tl.float32)
-    y = y * (1 - y)
-    y = y.to(dtype)
-
-    return y
 
 
 @triton.jit
@@ -81,8 +71,37 @@ def tanh_backward(y):
 
 
 @triton.jit
+def leaky_relu(x, negative_slope):
+    return max(0, x) + negative_slope * min(0, x)
+
+
+@triton.jit
 def leaky_relu_backward(y, relu_negative_slope):
     return tl.where(y >= 0, 1, relu_negative_slope)
+
+
+@triton.jit
+def silu(x, output_dtype: tl.constexpr = None):
+    if output_dtype is None:
+        output_dtype = x.dtype
+
+    x = x.to(tl.float32)
+    x *= sigmoid(x, output_dtype=tl.float32)
+    x = x.to(output_dtype)
+
+    return x
+
+
+@triton.jit
+def silu_backward(x):
+    dtype = x.dtype
+
+    x = x.to(tl.float32)
+    s = sigmoid(x, output_dtype=tl.float32)
+    x = s + s * (x - x * s)
+    x = x.to(dtype)
+
+    return x
 
 
 @triton.jit
