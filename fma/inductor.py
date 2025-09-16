b647d0f95f82cd6c5dd8ca49efd926eaefe7d4c5
Mayank Mishra <mayank31398@gmail.com>
2025-09-16 10:35:55 -0700

    drop inductor example
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- fma/inductor.py
+++ fma/inductor.py
@@ -42,8 +42,11 @@ def partialize_and_update_signature(func: Callable, **kwargs) -> Callable:
     return wrapper
 
 
-def register_rmsnorm(device: torch.device) -> tuple[tuple[torch.Tensor, torch.Tensor], dict]:
-    inputs = (torch.empty(1, device=device, requires_grad=True), torch.empty(1, device=device, requires_grad=True))
+def get_rmsnorm_replacer(device: torch.device) -> tuple[tuple[torch.Tensor, torch.Tensor], dict]:
+    example_inputs = (
+        torch.empty(1, device=device, requires_grad=True),
+        torch.empty(1, device=device, requires_grad=True),
+    )
 
     search_function = partialize_and_update_signature(
         rmsnorm, eps=None, memory_efficient=False, kernel_backend=KernelBackend.torch
@@ -53,24 +56,31 @@ def register_rmsnorm(device: torch.device) -> tuple[tuple[torch.Tensor, torch.Te
         rmsnorm, eps=None, memory_efficient=False, kernel_backend=KernelBackend.triton
     )
 
-    for trace_function in _ALL_TRACE_FUNCTIONS:
-        register_replacement(
-            search_fn=search_function,
-            replace_fn=replacement_function,
-            example_inputs=inputs,
-            trace_fn=trace_function,
-            pass_dicts=patterns,
-        )
+    return search_function, replacement_function, example_inputs
 
 
-def _fused_residual_add_rmsnorm_inputs_0(device: torch.device) -> list[torch.Tensor, torch.Tensor, None, None]:
-    return [
+def _fused_residual_add_rmsnorm_inputs_0(device: torch.device) -> list[torch.Tensor, torch.Tensor, None, None, None]:
+    inputs = (
         torch.empty(1, device=device, requires_grad=True),
         torch.empty(1, device=device, requires_grad=True),
         torch.empty(1, device=device, requires_grad=True),
-        None,
-        None,
-    ]
+    )
+
+    search_function = partialize_and_update_signature(
+        fused_residual_add_rmsnorm,
+        eps=None,
+        multiplier=None,
+        memory_efficient=False,
+        kernel_backend=KernelBackend.torch,
+    )
+
+    replacement_function = partialize_and_update_signature(
+        fused_residual_add_rmsnorm,
+        eps=None,
+        multiplier=None,
+        memory_efficient=False,
+        kernel_backend=KernelBackend.triton,
+    )
 
 
 def _fused_residual_add_rmsnorm_inputs_1(device: torch.device) -> list[torch.Tensor, torch.Tensor, None, float]:
@@ -84,7 +94,7 @@ def _fused_residual_add_rmsnorm_inputs_1(device: torch.device) -> list[torch.Ten
 
 
 _MAPPING = {
-    rmsnorm.__name__: register_rmsnorm,
+    rmsnorm.__name__: get_rmsnorm_replacer,
     fused_residual_add_rmsnorm.__name__: (
         fused_residual_add_rmsnorm_torch,
         partial(fused_residual_add_rmsnorm, kernel_backend=KernelBackend.triton),
@@ -97,4 +107,13 @@ def enable_kernels(kernels: list[str]):
     device = torch.cuda.current_device()
 
     for kernel in kernels:
-        _MAPPING[kernel](device)
+        search_function, replacement_function, example_inputs = _MAPPING[kernel](device)
+
+        for trace_function in _ALL_TRACE_FUNCTIONS:
+            register_replacement(
+                search_fn=search_function,
+                replace_fn=replacement_function,
+                example_inputs=example_inputs,
+                trace_fn=trace_function,
+                pass_dicts=patterns,
+            )
