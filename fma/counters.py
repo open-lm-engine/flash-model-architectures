c819bca721762f26cf0740129e9a041d3ed9d11d
Mayank Mishra <mayank31398@gmail.com>
2025-09-16 01:59:45 -0700

    cleanup rmsnorm
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- fma/counters.py
+++ fma/counters.py
@@ -1,4 +1,15 @@
 from collections import defaultdict
+from typing import Any
 
 
 _COUNTERS = defaultdict(int)
+
+
+def increment_counter(key: Any, value: int = 1) -> None:
+    global _COUNTERS
+    _COUNTERS[key] += 1
+
+
+def reset_counters() -> None:
+    global _COUNTERS
+    _COUNTERS = defaultdict(int)
