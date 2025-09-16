838918d29e6635a8a7aeb98005e895a1b739e36f
Mayank Mishra <mayank31398@gmail.com>
2025-09-16 08:45:47 -0700

    drop inductor example
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- examples/inductor.py
+++ examples/inductor.py
@@ -13,22 +13,22 @@ from torch._inductor.pattern_matcher import fwd_only, joint_fwd_bwd, register_re
 from fma.ops import rmsnorm, rmsnorm_torch
 
 
-def search_function(x: torch.Tensor) -> torch.Tensor:
+def search_function(x: torch.Tensor, w: torch.Tensor) -> torch.Tensor:
     y = x + 1
-    y = rmsnorm_torch(y, weight=y, eps=None)
+    y = rmsnorm_torch(y, weight=w, eps=None)
     y = F.silu(y)
     return y
 
 
-def replacement_function(x: torch.Tensor) -> torch.Tensor:
-    y = rmsnorm(x, x, eps=None)
+def replacement_function(x: torch.Tensor, w: torch.Tensor) -> torch.Tensor:
+    y = rmsnorm(x, w, eps=None)
     return y
 
 
 class MyModule(nn.Module):
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = search_function(x)
-        x = search_function(x)
+    def forward(self, x: torch.Tensor, w) -> torch.Tensor:
+        x = search_function(x, w)
+        x = search_function(x, w)
         return x
 
 
@@ -38,18 +38,36 @@ for trace_function in [joint_fwd_bwd, fwd_only]:
     register_replacement(
         search_fn=search_function,
         replace_fn=replacement_function,
-        example_inputs=[torch.empty(1, device=device, requires_grad=True)],
+        example_inputs=[
+            torch.empty(1, device=device, requires_grad=True),
+            torch.empty(1, device=device, requires_grad=True),
+        ],
         trace_fn=trace_function,
         pass_dicts=patterns,
     )
 
 m = MyModule()
 
-print("original value =", m(torch.tensor([1.0], device=device, requires_grad=True)))
+print(
+    "original value =",
+    m(torch.tensor([1.0], device=device, requires_grad=True), torch.tensor([1.0], device=device, requires_grad=True)),
+)
 print(
     "expected value =",
-    replacement_function(replacement_function(torch.tensor([1.0], device=device, requires_grad=True))),
+    replacement_function(
+        replacement_function(
+            torch.tensor([1.0], device=device, requires_grad=True),
+            torch.tensor([1.0], device=device, requires_grad=True),
+            0.1,
+        ),
+        torch.tensor([1.0], device=device, requires_grad=True),
+    ),
 )
 
 m_compiled = torch.compile(m, fullgraph=True)
-print("value with compile =", m_compiled(torch.tensor([1.0], device=device, requires_grad=True)))
+print(
+    "value with compile =",
+    m_compiled(
+        torch.tensor([1.0], device=device, requires_grad=True), torch.tensor([1.0], device=device, requires_grad=True)
+    ),
+)
