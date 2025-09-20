bf0d8582b020e5c4528a961a06c608a470be276e
Mayank Mishra <mayank31398@gmail.com>
2025-09-20 00:38:27 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- fma/ops/fused_residual_add_rmsnorm/cute_dsl_implementation/__init__.py
+++ fma/ops/fused_residual_add_rmsnorm/cute_dsl_implementation/__init__.py
@@ -1,1 +1,5 @@
+# **************************************************
+# Copyright (c) 2025, Mayank Mishra
+# **************************************************
+
 from .forward import fused_residual_add_rmsnorm_forward_cute_dsl
