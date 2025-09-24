d8fe213ba42877707d779826b735abd64863f800
Mayank Mishra <mayank31398@gmail.com>
2025-09-24 15:37:39 -0700

    cleanup gemm
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- fma/ops/fused_residual_add_rmsnorm/__init__.py
+++ fma/ops/fused_residual_add_rmsnorm/__init__.py
@@ -5,13 +5,15 @@
 import torch
 import torch.nn.functional as F
 
+from ...constants import MAX_TRITON_BLOCK_SIZE
 from ...counters import increment_counter
 from ...cutotune import CutoTuneParameter
 from ...enums import KernelBackend
+from ...math import ceil_divide, get_next_power_of_2
 from ...utils import ensure_contiguous, get_num_elements_and_hidden_size
 from .triton_implementation import (
     fused_residual_add_rmsnorm_backward_triton,
-    fused_residual_add_rmsnorm_forward_triton,
+    fused_residual_add_rmsnorm_forward_triton_kernel,
 )
 
 
@@ -38,22 +40,32 @@ class _FusedResidualAddRMSNorm(torch.autograd.Function):
         if eps is None:
             eps = torch.finfo(x.dtype).eps
 
-        B, _ = get_num_elements_and_hidden_size(x)
+        B, H = get_num_elements_and_hidden_size(x)
         has_residual = residual is not None
 
         output = torch.empty_like(x)
         added_x_residual = torch.empty_like(x) if has_residual else None
         rmsnorm_denominator = None if memory_efficient else torch.empty(B, device=x.device, dtype=torch.float32)
 
-        fused_residual_add_rmsnorm_forward_triton(
-            x=x,
-            residual=residual,
-            weight=weight,
-            output=output,
+        BLOCK_SIZE_B = 1
+        BLOCK_SIZE_H = get_next_power_of_2(H)
+        assert BLOCK_SIZE_H <= MAX_TRITON_BLOCK_SIZE
+        NUM_WARPS = 8
+
+        fused_residual_add_rmsnorm_forward_triton_kernel[ceil_divide(B, BLOCK_SIZE_B),](
+            x_ptr=x,
+            residual_ptr=residual,
+            weight_ptr=weight,
+            output_ptr=output,
             eps=eps,
             multiplier=multiplier,
-            added_x_residual=added_x_residual,
-            rmsnorm_denominator=rmsnorm_denominator,
+            added_x_residual_ptr=added_x_residual,
+            rmsnorm_denominator_ptr=rmsnorm_denominator,
+            B=B,
+            H=H,
+            BLOCK_SIZE_B=BLOCK_SIZE_B,
+            BLOCK_SIZE_H=BLOCK_SIZE_H,
+            num_warps=NUM_WARPS,
         )
 
         if residual is None:
--- fma/ops/fused_residual_add_rmsnorm/triton_implementation/__init__.py
+++ fma/ops/fused_residual_add_rmsnorm/triton_implementation/__init__.py
@@ -3,4 +3,4 @@
 # **************************************************
 
 from .backward import fused_residual_add_rmsnorm_backward_triton
-from .forward import fused_residual_add_rmsnorm_forward_triton
+from .forward import fused_residual_add_rmsnorm_forward_triton_kernel
