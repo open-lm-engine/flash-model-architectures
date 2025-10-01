66b4934236efadde38161ac7fe8c016d703ea5ba
Mayank Mishra <mayank31398@gmail.com>
2025-09-30 23:17:12 -0700

    cleanup tensor
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- fma/modules/gru/__init__.py
+++ fma/modules/gru/__init__.py
@@ -131,47 +131,30 @@ class _GRU_Varlen(torch.autograd.Function):
         gradient_clipping: float | None,
         cu_seqlens: torch.Tensor | None,
         max_seqlen: torch.Tensor | int | None,
-        kernel_backend: KernelBackend | CutoTuneParameter,
     ) -> torch.Tensor:
-        assert input.dim() in [3, 4]
-        assert weight.dim() == 3
-        assert kernel_backend == KernelBackend.triton or isinstance(kernel_backend, CutoTuneParameter)
-
-        N, H = input.size()[-2:]
-        assert weight.size() == (N, H, H)
-
         output = torch.empty_like(input)
         forget_gate = torch.empty_like(input)
         reset_gate = torch.empty_like(input)
         output_update = torch.empty_like(input)
 
-        kwargs = {
-            "input": input,
-            "weight": weight,
-            "forget_input": forget_input,
-            "forget_weight": forget_weight,
-            "forget_gate": forget_gate,
-            "reset_input": reset_input,
-            "reset_weight": reset_weight,
-            "reset_gate": reset_gate,
-            "output_update": output_update,
-            "input_state": input_state,
-            "output": output,
-        }
+        is_max_seqlen_tensor = isinstance(max_seqlen, torch.Tensor)
 
-        if cu_seqlens is None:
-            assert max_seqlen is None
-            gru_forward_triton(**kwargs)
-        else:
-            assert max_seqlen is not None
-            is_max_seqlen_tensor = isinstance(max_seqlen, torch.Tensor)
-
-            gru_varlen_forward_triton(
-                **kwargs,
-                cu_seqlens=cu_seqlens,
-                max_seqlen_tensor=max_seqlen if is_max_seqlen_tensor else None,
-                max_seqlen=None if is_max_seqlen_tensor else max_seqlen,
-            )
+        gru_varlen_forward_triton(
+            input=input,
+            weight=weight,
+            forget_input=forget_input,
+            forget_weight=forget_weight,
+            forget_gate=forget_gate,
+            reset_input=reset_input,
+            reset_weight=reset_weight,
+            reset_gate=reset_gate,
+            output_update=output_update,
+            input_state=input_state,
+            output=output,
+            cu_seqlens=cu_seqlens,
+            max_seqlen_tensor=max_seqlen if is_max_seqlen_tensor else None,
+            max_seqlen=None if is_max_seqlen_tensor else max_seqlen,
+        )
 
         tensors_to_save = (
             weight,
@@ -236,37 +219,28 @@ class _GRU_Varlen(torch.autograd.Function):
         forget_weight_grad = torch.zeros_like(weight, dtype=torch.float32)
         reset_weight_grad = torch.zeros_like(weight, dtype=torch.float32)
 
-        kwargs = {
-            "weight": weight,
-            "output": output,
-            "forget_weight": forget_weight,
-            "forget_gate": forget_gate,
-            "forget_input_grad": forget_input_grad,
-            "forget_weight_grad": forget_weight_grad,
-            "reset_weight": reset_weight,
-            "reset_gate": reset_gate,
-            "reset_input_grad": reset_input_grad,
-            "reset_weight_grad": reset_weight_grad,
-            "output_update": output_update,
-            "input_state": input_state,
-            "output_grad": output_grad,
-            "input_grad": input_grad,
-            "weight_grad": weight_grad,
-            "gradient_clipping": ctx.gradient_clipping,
-            "output": output,
-        }
-
-        if cu_seqlens is None:
-            gru_backward_triton(**kwargs)
-        else:
-            is_max_seqlen_tensor = isinstance(max_seqlen, torch.Tensor)
-
-            gru_varlen_backward_triton(
-                **kwargs,
-                cu_seqlens=cu_seqlens,
-                max_seqlen_tensor=max_seqlen if is_max_seqlen_tensor else None,
-                max_seqlen=None if is_max_seqlen_tensor else max_seqlen,
-            )
+        gru_varlen_backward_triton(
+            weight=weight,
+            output=output,
+            forget_weight=forget_weight,
+            forget_gate=forget_gate,
+            forget_input_grad=forget_input_grad,
+            forget_weight_grad=forget_weight_grad,
+            reset_weight=reset_weight,
+            reset_gate=reset_gate,
+            reset_input_grad=reset_input_grad,
+            reset_weight_grad=reset_weight_grad,
+            output_update=output_update,
+            input_state=input_state,
+            output_grad=output_grad,
+            input_grad=input_grad,
+            weight_grad=weight_grad,
+            gradient_clipping=ctx.gradient_clipping,
+            output=output,
+            cu_seqlens=cu_seqlens,
+            max_seqlen_tensor=max_seqlen if is_max_seqlen_tensor else None,
+            max_seqlen=None if is_max_seqlen_tensor else max_seqlen,
+        )
 
         weight_grad = weight_grad.type_as(weight)
         forget_weight_grad = forget_weight_grad.type_as(forget_weight)
@@ -413,7 +387,7 @@ def gru(
                 input, weight, forget_input, forget_weight, reset_input, reset_weight, input_state, gradient_clipping
             )
         else:
-            output = _GRU.apply(
+            output = _GRU_Varlen.apply(
                 input,
                 weight,
                 forget_input,
