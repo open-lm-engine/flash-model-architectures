6fc08b4fb9e6081e82a96c99673919ab6124036f
Mayank Mishra <mayank31398@gmail.com>
2025-10-15 14:43:32 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- fma/__init__.py
+++ fma/__init__.py
@@ -27,6 +27,6 @@ from .functional import (
     unpack_sequence,
 )
 from .inductor import enable_kernels, init_inductor
+from .layers import GRU, RNN, MoE
 from .math import ceil_divide, divide_if_divisible, get_powers_of_2
-from .modules import GRU, RNN, MoE
 from .utils import device_synchronize, get_ptx_from_triton_kernel, get_triton_num_warps, set_seed
