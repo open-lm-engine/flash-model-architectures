2a2f2960fe779f8336cf876e8144ff112bafbefe
Mayank Mishra <mayank31398@gmail.com>
2025-10-15 14:48:44 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- fma/functional/softmax/__init__.py
+++ fma/functional/softmax/__init__.py
@@ -6,13 +6,14 @@ import torch
 import torch.nn.functional as F
 
 from ...enums import KernelBackend
+from ...utils import empty_like_contiguous
 from .triton_implementation import softmax_backward_triton, softmax_forward_triton
 
 
 class _Softmax(torch.autograd.Function):
     @staticmethod
     def forward(ctx, x: torch.Tensor, logits_multiplier: float | None) -> torch.Tensor:
-        output = torch.empty_like(x, memory_format=torch.contiguous_format)
+        output = empty_like_contiguous(x)
 
         softmax_forward_triton(x=x, output=output, logits_multiplier=logits_multiplier)
 
@@ -24,7 +25,7 @@ class _Softmax(torch.autograd.Function):
     @staticmethod
     def backward(ctx, output_grad: torch.Tensor) -> tuple[torch.Tensor | None]:
         output = ctx.saved_tensors[0]
-        x_grad = torch.empty_like(output, memory_format=torch.contiguous_format)
+        x_grad = empty_like_contiguous(output)
 
         softmax_backward_triton(
             output=output, output_grad=output_grad, x_grad=x_grad, logits_multiplier=ctx.logits_multiplier
