9718c4f651a44934a25434e44e04656ab121da8b
Mayank Mishra <mayank31398@gmail.com>
2025-10-19 20:48:21 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/ops/pack_sequence_test.py
+++ tests/ops/pack_sequence_test.py
@@ -44,22 +44,22 @@ class PackSequenceTest(TestCommons):
 
         with torch._dynamo.config.patch(capture_scalar_outputs=True):
             z_kernel = function(
-                x_kernel,
+                [x_kernel],
                 cu_seqlens=cu_seqlens,
                 total_tokens=cu_seqlens[-1].item(),
                 padding_side=padding_side,
                 kernel_backend_forward=kernel_backend,
                 kernel_backend_backward=kernel_backend,
-            )
+            )[0]
 
         z_expected = pack_sequence(
-            x_expected,
+            [x_expected],
             cu_seqlens=cu_seqlens.to(torch.int),
             total_tokens=cu_seqlens[-1].item(),
             padding_side=padding_side,
             kernel_backend_forward=KernelBackend.torch,
             kernel_backend_backward=KernelBackend.torch,
-        )
+        )[0]
 
         z_expected.sum().backward()
         z_kernel.sum().backward()
