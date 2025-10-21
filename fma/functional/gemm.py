a468f16cbd33f06481683cdf4d3ad9ac4952b6ab
Mayank Mishra <mayank31398@gmail.com>
2025-10-20 22:12:05 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- fma/functional/gemm.py
+++ fma/functional/gemm.py
@@ -5,7 +5,7 @@
 import torch
 
 from ..enums import KernelBackend
-from .bmm.triton_implementation import bmm_triton
+from .bmm import bmm
 
 
 def gemm(
@@ -16,8 +16,6 @@ def gemm(
     is_B_transposed: bool = False,
     alpha: float = 1,
     beta: float = 1,
-    *,
-    kernel_backend: KernelBackend | str = KernelBackend.triton,
 ) -> torch.Tensor:
     """computes `alpha` * (`A` @ `B`) + `beta` * `C`
 
@@ -29,7 +27,6 @@ def gemm(
         is_B_transposed (bool, optional): whether B has shape N x K. Defaults to False.
         alpha (float, optional): alpha. Defaults to 1.
         beta (float, optional): beta. Defaults to 1.
-        kernel_backend (KernelBackend | str, optional): kernel backend to use. Defaults to KernelBackend.triton.
 
     Raises:
         ValueError: if unexpected `kernel_backend` is passed
@@ -41,47 +38,16 @@ def gemm(
     assert A.dim() == 2
     assert B.dim() == 2
 
-    M, K = A.size()
-    if is_A_transposed:
-        M, K = K, M
+    D = bmm(
+        A=A[None, ...],
+        B=B[None, ...],
+        C=None if C is None else C[None, ...],
+        is_A_transposed=is_A_transposed,
+        is_B_transposed=is_B_transposed,
+        alpha=alpha,
+        beta=beta,
+    )
 
-    assert B.size(1 if is_B_transposed else 0) == K
-    N = B.size(0 if is_B_transposed else 1)
+    D = D.squeeze(0)
 
-    if beta == 0:
-        assert C is None
-    else:
-        assert C is not None
-        assert C.size() == (M, N)
-
-    if kernel_backend == KernelBackend.torch:
-        if is_A_transposed:
-            A = A.T
-
-        if is_B_transposed:
-            B = B.T
-
-        if beta == 0:
-            output = A @ B
-            if alpha != 1:
-                output = alpha * output
-        else:
-            output = torch.addmm(C, A, B, alpha=alpha, beta=beta)
-    else:
-        output = torch.empty(M, N, dtype=A.dtype, device=A.device)
-
-        if kernel_backend == KernelBackend.triton:
-            bmm_triton(
-                A=A.unsqueeze(0),
-                B=B.unsqueeze(0),
-                C=None if C is None else C.unsqueeze(0),
-                D=output.unsqueeze(0),
-                is_A_transposed=is_A_transposed,
-                is_B_transposed=is_B_transposed,
-                alpha=alpha,
-                beta=beta,
-            )
-        else:
-            raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
-
-    return output
+    return D
