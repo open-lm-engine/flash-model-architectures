d763e1026d84c887b93476d5b8dd77bc50f6b281
Mayank Mishra <mayank31398@gmail.com>
2025-10-25 23:36:33 -0700

    op
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/layers/gru_test.py
+++ tests/layers/gru_test.py
@@ -174,19 +174,23 @@ class GRUTest(TestCommons):
 
             nn.init.normal_(gru.state_weight, std=0.01)
 
-        with force_kernel_backend(KernelBackend.torch):
-            y_kernel, _ = gru(
-                input=x_packed_kernel, input_state=input_state_kernel, cu_seqlens=cu_seqlens, max_seqlen=max_seqlen
-            )
+        y_kernel, _ = gru(
+            input=x_packed_kernel,
+            input_state=input_state_kernel,
+            cu_seqlens=cu_seqlens,
+            max_seqlen=max_seqlen,
+            kernel_backend=KernelBackend.torch,
+        )
 
-            y_torch = []
-            for i in range(batch_size):
-                y, _ = gru(
-                    input=x_packed_torch[cu_seqlens[i] : cu_seqlens[i + 1]].unsqueeze(0),
-                    input_state=input_state_torch[i].unsqueeze(0) if has_input_state else None,
-                )
-                y_torch.append(y.squeeze(0))
-            y_torch = torch.cat(y_torch)
+        y_torch = []
+        for i in range(batch_size):
+            y, _ = gru(
+                input=x_packed_torch[cu_seqlens[i] : cu_seqlens[i + 1]].unsqueeze(0),
+                input_state=input_state_torch[i].unsqueeze(0) if has_input_state else None,
+                kernel_backend=KernelBackend.torch,
+            )
+            y_torch.append(y.squeeze(0))
+        y_torch = torch.cat(y_torch)
 
         self.assert_equal_tensors(y_kernel, y_torch, False, atol_bfloat16=3.1e-5, rtol_bfloat16=0)
 
