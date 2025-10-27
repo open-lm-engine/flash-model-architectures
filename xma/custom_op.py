721b1975071a5f76aedfc946792b8303b213dc85
Mayank Mishra <mayank31398@gmail.com>
2025-10-26 18:35:09 -0700

    op
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/custom_op.py
+++ xma/custom_op.py
@@ -8,6 +8,7 @@ from typing import Any
 
 import torch
 
+from .counters import increment_counter
 from .enums import KernelBackend
 
 
@@ -39,7 +40,10 @@ class CustomOp(torch.autograd.Function):
 
         if kernel_backend is None:
             raise ValueError("code is not supposed to reach here! kernel_backend was not inferrable")
-        elif kernel_backend == KernelBackend.torch:
+
+        increment_counter(cls._get_key(kernel_backend))
+
+        if kernel_backend == KernelBackend.torch:
             output = cls.forward_backward_torch(*args, **kwargs)
         else:
             args = args + tuple(kwargs.values())
@@ -93,3 +97,7 @@ class CustomOp(torch.autograd.Function):
     @staticmethod
     def forward_backward_torch(*args, **kwargs) -> Any:
         raise NotImplementedError
+
+    @classmethod
+    def _get_key(cls, kernel_backend: KernelBackend) -> str:
+        return f"{cls.__name__}-{kernel_backend.value}"
