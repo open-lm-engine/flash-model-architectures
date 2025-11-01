cd97285bcc017baf525da9a9ddfef55e171e1c86
Mayank Mishra <mayank31398@gmail.com>
2025-11-01 15:56:47 -0700

    move to dev
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/swiglu/__init__.py
+++ xma/functional/swiglu/__init__.py
@@ -36,7 +36,10 @@ class _Swiglu(CustomOp):
     @ensure_contiguous
     def forward_cuda(ctx, gate: torch.Tensor, up: torch.Tensor) -> torch.Tensor:
         output = empty_like_contiguous(gate)
-        swiglu_forward_cuda(gate=gate, up=up, output=output, BLOCK_SIZE=1024)
+
+        swiglu_forward_cuda(
+            gate=gate.flatten(0, -2), up=up.flatten(0, -2), output=output.flatten(0, -2), BLOCK_SIZE=1024
+        )
 
         ctx_save_for_backward(ctx, gate, up)
 
