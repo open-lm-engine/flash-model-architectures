f6a57a1ffb3c6d1bf813654f655bbe31c0de7c57
Mayank Mishra <mayank31398@gmail.com>
2025-11-10 02:05:58 -0800

    slight cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/swiglu/cuda_implementation/__init__.py
+++ xma/functional/swiglu/cuda_implementation/__init__.py
@@ -2,21 +2,5 @@
 # Copyright (c) 2025, Mayank Mishra
 # **************************************************
 
-import torch
-from torch.library import custom_op
-
-from ....constants import LIBRARY_NAME
-from ....jit import cpp_jit
+from .backward import swiglu_backward_cuda
 from .forward import swiglu_forward_cuda
-
-
-@custom_op(f"{LIBRARY_NAME}::swiglu_backward_cuda", mutates_args={"gate_grad", "up_grad"})
-@cpp_jit()
-def swiglu_backward_cuda(
-    gate: torch.Tensor,
-    up: torch.Tensor,
-    output_grad: torch.Tensor,
-    gate_grad: torch.Tensor,
-    up_grad: torch.Tensor,
-    BLOCK_SIZE: int,
-) -> None: ...
