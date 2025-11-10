106d083a0e49497bcb51086ffd181bfaf931071d
Mayank Mishra <mayank31398@gmail.com>
2025-11-10 01:40:52 -0800

    slight cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/cute_dsl_utils/math.py
+++ xma/cute_dsl_utils/math.py
@@ -3,8 +3,9 @@
 # **************************************************
 
 import cutlass.cute as cute
-from cutlass import Float32
+from cutlass import Float32, const_expr, range_constexpr
 from cutlass._mlir.dialects import llvm
+from cutlass.cute import TensorSSA
 from cutlass.cutlass_dsl import T, dsl_user_op
 
 
@@ -23,8 +24,19 @@ def _tanh(x: Float32 | float, *, loc=None, ip=None):
     )
 
 
-def tanh(x: cute.Float32 | float | TensorSSA):
-    return 0.5 * tanh(0.5 * x) + 0.5
+@cute.jit
+def tanh(x: cute.Float32 | float | TensorSSA, output_dtype=None):
+    if const_expr(isinstance(x, TensorSSA)):
+        y = cute.make_fragment(x.shape, x.dtype)
+        for i in range_constexpr(cute.size(x.shape)):
+            _x = x[i].to(Float32)
+            y[i] = _tanh(_x)
+        y = y.load()
+    else:
+        y = _tanh(x.to(Float32))
+        y = y.to(x.dtype)
+
+    return y
 
 
 def sigmoid(x: cute.Float32 | float):
