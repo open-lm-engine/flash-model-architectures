547f858bc7727df170226afea0d2708bde4be761
Mayank Mishra <mayank31398@gmail.com>
2025-11-10 14:54:41 -0800

    slight cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/swiglu/__init__.py
+++ xma/functional/swiglu/__init__.py
@@ -92,7 +92,7 @@ class _SwigluPacked(CustomOp):
         output = torch.empty(*x.size()[:-1], divide_if_divisible(x.size(-1), 2), device=x.device, dtype=x.dtype)
         up, gate = x.chunk(2, dim=-1)
 
-        swiglu_forward_triton(gate=gate, up=up, output=output)
+        swiglu_forward_cuda(gate=gate, up=up, output=output)
 
         return output
 
