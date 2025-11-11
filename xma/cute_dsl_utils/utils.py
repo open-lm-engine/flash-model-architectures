69d256582ab171a68e8dc0888c517e1e135dfd17
Mayank Mishra <mayank31398@gmail.com>
2025-11-11 14:55:53 -0800

    drop use_padding_free_transformer
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/cute_dsl_utils/utils.py
+++ xma/cute_dsl_utils/utils.py
@@ -8,14 +8,8 @@ import cutlass.cute as cute
 from cutlass.cute.runtime import from_dlpack
 
 
-def torch_tensor_to_cute_tensor(
-    x: torch.Tensor, leading_dim: int, alignment: int = 16, divisibility: int = 1
-) -> cute.Tensor:
+def torch_tensor_to_cute_tensor(x: torch.Tensor, leading_dim: int, alignment: int = 16) -> cute.Tensor:
     x = x.detach()
-    dim_order = x.dim_order()
-
     x = from_dlpack(x, assumed_align=alignment)
     x = x.mark_layout_dynamic(leading_dim=leading_dim)
-    x = x.mark_compact_shape_dynamic(mode=leading_dim, stride_order=dim_order, divisibility=divisibility)
-
     return x
