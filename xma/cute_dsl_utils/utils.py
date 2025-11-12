7233603b64f22c54fb52792a4e912546e5d5773e
Mayank Mishra <mayank31398@gmail.com>
2025-11-12 14:11:53 -0800

    drop use_padding_free_transformer
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/cute_dsl_utils/utils.py
+++ xma/cute_dsl_utils/utils.py
@@ -13,5 +13,10 @@ from ..utils import get_alignment
 def torch_tensor_to_cute_tensor(x: torch.Tensor, leading_dim: int) -> cute.Tensor:
     x = x.detach()
     x = from_dlpack(x, assumed_align=get_alignment(x))
+
+    if x.stride[leading_dim] == 0:
+        leading_dim = None
+
     x = x.mark_layout_dynamic(leading_dim=leading_dim)
+
     return x
