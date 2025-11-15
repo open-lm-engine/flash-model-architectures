1119b593d911304320601c162188cd939d5c1d8a
Mayank Mishra <mayank31398@gmail.com>
2025-11-14 21:20:15 -0800

    drop use_padding_free_transformer
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/functional/continuous_count_test.py
+++ tests/functional/continuous_count_test.py
@@ -27,15 +27,15 @@ class ContiguousCountTest(TestCommons):
     )
     def test_continuous_count(
         self,
-        size: int,
+        bins: int,
         device: torch.device,
         dtype: torch.dtype,
         function: Callable,
     ) -> None:
         set_seed(_SEED)
-        x = torch.randint(0, _MAX_EXPERTS, (size,), device=device, dtype=dtype)
+        x = torch.randint(0, _MAX_EXPERTS, (bins,), device=device, dtype=dtype)
 
-        z_kernel = function(x=x, size=_MAX_EXPERTS, kernel_backend=KernelBackend.cuda)
-        z_expected = continuous_count(x.view(-1), size=_MAX_EXPERTS, kernel_backend=KernelBackend.torch)
+        z_kernel = function(x=x, bins=_MAX_EXPERTS, kernel_backend=KernelBackend.cuda)
+        z_expected = continuous_count(x.view(-1), bins=_MAX_EXPERTS, kernel_backend=KernelBackend.torch)
 
         self.assert_equal_tensors(z_kernel, z_expected, True)
