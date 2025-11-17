9355525f131f565e5806ec15473f17b756f29bc9
Mayank Mishra <mayank31398@gmail.com>
2025-11-16 17:14:27 -0800

    xla
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/functional/swiglu_test.py
+++ tests/functional/swiglu_test.py
@@ -21,7 +21,7 @@ class SwiGLUTest(TestCommons):
             [swiglu, torch.compile(swiglu, fullgraph=True)],  # function
         )
         + TestCommons.make_args_matrix(
-            TestCommons.get_2d_tensor_sizes(),  # size
+            [(4100, 3700)],  # size
             TestCommons.get_dtypes()[:1],  # dtype
             [KernelBackend.pallas],  # kernel_backend
             [swiglu],  # function
