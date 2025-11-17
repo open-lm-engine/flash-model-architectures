eecb5b313a4a9c2cbcbce814b1382c220ddc0d37
Mayank Mishra <mayank31398@gmail.com>
2025-11-17 02:04:46 -0800

    xla
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/swiglu/pallas_implementation/forward.py
+++ xma/functional/swiglu/pallas_implementation/forward.py
@@ -33,8 +33,8 @@ def swiglu_forward_pallas_kernel(g_ref, u_ref, y_ref):
 @jax.jit
 def swiglu_forward_pallas_jit(g: jax.Array, u: jax.Array) -> jax.Array:
     B, H = g.shape
-    BLOCK_SIZE_B = 1024
     BLOCK_SIZE_H = min(ceil_divide(H, 128) * 128, 1024)
+    BLOCK_SIZE_B = 32 * 1024 * 1024 // (3 * BLOCK_SIZE_H * g.dtype.itemsize)
 
     kernel = pl.pallas_call(
         swiglu_forward_pallas_kernel,
