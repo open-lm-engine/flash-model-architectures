f8e6acf5031c2493c150589243623c5feb8fa5c3
Mayank Mishra <mayank31398@gmail.com>
2025-11-17 01:24:59 -0800

    xla
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/swiglu/pallas_implementation/backward.py
+++ xma/functional/swiglu/pallas_implementation/backward.py
@@ -39,6 +39,8 @@ def swiglu_backward_pallas_kernel(g_ref, u_ref, dy_ref, dg_ref, du_ref):
 @jax.jit
 def swiglu_backward_pallas_jit(g: jax.Array, u: jax.Array, dy: jax.Array) -> tuple[jax.Array, jax.Array]:
     B, H = g.shape
+    BLOCK_SIZE_B = 1024
+    BLOCK_SIZE_H = 1024
 
     kernel = pl.pallas_call(
         swiglu_backward_pallas_kernel,
@@ -46,15 +48,15 @@ def swiglu_backward_pallas_jit(g: jax.Array, u: jax.Array, dy: jax.Array) -> tup
             jax.ShapeDtypeStruct(shape=g.shape, dtype=g.dtype),
             jax.ShapeDtypeStruct(shape=g.shape, dtype=g.dtype),
         ],
-        grid=(ceil_divide(B, 8), ceil_divide(H, 1024)),
+        grid=(ceil_divide(B, BLOCK_SIZE_B), ceil_divide(H, BLOCK_SIZE_H)),
         in_specs=[
-            pl.BlockSpec(block_shape=(8, 1024), index_map=lambda x, y: (x, y)),
-            pl.BlockSpec(block_shape=(8, 1024), index_map=lambda x, y: (x, y)),
-            pl.BlockSpec(block_shape=(8, 1024), index_map=lambda x, y: (x, y)),
+            pl.BlockSpec(block_shape=(BLOCK_SIZE_B, BLOCK_SIZE_H), index_map=lambda x, y: (x, y)),
+            pl.BlockSpec(block_shape=(BLOCK_SIZE_B, BLOCK_SIZE_H), index_map=lambda x, y: (x, y)),
+            pl.BlockSpec(block_shape=(BLOCK_SIZE_B, BLOCK_SIZE_H), index_map=lambda x, y: (x, y)),
         ],
         out_specs=[
-            pl.BlockSpec(block_shape=(8, 1024), index_map=lambda x, y: (x, y)),
-            pl.BlockSpec(block_shape=(8, 1024), index_map=lambda x, y: (x, y)),
+            pl.BlockSpec(block_shape=(BLOCK_SIZE_B, BLOCK_SIZE_H), index_map=lambda x, y: (x, y)),
+            pl.BlockSpec(block_shape=(BLOCK_SIZE_B, BLOCK_SIZE_H), index_map=lambda x, y: (x, y)),
         ],
     )
 
