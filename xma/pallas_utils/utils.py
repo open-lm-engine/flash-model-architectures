4c2fef25d5234fba9031829870a3ab47b718804e
Mayank Mishra <mayank31398@gmail.com>
2025-11-16 16:03:05 -0800

    xla
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/pallas_utils/utils.py
+++ xma/pallas_utils/utils.py
@@ -9,9 +9,13 @@ from ..math import ceil_divide
 
 
 def pad_tensor_for_pallas(x: torch.Tensor, padding: tuple[int, int] = (8, 128)) -> torch.Tensor:
-    padding = tuple(ceil_divide(x.size(-i), padding[-i]) * padding[-i] - x.size(-i) for i in range(len(padding)))
+    full_padding = []
+    for i in range(1, len(padding) + 1):
+        pad = ceil_divide(x.size(-i), padding[-i]) * padding[-i] - x.size(-i)
+        full_padding.append(0)
+        full_padding.append(pad)
 
     if padding != (0, 0):
-        x = F.pad(x, pad=padding)
+        x = F.pad(x, pad=full_padding)
 
     return x
