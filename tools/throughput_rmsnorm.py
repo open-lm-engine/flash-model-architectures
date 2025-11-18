0e6027ec35799cf4ed37ae70a7fb4b2fa8563b0c
Mayank Mishra <mayank31398@gmail.com>
2025-11-18 14:36:32 -0800

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

---
+++ tools/throughput_rmsnorm.py
@@ -0,0 +1,66 @@
+# **************************************************
+# Copyright (c) 2025, Mayank Mishra
+# **************************************************
+
+from functools import partial
+
+import torch
+from tabulate import tabulate
+
+from xma import RNN, Accelerator, KernelBackend, rmsnorm
+
+
+n = 100
+
+kernels = [
+    (rmsnorm, KernelBackend.torch, "torch"),
+    (torch.compile(rmsnorm, dynamic=True), KernelBackend.torch, "torch compile"),
+    (rmsnorm, KernelBackend.triton, "triton"),
+]
+dtypes = [torch.float32, torch.bfloat16, torch.float16]
+headers = ["kernel"] + dtypes
+table = []
+
+B = 16 * 4096
+H = 4096
+
+run_forward = False
+
+for kernel, kernel_backend, row_header in kernels:
+    row = [row_header]
+
+    if not kernel_backend.verify_accelerator():
+        for _ in range(len(dtypes)):
+            row.append("NA")
+        table.append(row)
+        continue
+
+    device = kernel_backend.get_compatible_accelerator().get_current_device()
+
+    for dtype in dtypes:
+        x = torch.randn(B, H, device=device, dtype=dtype, requires_grad=not run_forward)
+        W = torch.randn(H, device=device, dtype=dtype, requires_grad=not run_forward)
+
+        for i in range(n):
+            z = kernel(x=x, weight=W)
+
+        s = torch.cuda.Event(enable_timing=True)
+        e = torch.cuda.Event(enable_timing=True)
+
+        s.record()
+        for i in range(n):
+            if run_forward:
+                z = kernel(x=x, weight=W, kernel_backend=kernel_backend)
+            else:
+                torch.autograd.grad(z, (g, u), grad_outputs=dy, retain_graph=True)
+        e.record()
+
+        Accelerator.synchronize()
+
+        t = s.elapsed_time(e) / n / 1e3
+        row.append(((B + 1) * H if run_forward else 5) * dtype.itemsize / t / 1e12)
+
+    table.append(row)
+
+
+print(tabulate(table, headers=headers))
