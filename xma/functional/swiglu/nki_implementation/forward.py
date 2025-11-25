0427bc14403e088240f8265142fc7c87164439da
Mayank Mishra <mayank31398@gmail.com>
2025-11-25 14:50:15 -0800

    nki swiglu
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/swiglu/nki_implementation/forward.py
+++ xma/functional/swiglu/nki_implementation/forward.py
@@ -12,20 +12,33 @@ from ....utils import get_num_elements_and_hidden_size
 
 
 @nki.jit
-def swiglu_forward_nki_kernel(g_ptr, u_ptr, y_ptr):
+def swiglu_forward_nki_kernel(g_ptr, u_ptr, y_ptr, B, H):
     BLOCK_SIZE_B = 128
     BLOCK_SIZE_H = 512
 
-    g = nl.load(g_ptr[0:BLOCK_SIZE_B, 0:BLOCK_SIZE_H])
-    u = nl.load(u_ptr[0:BLOCK_SIZE_B, 0:BLOCK_SIZE_H])
+    BLOCK_Bs = 0
+    BLOCK_Be = 0
 
-    y = u * g * nl.sigmoid(g)
+    for b in range(B):
+        BLOCK_Be += BLOCK_SIZE_B
+        BLOCK_Hs = 0
+        BLOCK_He = 0
 
-    nl.store(y_ptr[0:BLOCK_SIZE_B, 0:BLOCK_SIZE_H], y)
+        for h in range(H):
+            BLOCK_He += BLOCK_SIZE_H
+
+            g = nl.load(g_ptr[BLOCK_Bs:BLOCK_Be, BLOCK_Hs:BLOCK_He])
+            u = nl.load(u_ptr[BLOCK_Bs:BLOCK_Be, BLOCK_Hs:BLOCK_He])
+
+            y = u * g * nl.sigmoid(g)
+
+            nl.store(y_ptr[BLOCK_Bs:BLOCK_Be, BLOCK_Hs:BLOCK_He], y)
+
+        BLOCK_Bs = BLOCK_Be
 
 
 @custom_op(f"{LIBRARY_NAME}::swiglu_forward_nki", mutates_args={"y"})
 def swiglu_forward_nki(g: torch.Tensor, u: torch.Tensor, y: torch.Tensor) -> None:
     B, H = get_num_elements_and_hidden_size(g)
 
-    swiglu_forward_nki_kernel(g_ptr=g, u_ptr=u, y_ptr=y)
+    swiglu_forward_nki_kernel(g_ptr=g, u_ptr=u, y_ptr=y, B=B, H=H)
