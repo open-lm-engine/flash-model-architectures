b964ade848e4ea40624e0af3ff3a02dee91ac6ea
Mayank Mishra <mayank31398@gmail.com>
2025-11-25 17:26:07 -0800

    nki swiglu
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/functional/swiglu/nki_implementation/forward.py
+++ xma/functional/swiglu/nki_implementation/forward.py
@@ -8,26 +8,27 @@ from torch.library import custom_op
 from torch_neuronx import nki
 
 from ....constants import LIBRARY_NAME
-from ....utils import get_num_elements_and_hidden_size
 
 
 @nki.jit
-def swiglu_forward_nki_kernel(g_ptr, u_ptr, y_ptr, B, H):
+def swiglu_forward_nki_kernel(g_ptr, u_ptr, y_ptr):
     BLOCK_SIZE_B = 128
     BLOCK_SIZE_H = 512
 
+    B, H = g_ptr.shape
     BLOCK_Bs = 0
     BLOCK_Be = 0
 
-    for b in range(B):
+    for b in nl.affine_range(B):
         BLOCK_Be += BLOCK_SIZE_B
         BLOCK_Hs = 0
         BLOCK_He = 0
+        # MASK_B = nl.arange(BLOCK_Bs, BLOCK_Be) < nl.arange(64)
 
-        for h in range(H):
+        for h in nl.affine_range(H):
             BLOCK_He += BLOCK_SIZE_H
 
-            g = nl.load(g_ptr[BLOCK_Bs:BLOCK_Be, BLOCK_Hs:BLOCK_He])
+            g = nl.load(g_ptr[BLOCK_Bs:BLOCK_Be, BLOCK_Hs:BLOCK_He])  # , mask=MASK_B[:, None] & MASK_H[None, :])
             u = nl.load(u_ptr[BLOCK_Bs:BLOCK_Be, BLOCK_Hs:BLOCK_He])
 
             y = u * g * nl.sigmoid(g)
@@ -41,6 +42,4 @@ def swiglu_forward_nki_kernel(g_ptr, u_ptr, y_ptr, B, H):
 
 @custom_op(f"{LIBRARY_NAME}::swiglu_forward_nki", mutates_args={"y"})
 def swiglu_forward_nki(g: torch.Tensor, u: torch.Tensor, y: torch.Tensor) -> None:
-    B, H = get_num_elements_and_hidden_size(g)
-
-    swiglu_forward_nki_kernel(g_ptr=g, u_ptr=u, y_ptr=y, B=B, H=H)
+    swiglu_forward_nki_kernel(g_ptr=g, u_ptr=u, y_ptr=y)
