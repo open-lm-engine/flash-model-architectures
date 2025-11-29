76db2b3fde4b8c01a17c277f86a918c162f4743b
Mayank Mishra <mayank31398@gmail.com>
2025-11-29 12:50:03 -0800

    add op
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/custom_op.py
+++ xma/custom_op.py
@@ -4,7 +4,8 @@
 
 from __future__ import annotations
 
-from typing import Any
+import inspect
+from typing import Any, Callable, Iterable, Sequence
 
 import torch
 
@@ -116,3 +117,28 @@ class CustomOp(torch.autograd.Function):
     @classmethod
     def _get_key(cls, kernel_backend: KernelBackend) -> str:
         return f"{cls.__name__}-{kernel_backend.value}"
+
+
+def xma_op(
+    mutates_args: str | Iterable[str] = None,
+    device_types: str | Sequence[str] | None = None,
+    schema: str | None = None,
+    fake_func: Callable | None = None,
+) -> Callable:
+    def _inner(func: Callable):
+        custom_op = torch.library.custom_op(
+            func.__name__, func, mutates_args=mutates_args, device_types=device_types, schema=schema
+        )
+
+        if fake_func is not None:
+            custom_op.register_fake(fake_func)
+
+        def _run(*args, **kwargs):
+            return custom_op(*args, **kwargs)
+
+        _run.__signature__ = inspect.signature(func)
+        _run.__name__ = func.__name__
+
+        return _run
+
+    return _inner
