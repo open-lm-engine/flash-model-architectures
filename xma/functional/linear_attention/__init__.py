bccb1da3cff03e26b8ddd04be92c3805e7c199b1
Mayank Mishra <mayank31398@gmail.com>
2025-12-25 22:19:12 -0800

    init

---
+++ xma/functional/linear_attention/__init__.py
@@ -0,0 +1,61 @@
+# **************************************************
+# Copyright (c) 2025, Mayank Mishra
+# **************************************************
+
+import torch
+
+from ...accelerator import KernelBackend
+from ...custom_op import CustomOp
+
+
+class _LinearAttention(CustomOp):
+    @staticmethod
+    def forward_backward_torch(
+        q: torch.Tensor,
+        k: torch.Tensor,
+        v: torch.Tensor,
+        h0: torch.Tensor | None,
+        cu_seqlens: torch.Tensor | None = None,
+        max_seqlen: torch.Tensor | int | None = None,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        B, S, Nq, K = q.size()
+        Nk = k.size(-2)
+        Nv, V = v.size()[-2:]
+
+        N = max(Nq, Nk, Nv)
+
+        Gq = N // Nq
+        Gk = N // Nk
+        Gv = N // Nv
+
+        if h0 is not None:
+            h0 = torch.zeros(B, S, N, K, V, dtype=q.dtype, device=q.device)
+
+        h = h0
+
+        y_shape = list(q.size())
+        y_shape[-2] = N
+        y = torch.empty(y_shape, device=q.device, dtype=q.dtype)
+
+        q = q.repeat_interleave(Gq, dim=-2)
+        k = k.repeat_interleave(Gk, dim=-2)
+        v = v.repeat_interleave(Gv, dim=-2)
+
+        for s in range(S):
+            h = h + k[..., None] * v[..., None, :]
+            y[:, s] = (q[..., None, :] @ h).squeeze(-2)
+
+        return y, h
+
+    @staticmethod
+    def forward(
+        ctx,
+        q: torch.Tensor,
+        k: torch.Tensor,
+        v: torch.Tensor,
+        h0: torch.Tensor | None,
+        cu_seqlens: torch.Tensor | None = None,
+        max_seqlen: torch.Tensor | int | None = None,
+        *,
+        kernel_backend: KernelBackend,
+    ) -> tuple[torch.Tensor, torch.Tensor]: ...
