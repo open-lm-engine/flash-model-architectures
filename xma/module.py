ed20c646574cf3e9dffb813a4af246bcd83bc257
Mayank Mishra <mayank31398@gmail.com>
2025-12-31 14:32:07 -0800

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/module.py
+++ xma/module.py
@@ -8,5 +8,4 @@ import torch.nn as nn
 class XMAModule(nn.Module):
     def extra_repr(self) -> str:
         num_parameters = sum(p.numel() for p in self.parameters())
-
         return f"# parameters = {num_parameters:,}"
