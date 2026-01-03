a602ce895b66711522ebd9c9216de589f5894550
Mayank Mishra <mayank31398@gmail.com>
2026-01-03 14:12:15 -0800

    add sphinx
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,7 @@
+# **************************************************
+# Copyright (c) 2025, Mayank Mishra
+# **************************************************
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,6 +10,12 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath("../xma"))
+
 project = "XMA"
 copyright = "2026, Mayank Mishra"
 author = "Mayank Mishra"
@@ -13,7 +23,14 @@ author = "Mayank Mishra"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",  # supports Google / NumPy style docstrings
+]
+
+autosummary_generate = True
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
