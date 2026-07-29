f4194b3fee42adaafcac841e2871db68860bf0ce
Mayank Mishra <mayank31398@gmail.com>
2026-07-29 16:52:35 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/optimizers/sgd/torch_implementation.py
+++ xma/optimizers/sgd/torch_implementation.py
@@ -5,29 +5,29 @@
 from __future__ import annotations
 
 import torch
+from torch.optim.optimizer import Optimizer
 
 
 def _sgd_torch(
     params: list[torch.Tensor],
     grads: list[torch.Tensor],
-    momentum_buffer_list: list[torch.Tensor],
+    momentum_buffer_list: list[torch.Tensor | None],
     lr: float,
     weight_decay: float,
     momentum: float,
     dampening: float,
     nesterov: bool,
     maximize: bool,
+    step: int,
 ) -> None:
     grouped_tensors = Optimizer._group_tensors_by_device_and_dtype(
         [params, grads, momentum_buffer_list],  # type: ignore[list-item]
         with_indices=True,
     )
 
-    for (
-        device_params_,
-        device_grads_,
-        device_momentum_buffer_list,
-    ), indices in grouped_tensors.values():
+    is_first_step = step == 1
+
+    for (device_params_, device_grads_, device_momentum_buffer_list), indices in grouped_tensors.values():
         device_params: list[torch.Tensor] = device_params_
         device_grads: list[torch.Tensor] = device_grads_
 
@@ -44,36 +44,12 @@ def _sgd_torch(
                 )
 
         if momentum != 0:
-            bufs: list[Tensor] = []
-
-            all_states_with_momentum_buffer = True
-            for i in range(len(device_momentum_buffer_list)):
-                if device_momentum_buffer_list[i] is None:
-                    all_states_with_momentum_buffer = False
-                    break
-                else:
-                    bufs.append(cast(Tensor, device_momentum_buffer_list[i]))
-
-            if all_states_with_momentum_buffer:
-                torch._foreach_mul_(bufs, momentum)
-                torch._foreach_add_(bufs, device_grads, alpha=1 - dampening)
-            else:
-                bufs = []
-
-                for i in range(len(device_momentum_buffer_list)):
-                    if device_momentum_buffer_list[i] is None:
-                        buf = device_momentum_buffer_list[i] = momentum_buffer_list[indices[i]] = (
-                            device_grads[i].detach().clone()
-                        )
-                    else:
-                        buf = cast(Tensor, device_momentum_buffer_list[i])
-                        buf.mul_(momentum).add_(device_grads[i], alpha=1 - dampening)
-
-                    bufs.append(buf)
+            torch._foreach_mul_(device_momentum_buffer_list, momentum)
+            torch._foreach_add_(device_momentum_buffer_list, device_grads, alpha=1 if is_first_step else 1 - dampening)
 
             if nesterov:
-                torch._foreach_add_(device_grads, bufs, alpha=momentum)
+                torch._foreach_add_(device_grads, device_momentum_buffer_list, alpha=momentum)
             else:
-                device_grads = bufs
+                device_grads = device_momentum_buffer_list
 
         torch._foreach_add_(device_params, device_grads, alpha=-lr)
