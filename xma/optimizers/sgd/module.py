6b42369d44f9651b7ce57dd3bf2f96064e5b7a38
Mayank Mishra <mayank31398@gmail.com>
2026-07-29 14:41:00 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/optimizers/sgd/module.py
+++ xma/optimizers/sgd/module.py
@@ -4,16 +4,35 @@
 
 from __future__ import annotations
 
-from typing import Callable
+from dataclasses import dataclass
+from typing import Any, Callable, Iterable, TypeAlias
 
 import torch
-from torch.optim import SGD as _TorchSGD
+from torch.optim import SGD
 
 from ...accelerator import KernelBackend
 from .op import sgd
 
 
-class SGD(_TorchSGD):
+ParamsT: TypeAlias = Iterable[torch.Tensor] | Iterable[dict[str, Any]] | Iterable[tuple[str, torch.Tensor]]
+
+
+@dataclass
+class SGDParamsGroup:
+    params: dict[str, torch.Tensor]
+    momentum_buffers: dict[str, torch.Tensor] | None
+    lr: float
+    momentum: float
+    dampening: float
+    weight_decay: float
+    nesterov: bool
+    maximize: bool
+
+
+class SGD:
+    def __init__(self, param_groups: list[SGDParamsGroup]) -> SGD:
+        self.param_groups = param_groups
+
     @torch.no_grad()
     def step(self, closure: Callable | None = None, *, kernel_backend: KernelBackend | None = None) -> None:
         loss = None
@@ -25,24 +44,24 @@ class SGD(_TorchSGD):
             params = []
             grads = []
             momentum_buffer_list = []
+            for name, W in group.params.items():
+                if W.grad is None:
+                    continue
 
-            has_sparse_grad = self._init_group(
-                group=group, params=params, grads=grads, momentum_buffer_list=momentum_buffer_list
-            )
-
-            assert not has_sparse_grad
+                params.append(W)
+                grads.append(W.grad)
+                momentum_buffer_list.append(group.momentum_buffers[name])
 
             sgd(
                 params=params,
                 grads=grads,
                 momentum_buffer_list=momentum_buffer_list,
-                lr=group["lr"],
-                weight_decay=group["weight_decay"],
-                momentum=group["momentum"],
-                dampening=group["dampening"],
-                nesterov=group["nesterov"],
-                maximize=group["maximize"],
-                foreach=group["foreach"],
+                lr=group.lr,
+                weight_decay=group.weight_decay,
+                momentum=group.momentum,
+                dampening=group.dampening,
+                nesterov=group.nesterov,
+                maximize=group.maximize,
                 kernel_backend=kernel_backend,
             )
 
--- xma/optimizers/sgd/op.py
+++ xma/optimizers/sgd/op.py
@@ -27,18 +27,18 @@ def sgd(
     dampening: float,
     nesterov: bool,
     maximize: bool,
-    foreach: bool,
     *,
     kernel_backend: KernelBackend | None = None,
 ) -> None:
+    if len(params) == 0:
+        return
+
     if kernel_backend is None:
         kernel_backend = Accelerator.get_kernel_backend()
     else:
         assert kernel_backend.verify_accelerator()
 
     if kernel_backend in [KernelBackend.cuda, KernelBackend.triton]:
-        assert not foreach
-
         is_first_step = False
         if momentum == 0:
             assert len(momentum_buffer_list) == 0
@@ -88,19 +88,69 @@ def sgd(
                 is_first_step=is_first_step,
             )
     elif kernel_backend == KernelBackend.torch:
-        (_multi_tensor_sgd if foreach else _single_tensor_sgd)(
-            params=params,
-            grads=grads,
-            momentum_buffer_list=momentum_buffer_list,
-            grad_scale=None,
-            found_inf=None,
-            weight_decay=weight_decay,
-            momentum=momentum,
-            lr=lr,
-            dampening=dampening,
-            nesterov=nesterov,
-            maximize=maximize,
-            has_sparse_grad=False,
+        grouped_tensors = Optimizer._group_tensors_by_device_and_dtype(
+            [params, grads, momentum_buffer_list],  # type: ignore[list-item]
+            with_indices=True,
         )
+
+        for (
+            device_params_,
+            device_grads_,
+            device_momentum_buffer_list,
+        ), indices in grouped_tensors.values():
+            device_params: list[Tensor] = cast(list[Tensor], device_params_)
+            device_grads: list[Tensor] = cast(list[Tensor], device_grads_)
+
+            if maximize:
+                device_grads = torch._foreach_neg(device_grads)  # type: ignore[assignment]
+
+            if weight_decay != 0:
+                # Reuse the intermediate memory (device_grads) already allocated for maximize
+                if maximize:
+                    torch._foreach_add_(device_grads, device_params, alpha=weight_decay)
+                else:
+                    device_grads = torch._foreach_add(  # type: ignore[assignment]
+                        device_grads, device_params, alpha=weight_decay
+                    )
+
+            if momentum != 0:
+                bufs: list[Tensor] = []
+
+                all_states_with_momentum_buffer = True
+                for i in range(len(device_momentum_buffer_list)):
+                    if device_momentum_buffer_list[i] is None:
+                        all_states_with_momentum_buffer = False
+                        break
+                    else:
+                        bufs.append(cast(Tensor, device_momentum_buffer_list[i]))
+
+                if all_states_with_momentum_buffer:
+                    torch._foreach_mul_(bufs, momentum)
+                    torch._foreach_add_(bufs, device_grads, alpha=1 - dampening)
+                else:
+                    bufs = []
+
+                    for i in range(len(device_momentum_buffer_list)):
+                        if device_momentum_buffer_list[i] is None:
+                            buf = device_momentum_buffer_list[i] = momentum_buffer_list[indices[i]] = (
+                                device_grads[i].detach().clone()
+                            )
+                        else:
+                            buf = cast(Tensor, device_momentum_buffer_list[i])
+                            buf.mul_(momentum).add_(device_grads[i], alpha=1 - dampening)
+
+                        bufs.append(buf)
+
+                if nesterov:
+                    torch._foreach_add_(device_grads, bufs, alpha=momentum)
+                else:
+                    device_grads = bufs
+
+            # handle internal item() call if lr is a tensor
+            if isinstance(lr, torch.Tensor) and torch.compiler.is_compiling():
+                grads_x_lr = torch._foreach_mul(device_grads, -lr)
+                torch._foreach_add_(device_params, grads_x_lr)
+            else:
+                torch._foreach_add_(device_params, device_grads, alpha=-lr)
     else:
         raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
