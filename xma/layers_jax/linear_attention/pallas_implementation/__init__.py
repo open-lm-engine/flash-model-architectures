d334c97d24e8c033b341d2cfe60637b543f3c41d
Mayank Mishra <mayank31398@gmail.com>
2026-08-05 22:29:10 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/layers_jax/linear_attention/pallas_implementation/__init__.py
+++ xma/layers_jax/linear_attention/pallas_implementation/__init__.py
@@ -7,7 +7,7 @@ from functools import partial
 import jax
 import jax.numpy as jnp
 
-from .backward import _linear_attention_backward_pallas
+from .backward import _backward_core, _state_passing_core
 from .forward import _forward_core
 
 
@@ -62,17 +62,35 @@ def _linear_attention_forward_jax(
     return (y, h), (query, key, value, input_state)
 
 
+@partial(jax.jit, static_argnames=("attention_multiplier", "BLOCK_SIZE_S", "BLOCK_SIZE_V"))
 def _linear_attention_backward_jax(
     attention_multiplier: float, BLOCK_SIZE_S: int, BLOCK_SIZE_V: int, residuals: tuple, cotangents: tuple
 ) -> tuple:
-    query, key, value, input_state = residuals
+    q, k, v, h0 = residuals
     dy, dht = cotangents
 
-    dq, dk, dv, dh0 = _linear_attention_backward_pallas(
-        q=query,
-        k=key,
-        v=value,
-        h0=input_state,
+    B, S, Nq, K = q.shape
+    Nk = k.shape[-2]
+    Nv, V = v.shape[-2:]
+
+    N = max(Nq, Nk, Nv)
+
+    Gq = N // Nq
+    Gk = N // Nk
+    Gv = N // Nv
+
+    q = jnp.swapaxes(q, 1, 2)
+    k = jnp.swapaxes(k, 1, 2)
+    v = jnp.swapaxes(v, 1, 2)
+    dy = jnp.swapaxes(dy, 1, 2)
+
+    h, _ = _state_passing_core(k=k, v=v, h0=h0, N=N, BLOCK_SIZE_S=BLOCK_SIZE_S, BLOCK_SIZE_V=BLOCK_SIZE_V)
+
+    dq, dk, dv, dh0 = _backward_core(
+        q=q,
+        k=k,
+        v=v,
+        h=h,
         dy=dy,
         dht=dht,
         attention_multiplier=attention_multiplier,
@@ -80,7 +98,15 @@ def _linear_attention_backward_jax(
         BLOCK_SIZE_V=BLOCK_SIZE_V,
     )
 
-    if input_state is None:
+    dq = jnp.swapaxes(dq, 1, 2)
+    dk = jnp.swapaxes(dk, 1, 2)
+    dv = jnp.swapaxes(dv, 1, 2)
+
+    dq = dq.reshape(B, S, Nq, Gq, K).sum(axis=3)
+    dk = dk.reshape(B, S, Nk, Gk, K).sum(axis=3)
+    dv = dv.reshape(B, S, Nv, Gv, V).sum(axis=3)
+
+    if h0 is None:
         dh0 = None
 
     return dq, dk, dv, dh0
