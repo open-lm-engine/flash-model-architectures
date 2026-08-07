4fc5cb8f5cde1462b74a88fee353a3ab693e0af6
Mayank Mishra <mayank31398@gmail.com>
2026-08-07 01:49:27 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/layers_jax/__init__.py
+++ xma/layers_jax/__init__.py
@@ -2,8 +2,6 @@
 # Copyright (c) 2026, Mayank Mishra
 # **************************************************
 
-from __future__ import annotations
-
 from ..utils import is_haliax_available
 from .depthwise_causal_convolution import depthwise_causal_convolution_jax
 from .linear_attention import linear_attention_jax
