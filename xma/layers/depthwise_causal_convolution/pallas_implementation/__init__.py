b0d75c63ed137b615e3e73ff9cdf48d813e73858
Mayank Mishra <mayank31398@gmail.com>
2026-08-09 00:14:30 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/layers/depthwise_causal_convolution/pallas_implementation/__init__.py
+++ xma/layers/depthwise_causal_convolution/pallas_implementation/__init__.py
@@ -18,30 +18,14 @@ class _DepthwiseCausalConvolutionPallas(torch.autograd.Function):
         input_state: torch.Tensor | None,
     ) -> tuple[torch.Tensor, torch.Tensor]:
         y, ht = _depthwise_causal_convolution_forward_pallas(x=x, W=weight, b=bias, h0=input_state)
-
-        ctx.bias_is_none = bias is None
-        ctx.input_state_is_none = input_state is None
-
-        tensors_to_save = [x, weight]
-        if bias is not None:
-            tensors_to_save.append(bias)
-        if input_state is not None:
-            tensors_to_save.append(input_state)
-
-        ctx.save_for_backward(*tensors_to_save)
-
+        ctx.save_for_backward(x, weight, bias, input_state)
         return y, ht
 
     @staticmethod
     def backward(
         ctx, dy: torch.Tensor, dht: torch.Tensor
     ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor | None, torch.Tensor | None]:
-        saved_tensors = list(ctx.saved_tensors)
-
-        x = saved_tensors.pop(0)
-        weight = saved_tensors.pop(0)
-        bias = None if ctx.bias_is_none else saved_tensors.pop(0)
-        input_state = None if ctx.input_state_is_none else saved_tensors.pop(0)
+        x, weight, bias, input_state = ctx.saved_tensors
 
         dx, dW, db, dh0 = _depthwise_causal_convolution_backward_pallas(
             x=x, W=weight, b=bias, h0=input_state, dy=dy, dht=dht
