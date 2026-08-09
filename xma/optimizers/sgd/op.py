4569b14c6cf5baa1d8a8692110b6a6801da9f759
Mayank Mishra <mayank31398@gmail.com>
2026-08-08 23:41:34 -0700

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- xma/optimizers/sgd/op.py
+++ xma/optimizers/sgd/op.py
@@ -2,8 +2,6 @@
 # Copyright (c) 2026, Mayank Mishra
 # **************************************************
 
-from __future__ import annotations
-
 import torch
 
 from ...accelerator import Accelerator, KernelBackend
